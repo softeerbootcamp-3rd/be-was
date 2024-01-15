76f6be6982c1c48666e48f4c80fdc3ff0d75c1a0
ddrongy <ddrongy@gmail.com>
2024-01-15 18:49:04 +0900

    feat: request 객체 생성
    {method, url}를 파싱하여 저장할 객체 클래스를 생성함
    - String, String 형으로 지정

---
+++ src/main/java/model/Request.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Request {
+    private String method;
+    private String url;
+
+    public Request(String method, String url) {
+        this.method = method;
+        this.url = url;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
