ae567f8dce6e246beb006d74b6ed7ca13f3a3892
johan1103 <johan1103@naver.com>
2024-01-15 18:56:01 +0900

    feat: 프로젝트 루트 디렉토리로부터 파일 경로 인식 후 해당 자원 반환하도록 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,19 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String absoluteRootPath = Paths.get("").toAbsolutePath().toString();
+    private static final String templateResourcePath = "/src/main/resources/templates";
+    private static final String staticResourcePath = "/src/main/resources/static";
 
     private Socket connection;
 
@@ -24,8 +27,10 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            MyHttpServletRequest httpServletRequest = printReceivedRequest(in);
+            logger.debug("http request : {}",httpServletRequest.toString());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World!".getBytes();
+            byte[] body = getFileBytes(httpServletRequest.getUri());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -51,5 +56,37 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private MyHttpServletRequest printReceivedRequest(InputStream in) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        String line = br.readLine();
+        logger.debug("request line : {}",line);
+        MyHttpServletRequest servletRequest = MyHttpServletRequest.init(line);
+        if(servletRequest==null)
+            return null;
+        while(true){
+            line=br.readLine();
+            if (line.equals(""))
+                break;
+            logger.debug("header : {}",line);
+            servletRequest.setFieldByName(line);
+        }
+        return servletRequest;
+    }
+
+    private byte[] getFileBytes(String uri){
+        byte[] body = null;
+        try {
+            body = Files.readAllBytes(buildPath(uri));
+        }catch (IOException ioException){
+            logger.error(ioException.getMessage());
+        }
+        return body;
+    }
+
+    private Path buildPath(String uri){
+        String path = absoluteRootPath+templateResourcePath+"/index.html";
+        return Paths.get(path);
     }
 }
