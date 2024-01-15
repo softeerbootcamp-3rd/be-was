5f94c8d4a8600b1209c073ff6fd16266cdf1cfd2
admin <admin@adminui-MacBookPro-26.local>
2024-01-15 14:20:18 +0900

    feat: index.html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +22,24 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            String path = tokens[1];
+            logger.debug("reqeust url {}",path);
+            //request 전부 읽어오기
+            //마지막엔 빈 공백 문자열이 있음
+            /*
+            while(!line.equals("")){
+                line = br.readLine();
+                logger.debug("header : " + line);
+            }*/
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./src/main/resources/templates" + path).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
