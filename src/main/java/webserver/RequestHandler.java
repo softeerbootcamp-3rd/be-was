d140791c976a17e4c88606903875cfadbea9963e
insiderhj <insiderhj@gmail.com>
2024-01-15 14:08:35 +0900

    step-1 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,42 +1,70 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.ResourceLoader;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello insiderhj".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String requestLine = reader.readLine();
+
+            StringBuilder sb = new StringBuilder(requestLine);
+
+            String line;
+            while (!(line = reader.readLine()).isEmpty())
+                sb.append(line).append("\n");
+
+            logger.debug("Connection IP : {}, Port : {}, request: {}",
+                    connection.getInetAddress(), connection.getPort(), sb);
+
+            if (requestLine != null) {
+                String[] requestParts = requestLine.split(" ");
+                String requestPath = requestParts[1];
+                responseFile(out, requestPath);
+            }
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            logger.error("Error processing request: {}", e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void responseFile(OutputStream out, String requestPath) throws IOException {
+        String basePath = "src/main/resources/templates";
+        if (requestPath.startsWith("/css/") || requestPath.startsWith("/fonts/")
+            || requestPath.startsWith("/images/") || requestPath.startsWith("/js/"))
+            basePath = "src/main/resources/static";
+
+        Path filePath = Paths.get(basePath + requestPath);
+
+        if (Files.exists(filePath) && Files.isRegularFile(filePath)) {
+            byte[] content = Files.readAllBytes(filePath);
+            DataOutputStream dos = new DataOutputStream(out);
+            response200Header(dos, ResourceLoader.getContentType(requestPath), content.length);
+            responseBody(dos, content);
+        } else {
+            send404(out);
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, String contentType, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -51,5 +79,17 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void send404(OutputStream out) throws IOException {
+        String errorMessage = "404 Not Found";
+        byte[] errorBody = errorMessage.getBytes();
+        DataOutputStream dos = new DataOutputStream(out);
+        dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
+        dos.writeBytes("Content-Type: text/plain;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + errorBody.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(errorBody, 0, errorBody.length);
+        dos.flush();
     }
 }
