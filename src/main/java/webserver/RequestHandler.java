6d1cf29dd2f779ff5479e6ca052e75bf38d4b33b
eyeben <qkrehdals97@naver.com>
2024-01-15 18:15:28 +0900

    RequestHandler: connected url to templates, added debug messages. WebServer: change thread generating method from thread to thread pool

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +24,35 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            //line: request line GET /index.html HTTP/1.1
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+            System.out.println("[[["+url+"]]]");
+
+
+
+
+            logger.debug("request line {}",line);
+            while(line != null && !line.isEmpty()) {
+                line = br.readLine();
+                logger.debug("header : {}", line);
+            }
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello 동민".getBytes();
+
+
+            // 1. File이라는 객체를 templateFilePath + url로 접근
+            // 2. File객체를 Path객체로 변환
+
+            String templateFilePath = "be-was/src/main/resources/templates";
+            Path path = new File(templateFilePath + url).toPath();
+            byte[] body = Files.readAllBytes(path);
+
+
+            //byte[] body = "Hello 동민".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -39,6 +66,7 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+            logger.debug(dos.toString());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
