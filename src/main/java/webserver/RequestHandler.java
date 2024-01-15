2e840b1cdce88f6573317e61847165aacd920795
admin <admin@admin-MacBook-Pro-3.local>
2024-01-15 13:58:36 +0900

    Step2-1 완성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,13 +24,55 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+//            System.out.println("Request : " + br.readLine());
+            // Request : GET /index.html HTTP/1.1
+            // Request : GET /favicon.ico HTTP/1.1
+            String line = br.readLine();
+//            System.out.println("Request : " + line);
+            logger.debug("Request line : {}", line);
+
+            String[] tokens = line.split(" ");
+
+//            while(line != null) {
+//            while(!line.equals("")) { // 헤더의 끝은 빈 공백 문자열이 들어있다.
+//                line = br.readLine();
+////                System.out.println("Request : " + line);
+//                logger.debug("Header : {}", line);
+//            }
+
             DataOutputStream dos = new DataOutputStream(out);
 //            byte[] body = "Hello World".getBytes();
-            byte[] body = "Hello, World!!".getBytes();
+//            byte[] body = "Hello, World!!".getBytes();
+            String url = tokens[1];
+
+            Path htmlFilePath = Paths.get("/Users/admin/Softeer/be-was/src/main/resources/templates" + url);
+
+            byte[] body = Files.readAllBytes(htmlFilePath);
+
+            Path htmlDirectory = htmlFilePath.getParent().getParent();
+
             response200Header(dos, body.length);
             responseBody(dos, body);
+
+            // Send related files
+            sendRelatedFile(htmlDirectory, "/static/css/bootstrap.min.css", dos);
+            sendRelatedFile(htmlDirectory, "/static/js/jquery-2.2.0.min.js", dos);
+            sendRelatedFile(htmlDirectory, "/static/js/bootstrap.min.js", dos);
+            sendRelatedFile(htmlDirectory, "/static/js/scripts.js", dos);
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private void sendRelatedFile(Path baseDirectory, String relativePath, DataOutputStream dos) throws IOException {
+        Path staticFilePath = Paths.get(baseDirectory.toString(), relativePath);
+        logger.debug("static path: "+staticFilePath);
+        if (Files.exists(staticFilePath)) {
+            byte[] fileContent = Files.readAllBytes(staticFilePath);
+            response200Header(dos, fileContent.length);
+            responseBody(dos, fileContent);
         }
     }
 
