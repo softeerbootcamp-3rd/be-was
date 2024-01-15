801d5b0cf817cb58dda40353a139bddb51915d1c
99winnmin <ojysep9987@gmail.com>
2024-01-15 13:57:29 +0900

    :art: Fix : Thread 구조 변경
    "요구사항 : 기존의 프로젝트는 Java Thread 기반으로 작성되어 있다. 이를 Concurrent 패키지를 사용하도록 변경한다."

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,6 +3,7 @@ package webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,12 +22,12 @@ public class WebServer {
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
-
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
+            var executor = Executors.newFixedThreadPool(30);
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                // STEP-1 [요구 사항 3] Concurrent 패키지를 사용하도록 변경.
+                executor.execute(new RequestHandler(connection));
             }
         }
     }
