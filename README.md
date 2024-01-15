162a56a4fd7a9d309d4b5c8e1274b35b2938c947
jbonnie <bonnie8623@gmail.com>
2024-01-15 17:30:39 +0900

    index.html 응답

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Java Web Application Server 2023
 
 이 프로젝트는 우아한 테크코스 박재성님의 허가를 받아 https://github.com/woowacourse/jwp-was 
 를 참고하여 작성되었습니다.
+-------------------------------
+
+## 1. 웹 서버 1단계 - index.html 응답
+### 1. WebServer 클래스
+- 웹 서버를 시작하며, 클라이언트의 요청이 있을 때까지 대기
+- 요청이 들어올 경우 해당 요청을 RequestHandler 클래스에게 위임
+- 새로운 thread를 실행
+
+### 2. RequestHandler 클래스
+- WebServer 클래스로부터 전달받은 Socket을 이용하여 클라이언트의 요청을 처리
+- InputStream:
+  - 클라이언트에서 서버로 요청을 보낼 때 전달되는 데이터를 담당하는 스트림
+- OutputStream:
+  - 서버에서 클라이언트로 응답을 보낼 때 전달하는 데이터를 담당하는 스트림
+### 3. 구현 방법
+- BufferedReader를 이용하여 Request header를 읽어옴
+- split 함수를 이용하여 공백을 기준으로 request line 쪼개기
+- path에 해당하는 파일 데이터를 byte 배열로 읽어 반환
+### 4. 기타 사항
+Q. 요청을 한 번만 보냈음에도 추가 요청이 들어오는 이유는?<br>
+A. 서버가 웹 페이지를 구성하는 모든 리소스를 한번의 응답으로 보내지 않기 때문<br>
+-> 다른 자원이 포함되어 있을 경우 서버에 해당 자원을 다시 요청<br><br>
+Q. 요청을 잘 받았음에도 html을 반환하면 해당하는 css가 반영이 안되는 이유는?<br>
+A. response200Header 함수에서 writeBytes 할 때 파일 종류에 따라 content type을 다르게 지정해주어야함.
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,18 +23,78 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello! I'm Bogyeong Jang:)".getBytes();
-            response200Header(dos, body.length);
+            // 헤더 값 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            // 요청 라인 읽어오기
+            String line = br.readLine();
+            if(line == null)
+                return;
+            // 요청 URL 추출
+            String[] tokens = line.split(" ");
+            logger.debug("request: {}", line);
+            // header 전부 출력
+            while(!line.equals("")) {
+                line = br.readLine();
+                logger.debug("header : {}", line);
+            }
+            // 해당 파일을 읽고 응답
+            String path = "./src/main/resources";
+            String type = "html";
+            // 1. html일 경우
+            if(tokens[1].contains(".html")) {
+                path += "/templates" + tokens[1];
+            }
+            // 2. css일 경우
+            else if(tokens[1].contains(".css")) {
+                path += "/static" + tokens[1];
+                type = "css";
+            }
+            // 3. fonts일 경우
+            else if(tokens[1].contains("halflings-regular")) {
+                path += "/static" + tokens[1];
+                type = "fonts";
+            }
+            // 4. images일 경우
+            else if(tokens[1].contains(".png")) {
+                path += "/static" + tokens[1];
+                type = "images";
+            }
+            // 5. js일 경우
+            else if(tokens[1].contains(".js")) {
+                path += "/static" + tokens[1];
+                type = "js";
+            }
+            // 6. .ico일 경우
+            else {
+                path += "/static" + tokens[1];
+                type = "ico";
+            }
+
+            logger.debug("path: {}", path);
+            byte[] body = Files.readAllBytes(new File(path).toPath());
+
+            response200Header(dos, body.length, type);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String type) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            if(type.equals("html"))
+                dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            else if(type.equals("css"))
+                dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+            else if(type.equals("fonts"))
+                dos.writeBytes("Content-Type: text/fonts;charset=utf-8\r\n");
+            else if(type.equals("images"))
+                dos.writeBytes("Content-Type: text/png;charset=utf-8\r\n");
+            else if(type.equals("js"))
+                dos.writeBytes("Content-Type: text/js;charset=utf-8\r\n");
+            else
+                dos.writeBytes("Content-Type: text/ico;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
