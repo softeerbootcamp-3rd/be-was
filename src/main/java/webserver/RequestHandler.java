011b5f2f41dc843988a66e328e140f9a655680c8
admin <admin@admin-MacBook-Pro-3.local>
2024-01-15 17:24:42 +0900

    step-1 정적인 html 파일 응답 & HTTP Request 내용 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +24,23 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            logger.debug("Request line : {}", line);
+
+            String[] tokens = line.split(" ");
+
+            while(!line.equals("")) { // 헤더의 끝은 빈 공백 문자열이 들어있다.
+                line = br.readLine();
+                logger.debug("Header : {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            String url = tokens[1];
+
+            byte[] body = Files.readAllBytes(Paths.get("/Users/admin/Softeer/be-was/src/main/resources/templates" + url));
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
