1b5863cf8bb80e074ef87e1c9aa87c37fed3df83
eyeben <qkrehdals97@naver.com>
2024-01-15 18:17:32 +0900

    스레드 개수 제한 12 -> 2000

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(12);
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(2000);
 
     public static void main(String args[]) throws Exception {
         int port = 0;
