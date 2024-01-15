c400c964cc97448adfd6850be6a87a3bf0027ff9
admin <admin@adminui-MacBookPro-26.local>
2024-01-15 17:10:50 +0900

    feat:회원가입 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,17 +25,36 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 
+            //경로 추출
             String line = br.readLine();
             String[] tokens = line.split(" ");
             String path = tokens[1];
-            logger.debug("reqeust url {}",path);
-            //request 전부 읽어오기
-            //마지막엔 빈 공백 문자열이 있음
-            /*
+
+            logger.debug("reqeust url:{}",path);
+            logger.debug("--header--",path);
+            logger.debug("header:{}",line);
             while(!line.equals("")){
                 line = br.readLine();
-                logger.debug("header : " + line);
-            }*/
+                logger.debug("header:{}",line);
+            }
+            logger.debug("---------",path);
+
+
+            //회원 정보 추출
+            if(path.contains("/create")){
+                String temp = path.split("\\?")[1];
+                String[] temp2 = temp.split("&");
+
+                String[] imformation = new String[4];
+
+                for(int i=0; i< temp2.length; i++){
+                    imformation[i] = temp2[i].split("=")[1];
+                }
+                User user = new User(imformation[0],imformation[1],imformation[2],imformation[3]);
+                logger.debug("user : {} ", user);
+
+                path = "/index.html";
+            }
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
