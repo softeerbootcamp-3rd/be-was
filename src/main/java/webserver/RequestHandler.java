8cefd2997faaeb30cfe65bbfb63bd325f5f5bc6f
Hymin2 <khm8246@gmail.com>
2024-01-15 17:12:26 +0900

    feat: 사용자가 입력한 URI를 parser하는 함수 및 해당 경로에 .html 파일을 담은 response 객체 반환하는 함수 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,18 +1,21 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.StringTokenizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
+    private final URL RESOURCES_URL = this.getClass().getClassLoader().getResource("./templates");
     private Socket connection;
+
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -25,9 +28,10 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            String uri = parseUri(in);
+
+            setResponse(dos, uri);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -51,5 +55,37 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private void setResponse(DataOutputStream dos, String uri) throws IOException {
+        try{
+            byte[] body = Files.readAllBytes(new File(RESOURCES_URL.getPath() + uri).toPath());
+
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e){
+            logger.error(e.getMessage());
+        }
+    }
+
+    private String parseUri(InputStream in) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String requestLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(requestLine, " ");
+
+        String method = st.nextToken();
+        String uri = st.nextToken();
+        String httpVersion = st.nextToken();
+
+        logger.debug("----------------------------------------------------------------------------------------------");
+        logger.debug(requestLine);
+
+        while(!(requestLine = br.readLine()).isEmpty()){
+            logger.debug(requestLine);
+        }
+        logger.debug("----------------------------------------------------------------------------------------------");
+
+        return uri;
     }
 }
