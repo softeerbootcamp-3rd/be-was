b850e0a372012596aa8a07b8d4c0f686dd304200
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-15 17:04:34 +0900

    웹 서버 1단계 - index.html 기능구현 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,12 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,16 +22,40 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader inBufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            List<String> header = getHeaderRequestInfo(inBufferedReader);
+            Optional<String> path = getPath(header);
+            if(path.isPresent() && path.get().equals("/index.html")){
+                byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + path.get()).toPath());
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private Optional<String> getPath(List<String> header) {
+        if(header.isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.ofNullable(header.get(0).split(" ")[1]);
+    }
+
+    private List<String> getHeaderRequestInfo(BufferedReader inBufferedReader) throws IOException {
+        List<String> header = new ArrayList<>();
+        String temp;
+        if(inBufferedReader.ready()){
+            while (!(temp = inBufferedReader.readLine()).isEmpty()){
+                header.add(temp);
+            }
+        }
+        header.forEach(logger::debug);
+        logger.debug("----------------------구분선----------------------");
+        return header;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
