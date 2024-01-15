f6b2f8971a19530db2b0dac3060d9ae7fecabcc6
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-15 16:30:37 +0900

    feat : create step1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,13 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
+import dto.RequestDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +15,10 @@ public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
-
+    private RequestDto req;
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        this.req = new RequestDto();
     }
 
     public void run() {
@@ -24,19 +27,25 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            getRequest(in);
+            req.requestInfo();
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+
+            String type = req.getType();
+            String parentDir = getDir(type);
+            byte[] body = Files.readAllBytes(new File(parentDir+req.getUrl()).toPath());
+
+            response200Header(dos, body.length,type);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String type) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: text/"+type+";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -51,5 +60,26 @@ public class RequestHandler implements Runnable {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+
+
+    private String getDir(String fileExtension){
+        if(fileExtension.equals("html")){
+            return Paths.get(System.getProperty("user.dir"), "src/main/resources/templates").toString();
+        }
+        else{
+            return Paths.get(System.getProperty("user.dir"), "src/main/resources/static").toString();
+        }
+    }
+
+    private void getRequest(InputStream inputStream) throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        //읽어온 값을 req에 추가하기
+        String[] line = bufferedReader.readLine().split(" ");
+        req.setMethod(line[0]);
+        req.setUrl(line[1]);
     }
 }
