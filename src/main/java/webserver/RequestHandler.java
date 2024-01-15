8dff7fc65dc668fdde59f31094bbd9eeb0ea898d
LSH <104637774+lsh2613@users.noreply.github.com>
2024-01-15 17:03:33 +0900

    [Feat] Step1 구현 (#24)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,10 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +13,8 @@ public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    static final String DEFAULT_STATIC_PATH = "./src/main/resources/templates";
+
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,13 +26,42 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            String request = inToString(in);
+            String path = request.split(" ")[1];
+
+            printRequestInfo(request);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            byte[] body = Files.readAllBytes(new File(DEFAULT_STATIC_PATH + path).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String inToString(InputStream inputStream) throws IOException {
+        // Read the request headers
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            requestBuilder.append(line).append("\r\n");
+        }
+
+        return requestBuilder.toString();
+    }
+
+    private void printRequestInfo(String request) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(request));
+
+        logger.debug("============== Request의 내용 출력 ==============");
+        String line = "";
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            logger.debug(line);
+        }
+        logger.debug("===============================================");
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
@@ -52,4 +83,6 @@ public class RequestHandler implements Runnable {
             logger.error(e.getMessage());
         }
     }
+
 }
+
