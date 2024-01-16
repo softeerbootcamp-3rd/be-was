27cb85f837bca022c301a9727ca6d003bb137420
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-16 15:01:11 +0900

    Refactor : 중복되지 않는 HttpHeader제거

--- src/main/java/model/http/RequestHeaders.java
+++ src/main/java/model/http/RequestHeaders.java
@@ -1,32 +1,18 @@
 package model.http;
 
+import java.util.HashMap;
+
 public class RequestHeaders {
     private final String host;
     private final String userAgent;
     private final String accept;
-    private final String connection;
-    private final ContentType contentType;
-    private final String charSet;
-    private final Integer contentLength;
+    private final HashMap<String, String> etc;
 
-    public RequestHeaders(String host, String userAgent, String accept, String connection, ContentType contentType, Integer contentLength) {
+    public RequestHeaders(String host, String userAgent, String accept, HashMap<String, String> etc) {
         this.host = host;
         this.userAgent = userAgent;
         this.accept = accept;
-        this.connection = connection;
-        this.contentType = contentType;
-        this.contentLength = contentLength;
-        this.charSet="utf-8";
-    }
-
-    public RequestHeaders(String host, String userAgent, String accept, String connection, ContentType contentType, String charset, Integer contentLength) {
-        this.host = host;
-        this.userAgent = userAgent;
-        this.accept = accept;
-        this.connection = connection;
-        this.contentType = contentType;
-        this.charSet = charset;
-        this.contentLength = contentLength;
+        this.etc = etc;
     }
 
     public String getHost() {
@@ -41,15 +27,17 @@ public class RequestHeaders {
         return accept;
     }
 
-    public String getConnection() {
-        return connection;
+    public HashMap<String, String> getEtc() {
+        return etc;
     }
 
-    public String getContentType() {
-        return contentType + ";" + charSet;
-    }
-
-    public Integer getContentLength() {
-        return contentLength;
+    @Override
+    public String toString() {
+        return "{" +
+                "host='" + host + '\n' +
+                ", userAgent='" + userAgent + '\n' +
+                ", accept='" + accept + '\n' +
+                ", etc=" + etc +
+                '}' + "\n";
     }
 }
