02ccfb58e126a3a5582b568fdfbf19d529e7901b
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-16 18:04:47 +0900

    feat : add member create in controller

--- src/main/java/controller/MemberFormController.java
+++ src/main/java/controller/MemberFormController.java
@@ -1,12 +1,35 @@
 package controller;
 
+import dto.UserDto;
 import frontController.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+import webserver.RequestHandler;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.Map;
 
 public class MemberFormController implements Controller{
+    private final UserService userService = UserService.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
     @Override
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
-        return "redirect:/login.html";
+        try{
+            UserDto dto = new UserDto();
+
+            dto.setEmail(URLDecoder.decode(paramMap.get("email"), "UTF-8"));
+            dto.setUserId(URLDecoder.decode(paramMap.get("userId"), "UTF-8"));
+            dto.setName(URLDecoder.decode(paramMap.get("name"), "UTF-8"));
+            dto.setPassword(URLDecoder.decode(paramMap.get("password"), "UTF-8"));
+            userService.signUp(dto);
+        }
+        catch (Exception e) {
+            logger.error(e.getMessage());
+            return "/user/form.html";
+        }
+        return "redirect:/user/login.html";
     }
 }
