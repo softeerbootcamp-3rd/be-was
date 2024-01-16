38b436f74acea345f38fa633e4905164476405c1
eekrwl <wani9909@naver.com>
2024-01-16 14:40:19 +0900

    Feat: 스레드풀 newCachedThreadPool로 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,9 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        // 스레드 풀 생성 (CachedThreadPool 사용)
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -25,9 +30,12 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                // 클라이언트 연결마다 스레드 풀을 이용하여 RequestHandler 실행
+                executorService.submit(new RequestHandler(connection));
             }
+        } finally {
+            // 프로그램이 종료될 때 스레드 풀을 종료
+            executorService.shutdown();
         }
     }
 }
