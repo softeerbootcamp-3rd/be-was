7364ad172c881e5884a2986e81559af76b1749db
h-sooah <s2sooas2@ewhain.net>
2024-01-16 12:03:11 +0900

    스레드풀 종료문 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -30,10 +30,11 @@ public class WebServer {
             ExecutorService executorService = Executors.newCachedThreadPool();
 
             while ((connection = listenSocket.accept()) != null) {
-
                 CompletableFuture.runAsync(new RequestHandler(connection), executorService);
-
             }
+
+            // 스레드풀 종료
+            executorService.shutdown();
         }
     }
 }
