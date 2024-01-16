209e0b3716764a43d8146d465a963b52abec4bb6
seonghwan <aiya0422@gmail.com>
2024-01-16 22:29:04 +0900

    홈 서비스 객체 제거
    단순 파일 읽기의 기능이기 때문에 컨트롤러에서 수행해도 된다고 판단했습니다.

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,26 +1,40 @@
 package controller;
 
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import service.HomeService;
+import java.nio.file.Files;
+import utils.ResponseBuilder;
 
 public class HomeController {
 
-    private static final HomeService homeService = new HomeService();
 
     public void route(String urn, OutputStream out) throws IOException {
         if (urn.equals("/") || urn.equals("/index.html")) {
-            home(urn, out);
+            getIndex(urn, out);
         } else {
             getStatic(urn, out);
         }
     }
 
-    private void home(String urn, OutputStream out) throws IOException {
-        homeService.getIndex(urn, out);
+    private void getIndex(String urn, OutputStream out) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+
+        String filePath = "src/main/resources/templates" + urn;
+
+        byte[] body = Files.readAllBytes(new File(filePath).toPath());
+        ResponseBuilder.response200Header(dos, body.length, urn);
+        ResponseBuilder.responseBody(dos, body);
     }
 
     private void getStatic(String urn, OutputStream out) throws IOException {
-        homeService.getStatic(urn, out);
+        DataOutputStream dos = new DataOutputStream(out);
+
+        String filePath = "src/main/resources/static" + urn;
+
+        byte[] body = Files.readAllBytes(new File(filePath).toPath());
+        ResponseBuilder.response200Header(dos, body.length, urn);
+        ResponseBuilder.responseBody(dos, body);
     }
 }
