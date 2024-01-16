2f8a0312e757c81ca1429783812207a5735bb0d3
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-16 16:17:07 +0900

    Feat : Body 클래스 getter 추가

--- src/main/java/model/http/Body.java
+++ src/main/java/model/http/Body.java
@@ -3,6 +3,10 @@ package model.http;
 public class Body {
     private String content;
 
+    public Body(String content) {
+        this.content = content;
+    }
+
     public String getContent() {
         return content;
     }
