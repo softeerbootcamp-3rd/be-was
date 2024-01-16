f740878eb50df4e008644c330537c502a7566e24
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-16 18:05:11 +0900

    feat : add instance to use database singletone

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -5,16 +5,22 @@ import com.google.common.collect.Maps;
 import model.User;
 
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 
 public class Database {
+    private static final Database instance = new Database();
     private static Map<String, User> users = Maps.newHashMap();
+
+    public static Database getInstance() {
+        return instance;
+    }
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
 
-    public static User findUserById(String userId) {
+    public static User findUserById(Long userId) {
         return users.get(userId);
     }
 
