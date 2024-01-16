064d2c1a7ca35d82ae9ab8dc1ba1efab42b6f7cc
LEE, TAEGON <taegon1998@gmail.com>
2024-01-16 20:31:51 +0900

    feat: URI에 따라 routing을 담당하는 메소드 구현

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import request.SignUpRequest;
+import util.SingletonUtil;
+
+import static request.SignUpRequest.parseSignUpRequest;
+
+
+public class UserController {
+
+    public String route(String path) {
+        if (path.startsWith("/user/create")) {
+            return "302 /index.html"; // index.html로 리다이렉트
+        }
+        else {
+            return "200 " + path; // 정적 파일 응답
+        }
+    }
+}
