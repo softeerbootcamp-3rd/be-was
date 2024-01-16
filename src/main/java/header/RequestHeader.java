fc9b245339b692d42a134a4e6b0bfd51397209ad
Hymin2 <khm8246@gmail.com>
2024-01-16 15:09:46 +0900

    refactor: RequestHeader 내의 parse 함수를 제거 및 RequestHeaderParser 객체 생성
    RequestHeader 클래스의 헤더를 파싱하는 메소드를 제거하고, RequestHeader의 내용을 파싱하는 객체 RequestHeaderParser를 생성하여 역할을 분담하도록 하였다.

--- src/main/java/header/RequestHeader.java
+++ src/main/java/header/RequestHeader.java
@@ -9,8 +9,6 @@ import java.lang.reflect.Field;
 import java.util.StringTokenizer;
 
 public class RequestHeader {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHeader.class);
-
     private String method;
     private String path;
     private String protocol;
@@ -38,42 +36,6 @@ public class RequestHeader {
 
     public static RequestHeader of(String method, String path, String Host){
         return new RequestHeader(method, path, Host);
-    }
-
-    public static RequestHeader parseHeader(BufferedReader br) throws IOException {
-        String requestLine = br.readLine();
-        StringTokenizer st = new StringTokenizer(requestLine, " ");
-
-        String method = st.nextToken();
-        String path = st.nextToken();
-        String protocol = st.nextToken();
-
-        RequestHeader requestHeader = of(method, path, protocol);
-
-        while(!(requestLine = br.readLine()).isEmpty()){
-            String[] requestLineSplit = requestLine.split(": ");
-
-            String headerName = requestLineSplit[0].replace("-", "");
-            String headerValue = requestLineSplit[1];
-
-            setHeaderValue(requestHeader, headerName, headerValue);
-        }
-
-        return requestHeader;
-    }
-
-    private static void setHeaderValue(RequestHeader requestHeader, String fieldName, String fieldValue){
-        Class<RequestHeader> requestHeaderClass = RequestHeader.class;
-
-        try{
-            Field field = requestHeaderClass.getDeclaredField(fieldName);
-            field.setAccessible(true);
-
-            field.set(requestHeader, fieldValue);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            logger.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
---
+++ src/main/java/parser/RequestHeaderParser.java
@@ -0,0 +1,61 @@
+package parser;
+
+import header.RequestHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.util.StringTokenizer;
+
+public class RequestHeaderParser {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHeaderParser.class);
+
+    public static RequestHeader parse(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        RequestHeader requestHeader = parseHeader(br);
+
+        logger.debug(requestHeader.toString());
+
+        return requestHeader;
+    }
+
+    private static RequestHeader parseHeader(BufferedReader br) throws IOException {
+        String requestLine = br.readLine();
+        StringTokenizer st = new StringTokenizer(requestLine, " ");
+
+        String method = st.nextToken();
+        String path = st.nextToken();
+        String protocol = st.nextToken();
+
+        RequestHeader requestHeader = RequestHeader.of(method, path, protocol);
+
+        while(!(requestLine = br.readLine()).isEmpty()){
+            String[] requestLineSplit = requestLine.split(": ");
+
+            String headerName = requestLineSplit[0].replace("-", "");
+            String headerValue = requestLineSplit[1];
+
+            setHeaderValue(requestHeader, headerName, headerValue);
+        }
+
+        return requestHeader;
+    }
+
+    private static void setHeaderValue(RequestHeader requestHeader, String fieldName, String fieldValue){
+        Class<RequestHeader> requestHeaderClass = RequestHeader.class;
+
+        try{
+            Field field = requestHeaderClass.getDeclaredField(fieldName);
+            field.setAccessible(true);
+
+            field.set(requestHeader, fieldValue);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
