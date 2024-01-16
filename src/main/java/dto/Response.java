30002a67288694420af3884e481b89a52b6c5b40
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-16 16:53:36 +0900

    feat : create Response
    Response를 처리하기 위한 객체 입니다.
    현재 redirect를 위해 필요한 location과 status 속성 만을 갖고 있으며 향후 추가할 예정입니다.
    send를 통해 외부에서 호출시 현재 response의 상태를 기반으로 응답을 생성하도록 구현하였습니다.

---
+++ src/main/java/dto/Response.java
@@ -0,0 +1,60 @@
+package dto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Response {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private String location;
+
+    private HttpStatus status;
+    public Response(){
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public void send(DataOutputStream dos,byte[] body, Request req){
+        setResponseHeader(dos,body.length,req);
+        setResponseBody(dos,body);
+    }
+    private void setResponseHeader(DataOutputStream dos, int lengthOfBodyContent, Request req) {
+        try {
+            dos.writeBytes("HTTP/1.1 " + status +"\r\n");
+            dos.writeBytes("Content-Type: "+req.getContentType()+";charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("Location : " + location + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void setResponseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
