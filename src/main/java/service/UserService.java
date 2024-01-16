d87df3508fb794525d7883f1e1283b3f212bec67
LEE, TAEGON <taegon1998@gmail.com>
2024-01-16 23:39:38 +0900

    refact: 회원가입되는 유저 정보 및 전체 유저 수 log 출력

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,13 +1,24 @@
 package service;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import request.SignUpRequest;
-import util.SingletonUtil;
+import webserver.RequestHandler;
 
+import static util.SingletonUtil.getDatabase;
 
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
     public void signUp(SignUpRequest signUpRequest) {
-        User user = new User(signUpRequest.getUserId(), signUpRequest.getPassword(), signUpRequest.getName(), signUpRequest.getEmail());
-        SingletonUtil.getDatabase().addUser(user);
+        User user = new User(signUpRequest); // 회원정보 객체 생성
+        getDatabase().addUser(user); // 데이터베이스에 회원정보 저장
+
+        // 데이터베이스에 정상적으로 저장되었는지 확인하기 위해 로그를 출력
+        logger.debug("Total User Count : {}\n", getDatabase().getUserCount());
+        logger.debug("UserId : {}, Password : {}, Name : {}, Email : {}\n",
+                signUpRequest.getUserId(), signUpRequest.getPassword(), signUpRequest.getName(), signUpRequest.getEmail());
     }
 }
