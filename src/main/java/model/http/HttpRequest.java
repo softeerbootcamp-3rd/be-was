88f802c676c6b8ee0d93cda7024857fd0f73fe49
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-16 15:02:42 +0900

    Refactor : HttpRequest, HttpResponse 내용 수정

--- src/main/java/model/http/HttpRequest.java
+++ src/main/java/model/http/HttpRequest.java
@@ -4,4 +4,31 @@ public class HttpRequest {
     private StartLine startLine;
     private RequestHeaders headers;
     private Body body;
+
+    public HttpRequest(StartLine startLine, RequestHeaders headers, Body body) {
+        this.startLine = startLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public StartLine getStartLine() {
+        return startLine;
+    }
+
+    public RequestHeaders getHeaders() {
+        return headers;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "\nHttpRequest{" +
+                "startLine=" + startLine +
+                ", headers=" + headers +
+                ", body=" + body +
+                '}';
+    }
 }
--- src/main/java/model/http/HttpResponse.java
+++ src/main/java/model/http/HttpResponse.java
@@ -4,4 +4,22 @@ public class HttpResponse {
     private StatusLine statusLine;
     private ResponseHeaders headers;
     private Body body;
+
+    public HttpResponse(StatusLine statusLine, ResponseHeaders headers, Body body) {
+        this.statusLine = statusLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public StatusLine getStatusLine() {
+        return statusLine;
+    }
+
+    public ResponseHeaders getHeaders() {
+        return headers;
+    }
+
+    public Body getBody() {
+        return body;
+    }
 }
