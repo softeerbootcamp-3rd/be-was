9b387e32954d16948604cde8e94c05fa95783d51
seojeewon <readyjiwon@gmail.com>
2024-01-16 16:13:11 +0900

    Fix: read html file, add RequestParser
    html file을 읽어오는 방법을 Files.readAllBytes를 사용하도록 바꿨습니다.
    또한 getRequestUrl로 Path를 가져오던 방식을 RequestParser를 이용하여
    Path도 가져올 뿐만 아니라 Request에 대한 로그도 찍을 수 있게 바꿨습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,32 +3,27 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.imageio.IIOException;
+import utils.RequestParser;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
-
-    //url 가져오는 함수
-    //HTTP Message - GET /index.html HTTP/1.1
-    private String getRequestUrl(InputStream in) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String requestLine = reader.readLine();
-        logger.info(requestLine);
-        String[] requestParts = requestLine.split(" ");
-        return requestParts.length > 1 ? requestParts[1] : "";
+    private void logRequest(InputStream in) throws IOException {
+        RequestParser.setHeaders(in);
+        logger.debug("HEADER> METHOD: {}", RequestParser.getMethod());
+        logger.debug("HEADER> HOST: {}", RequestParser.getHost());
+        logger.debug("HEADER> PATH: {}", RequestParser.getPath());
+        logger.debug("HEADER> ACCEPT: {}", RequestParser.getAccept());
+        logger.debug("HEADER> USER-AGENT: {}", RequestParser.getUserAgent());
+        logger.debug("HEADER> COOKIE: {}", RequestParser.getCookie());
     }
 
     public void run() {
@@ -38,21 +33,11 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
+            logRequest(in); // Request log
 
-            String requestUrl = getRequestUrl(in);
-            logger.debug("requested url: {}", requestUrl);
-
-            if ("/index.html".equals(requestUrl)) { //정적인 html파일 응답
-                byte[] body = Files.readAllBytes(Paths.get("src/main/resources/templates/index.html"));
-                response200Header(dos, body.length);
-                responseBody(dos, body);
-            }
-
-            else {
-                byte[] body = "Hello World".getBytes();
-                response200Header(dos, body.length);    //200 상태코드 보내기
-                responseBody(dos, body);
-            }
+            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + RequestParser.getPath()).toPath());
+            response200Header(dos, body.length);
+            responseBody(dos, body);
 
         } catch (IOException e) {
             logger.error(e.getMessage());
