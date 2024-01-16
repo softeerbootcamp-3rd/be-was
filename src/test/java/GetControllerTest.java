c3860b955af6627e79725c4f189ee5efb136ce93
h-sooah <s2sooas2@ewhain.net>
2024-01-16 10:52:10 +0900

    [Step-2] GET으로 회원가입, Junit 테스트, Request 파싱

---
+++ src/test/java/GetControllerTest.java
@@ -0,0 +1,26 @@
+import db.Database;
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class GetControllerTest {
+
+    @Test
+    public void signupTest() {
+
+        // given : 테스트에서 구체화하고자 하는 행동을 하기 전 테스트의 상태를 설명하는 부분
+        // [ 유저 생성 ] 이 주어졌을 때
+        User user1 = new User("softeer", "bootcamp", "softeer", "abc@abc.com");
+
+        // when : 구체화하고자 하는 행동
+        // [ 유저를 DB에 저장한 후, 유저를 조회 ] 했을 때
+        Database.addUser(user1);
+        User findUser = Database.findUserById("softeer");
+
+        // then : 예상되는 변화를 확인, 검증
+        // [ 아래 조건을 만족 ] 해야 한다.
+        Assertions.assertThat(user1).isEqualTo(findUser);
+
+    }
+
+}
