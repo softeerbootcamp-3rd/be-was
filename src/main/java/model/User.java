4af623c41cf4b8cd4eed5c1d1b3fc8c3f87a54a6
Hymin2 <khm8246@gmail.com>
2024-01-16 14:52:16 +0900

    feat: User 객체에 Builder 추가
    User 객체의 인스턴스 생성을 쉽게 하기 위해 User 객체에 Builder를 추가하였습니다.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -13,6 +13,13 @@ public class User {
         this.email = email;
     }
 
+    private User(Builder builder){
+        this.userId = builder.userId;
+        this.password = builder.password;
+        this.name = builder.name;
+        this.email = builder.email;
+    }
+
     public String getUserId() {
         return userId;
     }
@@ -32,5 +39,42 @@ public class User {
     @Override
     public String toString() {
         return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+    }
+
+    public static class Builder{
+        private String userId;
+        private String password;
+        private String name;
+        private String email;
+
+        public Builder(){}
+
+        public Builder userId(String userId){
+            this.userId = userId;
+
+            return this;
+        }
+
+        public Builder password(String password){
+            this.password = password;
+
+            return this;
+        }
+
+        public Builder name(String name){
+            this.name = name;
+
+            return this;
+        }
+
+        public Builder email(String email){
+            this.email = email;
+
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
     }
 }
