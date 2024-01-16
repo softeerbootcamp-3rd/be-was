ab54ee81e8d9317c599022294534871ebdf50abd
Hymin2 <khm8246@gmail.com>
2024-01-16 18:00:09 +0900

    feat: UserController 및 UserService에 logger 추가
    각 클래스에서 createUser()가 실행되었을 때 로그를 출력하도록 수정하였습니다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,11 +1,17 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import service.UserService;
+import webserver.RequestHandler;
 
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final UserService userService = new UserService();
 
     public void createUser(String userId, String password, String name, String email){
+        logger.debug("createUser() 실행");
+
         userService.createUser(userId, password, name, email);
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,8 +2,13 @@ package service;
 
 import db.Database;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
 
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
     public void createUser(String userId, String password, String name, String email){
         User user = new User.Builder()
                 .userId(userId)
@@ -13,5 +18,7 @@ public class UserService {
                 .build();
 
         Database.addUser(user);
+        logger.debug("User 생성 완료");
+        logger.debug(user.toString());
     }
 }
