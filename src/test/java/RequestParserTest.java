c742cfe3515ee29a8e36ab2a713c89481c7cdbcd
seojeewon <readyjiwon@gmail.com>
2024-01-16 17:42:48 +0900

    Test: RequestParserTest using JUnit5
    Request Message를 파싱하는 유틸클래스인 RequestParser의 메서드에 대한
    단위 테스트를 진행합니다.
    6개의 함수가 잘 파싱하는지 테스트하고, 유효하지 않은 request message에는
    예외 테스트를 진행했습니다.

---
+++ src/test/java/RequestParserTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import utils.RequestParser;
+import org.assertj.core.api.Assertions;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class RequestParserTest {
+
+
+    @BeforeAll
+    static void init() throws IOException {
+        String validRequest = "GET /index.html HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Cache-Control: max-age=0\n" +
+                "sec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"\n" +
+                "sec-ch-ua-mobile: ?0\n" +
+                "sec-ch-ua-platform: \"macOS\"\n" +
+                "Upgrade-Insecure-Requests: 1\n" +
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\n" +
+                "Sec-Fetch-Site: same-origin\n" +
+                "Sec-Fetch-Mode: navigate\n" +
+                "Sec-Fetch-User: ?1\n" +
+                "Sec-Fetch-Dest: document\n" +
+                "Referer: http://localhost:8080/user/login.html\n" +
+                "Accept-Encoding: gzip, deflate, br\n" +
+                "Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7\n" +
+                "Cookie: Idea-69015365=c84b7ca8-76bc-4946-8364-2ed31c35b3d4";
+
+        InputStream validRequestStream = new ByteArrayInputStream(validRequest.getBytes());
+        RequestParser.setHeaders(validRequestStream);
+    }
+
+    void makeInvalidRequest() throws IOException {
+        String invalidRequest = "";
+        InputStream invalidRequestStream = new ByteArrayInputStream(invalidRequest.getBytes());
+        RequestParser.setHeaders(invalidRequestStream);
+    }
+
+    @Test
+    void MethodParsing(){
+        assertThat("GET").isEqualTo(RequestParser.getMethod());
+    }
+
+    @Test
+    void HostParsing(){
+        assertThat("localhost:8080").isEqualTo(RequestParser.getHost());
+    }
+
+    @Test
+    void PathParsing(){
+        assertThat("/index.html").isEqualTo(RequestParser.getPath());
+    }
+
+    @Test
+    void AcceptParsing(){
+        assertThat("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7").isEqualTo(RequestParser.getAccept());
+    }
+
+    @Test
+    void UserAgentParsing(){
+        assertThat("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36").isEqualTo(RequestParser.getUserAgent());
+    }
+
+    @Test
+    void CookieParsing(){
+        assertThat("Idea-69015365=c84b7ca8-76bc-4946-8364-2ed31c35b3d4").isEqualTo(RequestParser.getCookie());
+    }
+
+    @Test
+    void Should_ThrowException_When_No_Request() throws IOException {
+        assertThatThrownBy(this::makeInvalidRequest);
+    }
+
+}
