1714f038803602e551aa1d009332a64c79d27a8f
99winnmin <ojysep9987@gmail.com>
2024-01-16 11:32:39 +0900

    :art: Fix : HTTP Header Parser 부분 HttpProcessor 로 변경

--- src/main/java/logger/HttpRequestHeader.java
+++ src/main/java/logger/HttpRequestHeader.java
@@ -11,29 +11,8 @@ import java.util.Map;
 public class HttpRequestHeader {
     private Map<String, String> headers;
 
-    private HttpRequestHeader(HashMap<String, String> headers) {
+    public HttpRequestHeader(HashMap<String, String> headers) {
         this.headers = headers;
-    }
-
-    public static HttpRequestHeader httpRequestHeaderBuilder(InputStream in) {
-        HashMap<String, String> map = new LinkedHashMap<>();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            String line = bufferedReader.readLine();
-            String[] firstHeader = line.split(" ");
-            map.put("method", firstHeader[0]);
-            map.put("path", firstHeader[1]);
-            map.put("protocol", firstHeader[2]);
-            line = bufferedReader.readLine();
-            while(!line.isEmpty()) {
-                map.put(line.split(": ")[0], line.split(": ")[1]);
-                line = bufferedReader.readLine();
-            }
-        } catch (Exception e) {
-            CustomLogger.printError(e);
-        }
-
-        return new HttpRequestHeader(map);
     }
 
     public Map<String, String> getHeaders() {
--- src/main/java/webserver/HttpProcessor.java
+++ src/main/java/webserver/HttpProcessor.java
@@ -1,11 +1,16 @@
 package webserver;
 
 import handler.ResponseHandler;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import logger.CustomLogger;
 import logger.HttpRequestHeader;
 
@@ -20,7 +25,7 @@ public class HttpProcessor implements Runnable {
     public void run() {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // HTTP Request Header 읽기
-            HttpRequestHeader httpRequestHeader = HttpRequestHeader.httpRequestHeaderBuilder(in);
+            HttpRequestHeader httpRequestHeader = httpRequestHeaderParser(in);
 
             // HTTP Request Header 및 IP,Port 출력하기
             CustomLogger.printIPAndPort(connection);
@@ -35,5 +40,26 @@ public class HttpProcessor implements Runnable {
         } catch (IOException e) {
             CustomLogger.printError(e);
         }
+    }
+
+    private HttpRequestHeader httpRequestHeaderParser(InputStream in) {
+        HashMap<String, String> map = new LinkedHashMap<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            String line = bufferedReader.readLine();
+            String[] firstHeader = line.split(" ");
+            map.put("method", firstHeader[0]);
+            map.put("path", firstHeader[1]);
+            map.put("protocol", firstHeader[2]);
+            line = bufferedReader.readLine();
+            while(!line.isEmpty()) {
+                map.put(line.split(": ")[0], line.split(": ")[1]);
+                line = bufferedReader.readLine();
+            }
+        } catch (Exception e) {
+            CustomLogger.printError(e);
+        }
+
+        return new HttpRequestHeader(map);
     }
 }
