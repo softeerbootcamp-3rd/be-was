cc3de89b1dd2512d972f575cd7cd65f737ce243b
LEE, TAEGON <taegon1998@gmail.com>
2024-01-16 20:32:15 +0900

    feat: 회원가입 컨트롤러 기능 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -10,10 +10,16 @@ public class UserController {
 
     public String route(String path) {
         if (path.startsWith("/user/create")) {
+            signUp(path);
             return "302 /index.html"; // index.html로 리다이렉트
         }
         else {
             return "200 " + path; // 정적 파일 응답
         }
+    }
+
+    private void signUp(String request) {
+        SignUpRequest signUpRequest = parseSignUpRequest(request);
+        SingletonUtil.getUserService().signUp(signUpRequest);
     }
 }
