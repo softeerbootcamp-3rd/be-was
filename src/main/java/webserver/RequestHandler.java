edbccc86cf8aa23df765000ecef026c449aac7a8
jbonnie <bonnie8623@gmail.com>
2024-01-16 10:34:34 +0900

    GET을 이용한 회원가입 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,6 +33,7 @@ public class RequestHandler implements Runnable {
             // 요청 URL 추출
             String[] tokens = line.split(" ");
             String url = tokens[1];
+
             String accept = "";
             logger.debug("HTTP Method: {}, Request Target: {}", tokens[0], url);
             // host, accept 출력
@@ -47,20 +49,38 @@ public class RequestHandler implements Runnable {
                     logger.debug("Accept: {}", accept);
                 }
             }
-            // 해당 파일을 읽고 응답
-            String path = "./src/main/resources";
-            // 1. html일 경우
-            if(url.contains(".html")) {
-                path += "/templates" + url;
+
+            byte[] body = new byte[10];     // 초기화
+            // GET으로 회원가입 기능 구현
+            if(url.contains("/user/create?")) {
+                tokens = url.split("\\?");
+                String[] parameters = tokens[1].split("&");
+                String userId = parameters[0].substring(parameters[0].indexOf("=")+1);
+                String password = parameters[1].substring(parameters[1].indexOf("=")+1);
+                String name = parameters[2].substring(parameters[2].indexOf("=")+1);
+                String email = parameters[3].substring(parameters[3].indexOf("=")+1);
+                email = email.replace("%40", "@");
+                // User 객체에 저장
+                User user = new User(userId, password, name, email);
+                logger.debug(user.toString());
+                String welcome = "Hello, " + user.getName() + "!";      // 회원가입 성공 시 뜨는 문구
+                body = welcome.getBytes();
             }
-            // 2. css, fonts, images, js, ico일 경우
             else {
-                path += "/static" + url;
+                // 해당 파일을 읽고 응답
+                String path = "./src/main/resources";
+                // 1. html일 경우
+                if(url.contains(".html")) {
+                    path += "/templates" + url;
+                }
+                // 2. css, fonts, images, js, ico일 경우
+                else {
+                    path += "/static" + url;
+                }
+
+                logger.debug("path: {}", path);
+                body = Files.readAllBytes(new File(path).toPath());
             }
-
-            logger.debug("path: {}", path);
-            byte[] body = Files.readAllBytes(new File(path).toPath());
-
             response200Header(dos, body.length, accept);
             responseBody(dos, body);
         } catch (IOException e) {
