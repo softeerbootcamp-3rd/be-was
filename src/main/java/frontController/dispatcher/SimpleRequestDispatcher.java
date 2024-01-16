75c01da3c9048e09e8b7a0f46177d534a7f03ccb
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-16 16:55:49 +0900

    feat : create SimpleRequestDispatcher

---
+++ src/main/java/frontController/dispatcher/SimpleRequestDispatcher.java
@@ -0,0 +1,47 @@
+package frontController.dispatcher;
+
+import dto.HttpStatus;
+import dto.Request;
+import dto.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.DataOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SimpleRequestDispatcher implements RequestDispatcher{
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    @Override
+    public void forward(Request request, Response response, String viewPath,DataOutputStream dos) throws IOException {
+        logger.debug("[ start forwarding ]");
+        logger.debug("viewPath : "+viewPath);
+        byte[] body = Files.readAllBytes(new File(getAbsolutePath(viewPath)).toPath());
+        logger.debug("set body in forward");
+        response.send(dos,body,request);
+    }
+
+    @Override
+    public void sendRedirect(Request request, Response response, String viewPath, DataOutputStream dos) {
+        byte[] body = new byte[0];
+        response.setLocation(viewPath);
+        response.setStatus(HttpStatus.MOVED_PERMANENTLY);
+        logger.debug("[ response ]");
+        logger.debug("location : "+response.getLocation());
+        logger.debug("status : "+response.getStatus());
+        response.send(dos,body,request);
+    }
+
+    private String getAbsolutePath(String viewPath){
+        if(viewPath.endsWith(".html")){
+            return Paths.get(System.getProperty("user.dir"), "src/main/resources/templates").toString()+"/"+viewPath;
+        }
+        else{
+            return Paths.get(System.getProperty("user.dir"), "src/main/resources/static").toString() + "/" + viewPath;
+        }
+    }
+
+}
