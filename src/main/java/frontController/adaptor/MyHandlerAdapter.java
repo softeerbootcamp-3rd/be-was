dbe84d6bed08adca2f40115d18a5aca7f96315ee
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-16 16:55:15 +0900

    feat : create MyHandlerAdapter

---
+++ src/main/java/frontController/adaptor/MyHandlerAdapter.java
@@ -0,0 +1,32 @@
+package frontController.adaptor;
+
+import dto.Request;
+import dto.Response;
+import controller.Controller;
+import frontController.ModelAndView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyHandlerAdapter implements HandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof Controller);
+    }
+
+    @Override
+    public ModelAndView handle(Request req, Response res, Object handler) throws IOException {
+        Controller controller = (Controller) handler;
+
+        Map<String, String> paramMap = req.getRequestParam();
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap,model);
+
+        ModelAndView mv = new ModelAndView(viewName);
+        mv.setModel(model);
+
+        return mv;
+    }
+}
