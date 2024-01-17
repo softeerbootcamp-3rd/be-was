727dac1f1e0962744ebfdcc437880157a631f9e6
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-17 11:54:52 +0900

    feat : update parameters of function process

--- src/main/java/controller/StaticController.java
+++ src/main/java/controller/StaticController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import http.Request;
+
 import java.util.Map;
 
 public class StaticController implements Controller{
     @Override
-    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+    public String process(Request req, Map<String, Object> model) {
+        Map<String, String> paramMap = req.getRequestParam();
         return paramMap.get("static");
     }
 }
