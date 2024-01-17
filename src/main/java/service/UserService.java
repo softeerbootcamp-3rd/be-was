4c658d472fe6458927c004599e439d05fdc282f5
LEE, TAEGON <taegon1998@gmail.com>
2024-01-17 12:06:58 +0900

    feat: 회원 단건 조회 및 전체 조회 메소드 구현

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -6,12 +6,15 @@ import org.slf4j.LoggerFactory;
 import request.SignUpRequest;
 import webserver.RequestHandler;
 
+import java.util.Collection;
+
 import static util.SingletonUtil.getDatabase;
 
 public class UserService {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
+    // 회원가입 요청을 처리하는 메소드
     public void signUp(SignUpRequest signUpRequest) {
         User user = new User(signUpRequest); // 회원정보 객체 생성
         getDatabase().addUser(user); // 데이터베이스에 회원정보 저장
@@ -20,5 +23,15 @@ public class UserService {
         logger.debug("Total User Count : {}\n", getDatabase().getUserCount());
         logger.debug("UserId : {}, Password : {}, Name : {}, Email : {}\n",
                 signUpRequest.getUserId(), signUpRequest.getPassword(), signUpRequest.getName(), signUpRequest.getEmail());
+    }
+
+    // 회원 아이디를 전달받아서 데이터베이스에서 회원정보를 조회하는 메소드
+    public User findUserById(String userId) {
+        return getDatabase().findUserById(userId);
+    }
+
+    // 데이터베이스에 저장된 모든 회원정보를 조회하는 메소드
+    public Collection<User> findAll() {
+        return getDatabase().findAll();
     }
 }
