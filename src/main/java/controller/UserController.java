27ec4f3b830d0aef64db69d6e396caacd8a77e8a
Hymin2 <khm8246@gmail.com>
2024-01-17 11:35:05 +0900

    feat: createUser()에 각 파라미터마다 @RequestParam annotation 추가
    GET 요청인 createUser() 함수에 파라미터임을 나타내는 @RequestParam을 추가하였습니다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,15 +1,20 @@
 package controller;
 
+import annotation.GetMapping;
+import annotation.RequestParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
-import webserver.RequestHandler;
 
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService = new UserService();
 
-    public void createUser(String userId, String password, String name, String email){
+    @GetMapping(path = "/user/create")
+    public void createUser(@RequestParam(name = "userId") String userId,
+                           @RequestParam(name = "password") String password,
+                           @RequestParam(name = "name") String name,
+                           @RequestParam(name = "email") String email){
         logger.debug("createUser() 실행");
 
         userService.createUser(userId, password, name, email);
