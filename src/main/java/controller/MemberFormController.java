ecc6feaa4d225f1763ec338ddbd3b6ba389525eb
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-17 11:54:19 +0900

    feat : update controller to process subpath

--- src/main/java/controller/MemberFormController.java
+++ src/main/java/controller/MemberFormController.java
@@ -1,10 +1,12 @@
 package controller;
 
 import dto.UserDto;
+import http.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
 import webserver.FrontController;
+import webserver.ModelAndView;
 
 import java.net.URLDecoder;
 import java.util.Map;
@@ -14,10 +16,24 @@ public class MemberFormController implements Controller{
     private static final Logger logger = LoggerFactory.getLogger(FrontController.class);
 
     @Override
-    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+    public String process(Request req, Map<String, Object> model) {
+        logger.debug("[MemberFormController.process] req.getUrl() : "+req.getUrl());
+        String subPath = req.getUrl().replace("/user/", "");
+        switch (subPath) {
+            case "create":
+                return createUser(req, model);
+            // Add more cases for other sub-paths as needed
+            default:
+                logger.warn("Unknown subPath: {}", subPath);
+                return null;
+        }
+
+    }
+
+    private String createUser(Request req, Map<String, Object> model){
         try{
             UserDto dto = new UserDto();
-
+            Map<String, String> paramMap = req.getRequestParam();
             dto.setEmail(URLDecoder.decode(paramMap.get("email"), "UTF-8"));
             dto.setUserId(URLDecoder.decode(paramMap.get("userId"), "UTF-8"));
             dto.setName(URLDecoder.decode(paramMap.get("name"), "UTF-8"));
