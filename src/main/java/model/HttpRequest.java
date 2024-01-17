ab86f5887d07a54e8c104b495af457e6e5c7172b
Yang-soeun <soeun8636@naver.com>
2024-01-17 10:27:22 +0900

    HttpRequest 정적 팩토리 메소드를 사용하여 생성하도록 변경

--- src/main/java/model/HttpRequest.java
+++ src/main/java/model/HttpRequest.java
@@ -1,44 +1,39 @@
 package model;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.Objects;
+
+import static constant.HttpRequestConstant.*;
 
 public class HttpRequest {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String END = "";
-    private static final String CHARSET_NAME = "UTF-8";
-    private static final String HOST = "Host:";
-    private static final String CONNECTION = "Connection:";
-    private static final String ACCEPT = "Accept:";
-    private static final String NEW_LINE = "\n";
-    private static final int HTTP_METHOD_POS = 0;
-    private static final int PATH_POS = 1;
-    private static final int HOST_POS = 1;
-    private static final int CONNECTION_POS = 2;
-    private static final int ACCEPT_POS = 3;
-    private static final String PATH_DELIMITER = " ";
-
     private final String method;
     private final URI uri;
     private final String host;
     private final String connection;
     private final String accept;
 
-    public HttpRequest(InputStream in) throws IOException{
+    private HttpRequest(String method, URI uri, String host, String connection, String accept) {
+        this.method = Objects.requireNonNull(method);
+        this.uri = Objects.requireNonNull(uri);
+        this.host = Objects.requireNonNull(host);
+        this.connection = Objects.requireNonNull(connection);
+        this.accept = Objects.requireNonNull(accept);
+    }
+
+    public static HttpRequest from(InputStream in) throws IOException{
         String requestHeader = getRequestHeader(in);
 
-        this.method = requestHeader.split(PATH_DELIMITER)[HTTP_METHOD_POS];
-        this.uri = URI.create(requestHeader.split(PATH_DELIMITER)[PATH_POS]);
-        this.host = requestHeader.split(NEW_LINE)[HOST_POS];
-        this.connection = requestHeader.split(NEW_LINE)[CONNECTION_POS];
-        this.accept = requestHeader.split(NEW_LINE)[ACCEPT_POS];
+        String method = requestHeader.split(PATH_DELIMITER)[HTTP_METHOD_POS];
+        URI uri = URI.create(requestHeader.split(PATH_DELIMITER)[PATH_POS]);
+        String host = requestHeader.split(NEW_LINE)[HOST_POS];
+        String connection = requestHeader.split(NEW_LINE)[CONNECTION_POS];
+        String accept = requestHeader.split(NEW_LINE)[ACCEPT_POS];
+
+        return new HttpRequest(method, uri, host, connection, accept);
     }
 
     public URI getUri() {
@@ -49,7 +44,7 @@ public class HttpRequest {
         return method;
     }
 
-    private String getRequestHeader(InputStream in) throws IOException {
+    private static String getRequestHeader(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, CHARSET_NAME));
         StringBuilder request = new StringBuilder();
 
