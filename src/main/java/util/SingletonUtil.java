66ffb28cb0d9c1c019829313d7dba8bfc79de90e
LEE, TAEGON <taegon1998@gmail.com>
2024-01-17 14:00:16 +0900

    fix: SingletonUtil 로드시에 Database는 의존성 주입 받지 않도록 수정

--- src/main/java/util/SingletonUtil.java
+++ src/main/java/util/SingletonUtil.java
@@ -2,20 +2,17 @@ package util;
 
 import controller.HomeController;
 import controller.UserController;
-import db.Database;
 import service.UserService;
 
 public class SingletonUtil {
     private static final HomeController homeController;
     private static final UserController userController;
     private static final UserService userService;
-    private static final Database database;
 
     static {
         homeController = new HomeController();
         userController = new UserController();
         userService = new UserService();
-        database = new Database();
     }
 
     public static UserController getUserController() {
@@ -24,10 +21,6 @@ public class SingletonUtil {
 
     public static UserService getUserService() {
         return userService;
-    }
-
-    public static Database getDatabase() {
-        return database;
     }
 
     public static HomeController getHomeController() {
