3bc0d77a3d6593fa1bd2984a52e1ba0c5121059f
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-17 13:27:46 +0900

    Refactor : http response를 전송하는 것을 별도의 Service로 분리

---
+++ src/main/java/service/HttpResponseService.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.http.response.HttpResponse;
+import java.io.OutputStream;
+
+public interface HttpResponseService {
+    void sendHttpResponse(OutputStream out, HttpResponse httpResponse);
+}
---
+++ src/main/java/service/HttpResponseServiceImpl.java
@@ -0,0 +1,39 @@
+package service;
+
+import model.http.response.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class HttpResponseServiceImpl implements HttpResponseService {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponseService.class);
+    @Override
+    public void sendHttpResponse(OutputStream out, HttpResponse httpResponse){
+        DataOutputStream dos = new DataOutputStream(out);
+        setStatusAndHeader(dos, httpResponse);
+        setBody(dos, httpResponse);
+    }
+
+    private void setBody(DataOutputStream dos, HttpResponse httpResponse){
+        try {
+            dos.write(httpResponse.getBody().getContent(), 0, httpResponse.getBody().getContent().length);
+            dos.flush();
+        } catch (IOException e){
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void setStatusAndHeader(DataOutputStream dos, HttpResponse httpResponse) {
+        try {
+            dos.writeBytes(httpResponse.getStatusLine().getStatusHeader());
+            dos.writeBytes(httpResponse.getHeaders().getContentTypeHeader());
+            dos.writeBytes(httpResponse.getHeaders().getContentLengthHeader());
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
