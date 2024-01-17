345cacc954e85264f820e4edbd66f150bef17fa6
LEE, TAEGON <taegon1998@gmail.com>
2024-01-17 11:03:58 +0900

    refact: HomeController 필드 추가

--- src/main/java/util/SingletonUtil.java
+++ src/main/java/util/SingletonUtil.java
@@ -1,15 +1,18 @@
 package util;
 
+import controller.HomeController;
 import controller.UserController;
 import db.Database;
 import service.UserService;
 
 public class SingletonUtil {
+    private static final HomeController homeController;
     private static final UserController userController;
     private static final UserService userService;
     private static final Database database;
 
     static {
+        homeController = new HomeController();
         userController = new UserController();
         userService = new UserService();
         database = new Database();
@@ -25,5 +28,9 @@ public class SingletonUtil {
 
     public static Database getDatabase() {
         return database;
+    }
+
+    public static HomeController getHomeController() {
+        return homeController;
     }
 }
