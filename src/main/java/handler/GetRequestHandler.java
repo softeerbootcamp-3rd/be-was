285a24c3589ddcb13769aa6c9f51da9a90334222
Hymin2 <khm8246@gmail.com>
2024-01-17 11:37:47 +0900

    feat: GetRequestHandler 객체 생성 및 GET 요청 구현
    GET 요청일 때 @GetMapping의 annotation을 이용해 path에 알맞은 메소드를 찾고, @RequestParam으로 각 파라미터의 이름을 구분하여 파리미터에 알맞은 값을 넣어주도록 구현했습니다.

--- src/main/java/handler/GetRequestHandler.java
+++ src/main/java/handler/GetRequestHandler.java
@@ -1,41 +1,88 @@
 package handler;
 
+import annotation.Controller;
+import annotation.GetMapping;
+import annotation.RequestParam;
 import controller.UserController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import request.GetRequest;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+@Controller
 public class GetRequestHandler {
     private static final Logger logger = LoggerFactory.getLogger(GetRequestHandler.class);
 
-    public static void map(GetRequest getRequest) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        if(getRequest.getPath().equals("/user/create")) {
-            UserController userController = new UserController();
-            Class<UserController> userControllerClass = UserController.class;
+    public static Object run(GetRequest getRequest) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        List<Class<?>> classes = getControllerClasses();
+        Method method = findMethod(classes, getRequest.getPath());
 
-            Method method = userControllerClass.getDeclaredMethod("createUser", String.class, String.class, String.class, String.class);
-            Parameter[] parameters = method.getParameters();
+        if(method == null){
+            throw new NoSuchMethodException(String.format("GET %s 를 찾을 수 없습니다.", getRequest.getPath()));
+        }
 
-            Object[] params = new Object[parameters.length];
-            int index = 0;
+        Object[] params = createParams(method, getRequest.getParamsMap());
 
-            for(Parameter parameter: parameters){
-                for(String parameterName: getRequest.getParamsMap().keySet()){
-                    if(parameterName.equals(parameter.getName())){
-                        params[index++] = getRequest.getParamsMap().get(parameterName);
+       return executeMethod(method, params);
+    }
 
-                        break;
+    private static Object executeMethod(Method method, Object[] params) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> c = method.getDeclaringClass();
+        Object instance = c.getDeclaredConstructor().newInstance();
+
+        return method.invoke(instance, params);
+    }
+
+    private static Object[] createParams(Method method, Map<String, String> originParams){
+        Class<? extends Annotation> requestParam = RequestParam.class;
+        Parameter[] parameters = method.getParameters();
+
+        Object[] params = new Object[parameters.length];
+        int index = 0;
+
+        for(Parameter parameter: parameters){
+            if(parameter.isAnnotationPresent(requestParam)){
+                RequestParam annotation = (RequestParam) parameter.getAnnotation(requestParam);
+
+                params[index++]= originParams.get(annotation.name());
+            }
+        }
+
+        return params;
+    }
+
+    private static Method findMethod(List<Class<?>> classes, String path){
+        Class<? extends Annotation> getMapping = GetMapping.class;
+
+        for(Class<?> c: classes){
+            Method[] methods = c.getDeclaredMethods();
+
+            for(Method method: methods){
+                if(method.isAnnotationPresent(getMapping)){
+                    GetMapping annotation = (GetMapping) method.getAnnotation(getMapping);
+
+                    if(path.equals(annotation.path())){
+                        return method;
                     }
                 }
             }
-
-            method.invoke(userController, params);
-        } else{
-            throw new NoSuchMethodException("요청 PATH가 존재하지 않음");
         }
+
+        return null;
+    }
+
+    private static List<Class<?>> getControllerClasses(){
+        List<Class<?>> classes = new ArrayList<>();
+
+        classes.add(UserController.class);
+
+        return classes;
     }
 }
