e7c43674d92355ec4a7e0b75ee5e136b996b94c5
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-17 11:54:58 +0900

    feat : update parameters of function process

--- src/main/java/controller/TemplateController.java
+++ src/main/java/controller/TemplateController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import http.Request;
+
 import java.util.Map;
 
 public class TemplateController implements Controller
 {
     @Override
-    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+    public String process(Request req, Map<String, Object> model) {
+        Map<String, String> paramMap = req.getRequestParam();
         System.out.println("[TemplateController.process] template : " + paramMap.get("template"));
         return paramMap.get("template");
     }
