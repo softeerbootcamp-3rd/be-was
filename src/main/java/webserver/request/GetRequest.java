880d7ce8081559c5042a03e23ce7758bbe47a42e
Hymin2 <khm8246@gmail.com>
2024-01-17 17:36:57 +0900

    refactor: GetRequest의 생성 방법을 정적 팩토리 메소드 방법으로 변경
    GetRequest를 생성할 때 생성자로 인스턴스를 생성하지 않고, static 메소드인 of를 통해 생성하도록 변경하였습니다.

--- src/main/java/webserver/request/GetRequest.java
+++ src/main/java/webserver/request/GetRequest.java
@@ -6,9 +6,17 @@ public class GetRequest {
     private final String path;
     private final Map<String, String> paramsMap;
 
-    public GetRequest(String path, Map<String, String> paramsMap){
+    private GetRequest(String path, Map<String, String> paramsMap){
         this.path = path;
         this.paramsMap = paramsMap;
+    }
+
+    public static GetRequest of(String path, Map<String, String> paramsMap){
+        return new GetRequest(path, paramsMap);
+    }
+
+    public static GetRequest of(String path){
+        return new GetRequest(path, null);
     }
 
     public String getPath(){
