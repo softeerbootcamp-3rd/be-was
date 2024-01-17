15da21d2fdaed2b18bdd24c37ffe72cb8bfb3bcb
johan1103 <johan1103@naver.com>
2024-01-17 18:17:16 +0900

    fix: socket에 byte를 write하는 책임을 HttpResponseBuilder에게 위임하도록 수정

--- src/main/java/webserver/io/RequestHandler.java
+++ src/main/java/webserver/io/RequestHandler.java
@@ -1,17 +1,21 @@
 package webserver.io;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.MyHttpServletRequest;
+import webserver.MyHttpServletResponse;
 import webserver.mapper.ControllerMapper;
 import webserver.mapper.ParameterMapper;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private final HttpResponseBuilder responseBuilder;
+    private HttpResponseBuilder responseBuilder;
     private final ControllerHandler controllerHandler = new ControllerHandler();
     private final ControllerMapper controllerMapper = new ControllerMapper();
     private final ParameterMapper parameterMapper = new ParameterMapper();
@@ -19,13 +23,11 @@ public class RequestHandler implements Runnable {
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        this.responseBuilder=new HttpResponseBuilder();
     }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             MyHttpServletRequest httpServletRequest = printReceivedRequest(in);
@@ -33,40 +35,19 @@ public class RequestHandler implements Runnable {
 
             controllerMapper.findAppropriateControllerMethod(controllerHandler,httpServletRequest.getUri());
             parameterMapper.findAppropriateParameter(controllerHandler,httpServletRequest);
-            String resourcePath = controllerHandler.handleController();
+            MyHttpServletResponse httpResponse = controllerHandler.handleController();
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = responseBuilder.getFileBytes(resourcePath);
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+            this.responseBuilder= new HttpResponseBuilder(new DataOutputStream(out));
+            responseBuilder.flushHttpResponse(httpResponse);
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
+        } catch (IOException | InvocationTargetException | IllegalAccessException e) {
             logger.error(e.getMessage());
         }
     }
 
     private MyHttpServletRequest printReceivedRequest(InputStream in) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-        String line = br.readLine();
+        String line = URLDecoder.decode(br.readLine(), StandardCharsets.UTF_8);
         logger.debug("request line : {}",line);
         MyHttpServletRequest servletRequest = MyHttpServletRequest.init(line);
         if(servletRequest==null)
