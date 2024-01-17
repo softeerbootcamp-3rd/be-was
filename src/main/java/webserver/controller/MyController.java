a0c77d733de9fc3426f52e6d56018b4591d2048c
johan1103 <johan1103@naver.com>
2024-01-17 18:18:03 +0900

    feat: 회원가입 요청을 받고 redirect응답을 하는 controller 작성

--- src/main/java/webserver/controller/MyController.java
+++ src/main/java/webserver/controller/MyController.java
@@ -6,8 +6,14 @@ import org.slf4j.LoggerFactory;
 public class MyController {
   private final Logger logger = LoggerFactory.getLogger(MyController.class);
   @Controller(uri = "/user/form.html")
-  public String join(String word,Integer age){
+  public String joinForm(String word,Integer age){
     logger.info("Controller executed word : {}, age :{}",word,age);
-    return "/index.html";
+    return "/user/form.html";
+  }
+  ///create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net
+  @Controller(uri = "/user/create")
+  public String join(String userId,String password,String email){
+    logger.info("Controller executed userId : {}, password : {}, email : {}",userId,password,email);
+    return "redirect:/user/form.html";
   }
 }
