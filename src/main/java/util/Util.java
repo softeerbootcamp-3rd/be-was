1ef407bd2d016e8b69f226cbaa627b61663f9e9d
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-17 13:41:57 +0900

    Feat : HTTP URL 파싱 기능 구현

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,9 +1,15 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Util {
 
     private static final String REQUEST_LINE_DELIMITER = " ";
     private static final String REQUEST_HEADER_DELIMITER = ": ";
+    private static final String URL_DELIMITER = "\\?";
+    private static final String PARAMETERS_DELIMITER = "&";
+    private static final String PARAMETER_DELIMITER = "=";
 
     public static String[] splitRequestLine(String input) {
         return input.split(REQUEST_LINE_DELIMITER);
@@ -11,5 +17,18 @@ public class Util {
 
     public static String[] splitRequestHeader(String input) {
         return input.split(REQUEST_HEADER_DELIMITER);
+    }
+
+    public static String[] splitUrlToPathAndParameters(String url) {
+        return url.split(URL_DELIMITER);
+    }
+
+    public static Map<String, String> splitParamtersToKeyAndValue(String parameters) throws IllegalArgumentException{
+        Map<String, String> queryParameters = new HashMap<>();
+        for (String parameter: parameters.split(PARAMETERS_DELIMITER)) {
+            String[] token = parameter.split(PARAMETER_DELIMITER);
+            queryParameters.put(token[0], token[1]);
+        }
+        return queryParameters;
     }
 }
