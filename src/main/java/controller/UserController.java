7a52c4619809799adc0151012c706258de80a706
LEE, TAEGON <taegon1998@gmail.com>
2024-01-17 11:05:10 +0900

    refact: 요청한 Path에 따른 분기문 작성

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,12 +8,27 @@ public class UserController {
 
     // 요청 URI에 따라 요청을 처리할 컨트롤러를 선택하는 역할
     public String route(String path) {
-        if (path.startsWith("/user/create")) { // 회원가입 요청
-            signUp(path);
+        if (path.startsWith("/user/form")) { // 회원가입 페이지 요청
+            return "200 " + path;
+        }
+        else if (path.startsWith("/user/list")) {
+            return "200 " + path;
+        }
+        else if (path.startsWith("/user/login")) {
+            return "200 " + path;
+        }
+        else if (path.startsWith("/user/login_failed")) {
+            return "200 " + path;
+        }
+        else if (path.startsWith("/user/profile")) {
+            return "200 " + path;
+        }
+        else if (path.startsWith("/user/create")) { // 회원가입 요청
+            signUp(path); // 회원가입 요청 처리
             return "302 /index.html";
         }
-        else { // 정적 파일 요청
-            return "200 " + path;
+        else {
+            throw new IllegalArgumentException("요청 URI에 해당하는 컨트롤러가 없습니다.");
         }
     }
 
