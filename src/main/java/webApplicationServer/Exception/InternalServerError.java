e52bcfea18d4e1822387b7e544b77848976c2ccd
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-18 11:09:36 +0900

    Feat : 에러관리를 위한 CustomError 생성

---
+++ src/main/java/webApplicationServer/Exception/InternalServerError.java
@@ -0,0 +1,11 @@
+package webApplicationServer.Exception;
+
+public class InternalServerError extends RuntimeException {
+    public InternalServerError(String message) {
+        super(message);
+    }
+
+    public InternalServerError(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
