6d171d5ed3e4a2ac418d2752113f057f87f23aa4
admin <lejs1008@naver.com>
2024-01-18 11:54:26 +0900

    step-2 회원가입 기능에 서비스 계층 추가

--- src/main/java/controller/MemberJoinController.java
+++ src/main/java/controller/MemberJoinController.java
@@ -5,21 +5,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import request.HttpRequest;
 import response.HttpResponse;
+import service.MemberJoinService;
 import webserver.RequestHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MemberJoinController implements Controller {
-
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    MemberJoinService memberJoinService = new MemberJoinService();
     @Override
     public void process(HttpRequest request, HttpResponse response) {
         String fullUrl = request.getUrl();
         String queryString = fullUrl.substring(fullUrl.indexOf("?") + 1);
         Map<String, String> params = parse(queryString);
-        User user = createUser(params);
-        logger.debug("User : {}", user);
+
+        memberJoinService.createUser(params);
+
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Location", "/index.html");
@@ -28,24 +29,12 @@ public class MemberJoinController implements Controller {
 
     public static Map<String, String> parse(String query) {
         Map<String, String> params = new HashMap<>();
-
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             String[] keyValue = pair.split("=");
             params.put(keyValue[0], keyValue[1]);
         }
-
         return params;
-    }
-
-    public static User createUser(Map<String, String> params) {
-        String userId = params.get("userId");
-        String password = params.get("password");
-        String name = params.get("name");
-        String email = params.get("email");
-
-        User user = new User(userId, password, name, email);
-        return user;
     }
 
 }
