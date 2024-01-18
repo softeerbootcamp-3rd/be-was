64c1b101b6c83549e5235da6a279ec53673b31f1
johan1103 <johan1103@naver.com>
2024-01-18 12:14:18 +0900

    feat: RequestHandler 실행 흐름 설명용 주석 작성

--- src/main/java/webserver/io/RequestHandler.java
+++ src/main/java/webserver/io/RequestHandler.java
@@ -33,11 +33,16 @@ public class RequestHandler implements Runnable {
             MyHttpServletRequest httpServletRequest = printReceivedRequest(in);
             logger.debug("http request : {}",httpServletRequest.toString());
 
+            //ControllerMapper로부터 요청을 수행할 수 있는 Method를 가져온다.
             controllerMapper.findAppropriateControllerMethod(controllerHandler,httpServletRequest.getUri());
+            //ParameterMapper로부터 Method의 파라미터들을 MyHttpServletRequest가 가지고 있는 쿼리 파라미터에서 추출해 주입한다.
             parameterMapper.findAppropriateParameter(controllerHandler,httpServletRequest);
+            //Method를 실행할 수 있는 조건(실행할 Method, Method에 필요한 파라미터값, 실행할 Method를 가지고 있는 객체)이
+            //다 갖춰졌기 때문에 해당 Method를 ControllerHandler가 실행한다.
             MyHttpServletResponse httpResponse = controllerHandler.handleController();
 
             this.responseBuilder= new HttpResponseBuilder(new DataOutputStream(out));
+            //responseBuilder에게 socket에 http응답정보를 작성하도록 요청한다.
             responseBuilder.flushHttpResponse(httpResponse);
 
         } catch (IOException | InvocationTargetException | IllegalAccessException e) {
@@ -49,9 +54,11 @@ public class RequestHandler implements Runnable {
         BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
         String line = URLDecoder.decode(br.readLine(), StandardCharsets.UTF_8);
         logger.debug("request line : {}",line);
+        //Http Method & Version & URI 파싱
         MyHttpServletRequest servletRequest = MyHttpServletRequest.init(line);
         if(servletRequest==null)
             return null;
+        //Http Header 파싱
         while(true){
             line=br.readLine();
             if (line.equals(""))
