7a2a56c4471203a435e5b47cb7bc7cb0f2cd95f4
johan1103 <johan1103@naver.com>
2024-01-18 12:09:13 +0900

    feat: ControllerMapper 핵심 로직 주석작성

--- src/main/java/webserver/mapper/ControllerMapper.java
+++ src/main/java/webserver/mapper/ControllerMapper.java
@@ -10,10 +10,13 @@ public class ControllerMapper {
 
   public void findAppropriateControllerMethod(ControllerHandler handler, String uri){
     MyController myController = new MyController();
+    //MyController가 가진 모든 method 획득
     Method[] methods = myController.getClass().getDeclaredMethods();
     for (Method m : methods){
       Controller annotation = m.getAnnotation(Controller.class);
+      //메서드가 @Controller를 가지고 있고 http요청 uri가 @Controller내부의 uri value와 일치하는지 검사
       if(annotation!=null && annotation.uri().equals(uri)){
+        //일치하면 해당 method를 controller method로 선택.
         handler.setByControllerMapper(myController,m);
         return;
       }
