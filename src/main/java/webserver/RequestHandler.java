1ae6d6160aea4acc6ac9b684311afc24e62eb262
OSSU <o.ss.u2056@gmail.com>
2024-01-18 10:51:16 +0900

    refactor: 요청/응답 방식 객체화 1. 요청이 들어오면 어떤 컨트롤러에 위임할 지 탐색 (ControllerMapper 이용) 2. 컨트롤러 탐색 후 있는 경우 동적 자원 처리 로직으로 3. 컨트롤러 없는 경우 정적 자원 처리 로직으로 4. 응답값 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,18 +3,17 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Path;
+import java.util.function.Function;
 
-import db.Database;
-import model.HttpRequest;
-import model.User;
+import dto.HttpResponseDto;
+import utils.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.FileReader;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
     private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -26,152 +25,48 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // httpRequest 객체 생성
-            HttpRequest httpRequest = getHttpRequest(in);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-            if (httpRequest == null) {
-                return;
-            }
-
-            // httpRequest 출력
+            // httpRequest 생성 및 출력
+            HttpRequest httpRequest = new HttpRequest(br);
             logger.debug(httpRequest.toString());
 
-            String requestURL = httpRequest.getTarget();
-
-            DataOutputStream dos = new DataOutputStream(out);
-            // 파라미터 처리
-            if (requestURL.contains("?")) {
-                requestURL = runAPI(requestURL);
-                linkPage(requestURL, dos);
+            // 동적 & 정적 자원 분할 처리
+            Function<HttpRequest, HttpResponseDto> controller = ControllerMapper.getController(httpRequest);
+            HttpResponseDto response;
+            String requestUrl = httpRequest.getUrl();
+            String basePath = FileReader.getBasePath(requestUrl);
+            // 동적 자원 처리
+            if (controller != null) {
+                logger.info("controller 호출");
+                response = controller.apply(httpRequest);
             } else {
-                // 페이지 링크
-                linkPage(requestURL, dos);
+                // 정적 자원 처리
+                logger.info("정적 파일 호출");
+                if (requestUrl.equals("/")) {
+                    byte[] body = FileReader.readFile("/index.html");
+                    response = HttpResponseDto.of(HttpStatus.OK, ContentType.HTML, body);
+                } else {
+                    Path filePath = Path.of(basePath + requestUrl);
+                    if (Files.exists(filePath) && Files.isRegularFile(filePath)) {
+                        String extension = FileReader.getFileExtension(filePath);
+                        byte[] body = FileReader.readFile(requestUrl);
+                        response = HttpResponseDto.of(HttpStatus.OK, ContentType.getContentType(extension), body);
+                    } else {
+                        logger.info("404 not found");
+                        byte[] body = FileReader.readFile("/404.html");
+                        response = HttpResponseDto.of(HttpStatus.NOT_FOUND, ContentType.HTML, body);
+                    }
+                }
             }
+
+            // 응답
+            DataOutputStream dos = new DataOutputStream(out);
+            ResponseBuilder.send(dos, response);
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private String runAPI(String requestURL) {
-        String path = requestURL.split("\\?")[0];
-        String params = requestURL.split("\\?")[1];
-
-        // 회원가입
-        if (path.equals("/user/create")) {
-            String[] tokens = params.split("&");
-            Map<String, String> map = new HashMap<>();
-            for (String token : tokens) {
-                map.put(token.split("=")[0], token.split("=")[1]);
-            }
-            createUser(map);
-            path = "/user/form.html";
-        }
-
-        return path;
-    }
-
-    private void createUser(Map<String, String> param) {
-        User user = new User(param.get("userId"), param.get("password"), param.get("name"), param.get("email"));
-
-        if (Database.findUserById(user.getUserId()) == null) {
-            Database.addUser(user);
-        }
-
-        logger.info(Database.findAll().toString());
-    }
-
-    private void linkPage(String requestURL, DataOutputStream dos) throws IOException {
-        String basePath = getBasePath(requestURL);
-
-        byte[] body;
-        if (basePath.isEmpty()) {
-            body = "Hello World".getBytes();
-        } else {
-            body = Files.readAllBytes(new File(basePath + requestURL).toPath());
-        }
-
-        String content = getContent(requestURL);
-        response200Header(dos, body.length, content);
-        responseBody(dos, body);
-
-    }
-
-    private String getContent(String url) {
-        if (url.contains(".html")) {
-            return "text/html";
-        }
-
-        if (url.contains(".css")) {
-            return "text/css";
-        }
-
-        if (url.contains(".js")) {
-            return "application/x-javascript";
-        }
-
-        if (url.contains(".png")) {
-            return "image/png";
-        }
-
-        if (url.contains(".ico")) {
-            return "image/x-icon";
-        }
-
-        return "text/html";
-    }
-
-    private String getBasePath(String requestURL) {
-        if (requestURL.contains(".html")) {
-            return "src/main/resources/templates";
-        }
-
-        if(requestURL.contains("/css/") || requestURL.contains("/fonts/") || requestURL.contains("/images/") || requestURL.contains("/js/") || requestURL.contains("favicon.ico"))
-            return "src/main/resources/static";
-
-        return "";
-    }
-
-    private HttpRequest getHttpRequest(InputStream in) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-
-        // http request 처리
-
-        // parser -> startline parser, httprequest form, queryparser
-        String line;
-        if (!(line = bufferedReader.readLine()).isEmpty()) {
-            // start line 처리
-            String[] tokens = line.split(" ");
-
-            HttpRequest httpRequest = new HttpRequest(tokens[0], tokens[1], tokens[2]);
-
-            // header 처리
-            while (!(line = bufferedReader.readLine()).isEmpty()) {
-                tokens = line.split(": ");
-                httpRequest.updateHeaders(tokens[0], tokens[1]);
-            }
-
-            return httpRequest;
-        }
-        return null;
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String content) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + content + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 }
