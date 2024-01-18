67a588967811aa901ff871fd1b396697b5d9ccf8
OSSU <o.ss.u2056@gmail.com>
2024-01-18 10:45:13 +0900

    refactor: http 응답 형식 객체화 - status : 응답 상태 (응답 코드 + 메시지) - content-type : 반환될 컨텐츠의 타입 - body : 응답 body 부분

---
+++ src/main/java/utils/HttpStatus.java
@@ -0,0 +1,26 @@
+package utils;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    ;
+
+    private Integer code;
+    private String message;
+
+    HttpStatus(Integer code, String status) {
+        this.code = code;
+        this.message = status;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
