744cdfbd755b4fc17779ae6603f02f979ebcb818
seojeewon <readyjiwon@gmail.com>
2024-01-18 10:45:44 +0900

    Feat: 회원가입과 일반 페이지 요청 분리
    RequestHandler에서 가입 버튼을 눌렀을 때 요청과 그 외의 페이지 요청을 분리했습니다.
    또한 가입 버튼시 오는 요청의 경우 새로운 유저를 데이터베이스에 저장하고, 홈 화면(/index.html)로 리다이렉트하게 만들었습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,8 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
+import dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import service.UserService;
 import utils.FileReader;
 import utils.RequestLogger;
 import utils.RequestParser;
@@ -12,6 +14,7 @@ import utils.ResponseSender;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final UserService userService = new UserService();
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -24,13 +27,28 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            DataOutputStream dos = new DataOutputStream(out);
             //HTTP Request Logging
             RequestLogger.logRequest(in);
-            //파일 읽기
-            byte[] body = FileReader.readFile(RequestParser.getPath());
-            //HTTP Response
-            DataOutputStream dos = new DataOutputStream(out);
-            ResponseSender.sendResponse(dos, body.length, body);
+
+            //TODO 파일을 읽어오는 요청이 아닌 경우(예: 회원가입의 가입 버튼)
+            String path = RequestParser.getPath();
+            //가입 저장 요청
+            if (path.contains("user/create")) {
+                UserDTO userInfo = RequestParser.parseNewUserInfo();
+                String userId = userService.registerUser(userInfo);
+                logger.debug("New User Registered! User Id: {}", userId);
+
+                //홈 화면(/index.html)로 리다이렉트
+                String redirectLocation = "/index.html";
+                ResponseSender.sendRedirectResponse(dos, redirectLocation);
+            } else {   //html, js, css 등 페이지 요청
+                //파일 읽기
+                byte[] body = FileReader.readFile(RequestParser.getPath());
+                //HTTP Response
+                ResponseSender.sendResponse(dos, body.length, body);
+            }
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
