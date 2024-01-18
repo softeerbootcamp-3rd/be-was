9b96c68347f5e8fa6e44ac640a812da3a88b7106
johan1103 <johan1103@naver.com>
2024-01-18 13:55:15 +0900

    feat: parameter parsing부분 주석 작성

--- src/main/java/webserver/mapper/ParameterMapper.java
+++ src/main/java/webserver/mapper/ParameterMapper.java
@@ -20,20 +20,31 @@ public class ParameterMapper {
   }
 
   public void findAppropriateParameter(ControllerHandler handler, MyHttpServletRequest request){
+    //ControllerMapper가 찾아낸 method의 parameter 리스트 획득
     Parameter[] parameters = handler.getMethod().getParameters();
+    //매핑된 parameter는 mappedParameters에 저장 (매핑 실패해도 null로 저장)
     List<Object> mappedParameters = new ArrayList<>();
 
     for(Parameter p : parameters){
       if(!p.isNamePresent())
         throw new ParameterMapperException();
+      //파라미터의 이름, 변수타입 이름(Integer,Double..) 획득
       String parameterTypeName=p.getType().getSimpleName();
       String parameterName=p.getName();
+      //쿼리파라미터에서 파라미터 이름에 맞는 value 획득
       String parameterValue = request.getQueryParameterValue(parameterName);
 
       boolean successParsing=false;
       for (Parser parser : parsers){
+        //특정 Parser가 parsing이 가능할 때, 파싱 로직 수행
         if (parser.canParse(parameterTypeName)){
-          mappedParameters.add(parser.parse(parameterValue));
+          Object convertedParameter = null;
+          //parse수행, exception 발생시에는 mappedParameters에 null 삽입
+          try {
+            convertedParameter = parser.parse(parameterValue);
+          }finally {
+            mappedParameters.add(convertedParameter);
+          }
           successParsing=true;
           break;
         }
