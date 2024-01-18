a31374d7b2fa09e94c013c88095fd9c7b2767389
johan1103 <johan1103@naver.com>
2024-01-19 04:30:40 +0900

    fix: controller에 맞는 method를 찾을 때에는 uri기반으로 hashmap에서 찾도록하고 reflection은 초기 생성자에서 한번만 사용해서 hashmap에 저장하도록 수정

--- src/main/java/webserver/mapper/ControllerMapper.java
+++ src/main/java/webserver/mapper/ControllerMapper.java
@@ -5,23 +5,28 @@ import webserver.controller.MyController;
 import webserver.io.ControllerHandler;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 public class ControllerMapper {
+  private final HashMap<String,Method> controllerMethodMap = new HashMap<>();
+  public ControllerMapper(){
+    //MyController가 가진 모든 method 획득
+    Method[] methods = MyController.class.getDeclaredMethods();
+    for (Method m : methods){
+      Controller annotation = m.getAnnotation(Controller.class);
+      if(annotation==null)
+        continue;
+      String uri = annotation.uri();
+      controllerMethodMap.put(uri,m);
+    }
+  }
 
   public void findAppropriateControllerMethod(ControllerHandler handler, String uri){
     MyController myController = new MyController();
-    //MyController가 가진 모든 method 획득
-    Method[] methods = myController.getClass().getDeclaredMethods();
-    for (Method m : methods){
-      Controller annotation = m.getAnnotation(Controller.class);
-      //메서드가 @Controller를 가지고 있고 http요청 uri가 @Controller내부의 uri value와 일치하는지 검사
-      if(annotation!=null && annotation.uri().equals(uri)){
-        //일치하면 해당 method를 controller method로 선택.
-        handler.setByControllerMapper(myController,m);
-        return;
-      }
-    }
-    throw new ControllerMapperException();
+    Method controllerMethod = controllerMethodMap.get(uri);
+    if(controllerMethod==null)
+      throw new ControllerMapperException();
+    handler.setByControllerMapper(myController,controllerMethod);
   }
   public static class ControllerMapperException extends RuntimeException{
   }
