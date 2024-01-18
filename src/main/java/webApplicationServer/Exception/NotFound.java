505af0717ff8de1872967951b2dbd1b6e7a01800
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-18 13:43:55 +0900

    Feat : Not Found Error 추가

---
+++ src/main/java/webApplicationServer/Exception/NotFound.java
@@ -0,0 +1,11 @@
+package webApplicationServer.Exception;
+
+public class NotFound extends RuntimeException {
+    public NotFound(String message) {
+        super(message);
+    }
+
+    public NotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
