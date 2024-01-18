00101ebfd6a41fccffe111b60c1cc78bf4729181
OSSU <o.ss.u2056@gmail.com>
2024-01-18 16:18:32 +0900

    step 2: Junit 단위 테스트 - 회원가입 로직 검증 - 중복 아이디 검증

---
+++ src/test/java/service/UserServiceTest.java
@@ -0,0 +1,44 @@
+package service;
+
+import db.Database;
+import dto.UserDto;
+import model.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    UserService userService = new UserService();
+
+    @Test
+    void 회원가입() {
+        // given
+        UserDto userDto = new UserDto("ossu1975", "1234", "ossu", "ossu@gmail.com");
+
+        // when
+        userService.createUser(userDto);
+
+        // then
+        User user = Database.findUserById(userDto.getUserId());
+        assertEquals(userDto.getName(), user.getName());
+    }
+
+    @Test
+    void 중복_아이디_검증() {
+        // given
+        UserDto userDto1 = new UserDto("ossu1975", "1234", "ossu", "ossu@gmail.com");
+        UserDto userDto2 = new UserDto("ossu1975", "1234", "ossu", "ossu@gmail.com");
+
+
+        // when
+        userService.createUser(userDto1);
+        userService.createUser(userDto2);
+
+        // then
+        Collection<User> users = Database.findAll();
+        assertEquals(1, users.size());
+
+    }
+}
