a4a0ec6c0b32ce2c4fe774d9c8e77b100e9b1bb4
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-18 11:09:58 +0900

    Feat : User 회원가입 정보를 옮기는 DTO 생성

---
+++ src/main/java/dto/UserSignUpDto.java
@@ -0,0 +1,28 @@
+package dto;
+
+import model.User;
+
+public class UserSignUpDto {
+    private final String id;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public UserSignUpDto(String id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public User userMapping(){
+        return new User(id, password, name, email);
+    }
+}
