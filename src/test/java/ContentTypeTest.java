98ac9130a696b7790682cfeab9e45e021a52f854
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-18 11:07:42 +0900

    feat : contentType test

---
+++ src/test/java/ContentTypeTest.java
@@ -0,0 +1,40 @@
+import controller.Controller;
+import controller.ResourceController;
+import http.Request;
+import http.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.ModelAndView;
+import webserver.MyView;
+import webserver.ViewResolver;
+import webserver.adaptor.MyHandlerAdapter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContentTypeTest {
+    MyHandlerAdapter adapter = new MyHandlerAdapter();
+    ViewResolver viewResolver = new ViewResolver();
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    public ContentTypeTest() throws IOException {
+    }
+    @Test
+    @DisplayName("html")
+    public void HtmlContentType() throws IOException {
+        //given
+        Request req = new Request("GET","/css/styles.css");
+        Response res = new Response();
+        Controller handler = new ResourceController();
+
+        //when
+        ModelAndView mv = adapter.handle(req, res, handler);
+        MyView view = viewResolver.resolve(mv.getViewName());
+        view.render(dos, req, res);
+        //then
+        assertEquals("text/css",res.getContentType());
+    }
+}
