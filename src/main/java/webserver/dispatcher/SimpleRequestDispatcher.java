4515c9e2b3028e8d700d72b94d7b3a438e5bb336
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-18 14:46:35 +0900

    fix : delete unused library import

--- src/main/java/webserver/dispatcher/SimpleRequestDispatcher.java
+++ src/main/java/webserver/dispatcher/SimpleRequestDispatcher.java
@@ -5,7 +5,6 @@ import http.Request;
 import http.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.FrontController;
 
 import java.io.File;
 import java.io.IOException;
