5be70d61d0edc0031a4ddbd6bd7a2c4badee71bc
seojeewon <readyjiwon@gmail.com>
2024-01-18 10:38:39 +0900

    Feat: 회원가입
    UserService class를 만들어 새로운 유저를 가입시켜 데이터베이스에 저장하는 메서드 registerUser입니다.

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,18 @@
+package service;
+
+import db.Database;
+import dto.UserDTO;
+import model.User;
+import utils.RequestParser;
+
+public class UserService {
+    public String registerUser(UserDTO userInfo){
+        //TODO: 이미 가입한 사람인지 확인
+
+
+        //Database에 새로운 유저 저장
+        User newbie = new User(userInfo.getUserId(), userInfo.getPassword(), userInfo.getName(), userInfo.getEmail());
+        Database.addUser(newbie);
+        return userInfo.getUserId();
+    }
+}
