a48a74646d8c1b8176b134e591baa01d78d962bb
OSSU <o.ss.u2056@gmail.com>
2024-01-18 10:47:23 +0900

    refactor: 객체 추출 - ContentType : 응답 값에 들어갈 content-type을 확장자와 맵핑시키는 역할 - FileReader : 응답 body 부분에 들어갈 파일을 읽어오는 역할 - ResponseBuilder : 클라이언트로 응답값 반환하는 역할

---
+++ src/main/java/utils/ResponseBuilder.java
@@ -0,0 +1,34 @@
+package utils;
+
+import dto.HttpResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * 응답 반환
+ */
+public class ResponseBuilder {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseBuilder.class);
+
+    public static void send(DataOutputStream dos, HttpResponseDto response) {
+        HttpStatus status = response.getStatus();
+        try {
+            dos.writeBytes("HTTP/1.1" + status.getCode() + " " + status.getMessage() + "\r\n");
+            dos.writeBytes("Content-Type: " + response.getContent().getType() + ";charset=utf-8\r\n");
+
+            byte[] body = response.getBody();
+            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+}
