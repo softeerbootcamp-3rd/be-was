02c9c82d9ef437e70b192c0a3b0d598d3c14722c
jbonnie <bonnie8623@gmail.com>
2024-01-18 17:39:04 +0900

    로그인 테스트 코드 추가

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -3,6 +3,7 @@ package service;
 import db.Database;
 import dto.HTTPRequestDto;
 import dto.HTTPResponseDto;
+import model.User;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -121,5 +122,34 @@ class ServiceTest {
         catch(IOException e) {
             fail();
         }
+    }
+
+    // 로그인 실패 테스트
+    @Test
+    void testLoginFail() {
+        // given
+        HTTPResponseDto expected = new HTTPResponseDto(303, "/user/login_failed.html".getBytes());
+        // when
+        String body = "userId=login&password=fail";
+        HTTPRequestDto httpRequestDto = new HTTPRequestDto("POST", "/user/login",
+                "HTTP/1.1", "localhost:8080", "text/html", body.length(), body);
+        HTTPResponseDto actual = Service.login(httpRequestDto);
+        // then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    // 로그인 성공 테스트
+    @Test
+    void testLoginSuccess() {
+        // given
+        HTTPResponseDto expected = new HTTPResponseDto(303, "/index.html".getBytes());
+        // when
+        Database.addUser(new User("hello", "world", "hello", "world"));
+        String body = "userId=hello&password=world";
+        HTTPRequestDto httpRequestDto = new HTTPRequestDto("POST", "/user/login",
+                "HTTP/1.1", "localhost:8080", "text/html", body.length(), body);
+        HTTPResponseDto actual = Service.login(httpRequestDto);
+        // then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 }
