ee16f3f5d8ce3e2df2745426102cf68df20b4139
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-18 16:01:07 +0900

    Refactor(UserController) : 회원가입 시, User 객체를 Service 단에서 생성하도록 수정
    회원가입 입력 데이터 기반 User 객체 생성은 Service 단에서 이루어져야 한다고 판단하여 Controller에서 Service로 옮겼다

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import model.User;
 import validate.InputValidate;
 import webserver.RequestParser;
 
@@ -13,11 +12,6 @@ public class UserController {
     public String create(String userInfoQueryString) {
         InputValidate.validateUserInfo(userInfoQueryString);
         Map<String, String> userInfo = RequestParser.parseParameters(userInfoQueryString);
-        User user = new User(
-                userInfo.get("userId"),
-                userInfo.get("password"),
-                userInfo.get("name"),
-                userInfo.get("email"));
-        return userService.create(user);
+        return userService.create(userInfo);
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,10 +2,12 @@ package service;
 
 import db.Database;
 import model.User;
+import java.util.Map;
 
 public class UserService {
 
-    public String create(User user) {
+    public String create(Map<String, String> userInfo) {
+        User user = new User(userInfo.get("userId"), userInfo.get("password"), userInfo.get("name"), userInfo.get("email"));
         Database.addUser(user);
         return user.getUserId();
     }
