438d4d1802e15af16679adc74b2bd7bf5085ddf3
admin <admin@admin-MacBook-Pro-3.local>
2024-01-18 16:54:26 +0900

    Test: ParameterizedTest로 변경 & 테스트 케이스 추가
    [기존]
    
    - 각 테스트케이스를 테스트 안에서 모두 작성해주는 것이 코드의 중복이 심하다고 판단함
    
    - 기존에는 파일 요청에 관한 테스트만 진행했으나 컨트롤러의 개선으로 인해 API에 대한 테스트도 필요하다고 판단함
    
    [수정]
    
    - ParameterizedTest를 적용하여 코드의 중복이 적은 가독성이 좋은 테스트 작성함
    
    - API의 성공과 실패에 관한 테스트 추가함

--- src/test/java/controller/RequestDataControllerTest.java
+++ src/test/java/controller/RequestDataControllerTest.java
@@ -2,42 +2,84 @@ package controller;
 
 import data.RequestData;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class RequestDataControllerTest {
 
-    // URL에 해당하는 데이터가 있는 경우
-    @Test
-    @DisplayName("html 리소스 라우팅 테스트")
-    public void routeRequest_ValidResource() throws IOException {
+    static Stream<Object[]> provideResourcePaths() {
+        return Stream.of(
+                new Object[]{"/index.html", "200 /index.html"},
+                new Object[]{"/user/form.html", "200 /user/form.html"},
+                new Object[]{"/user/list.html", "200 /user/list.html"},
+                new Object[]{"/user/login.html", "200 /user/login.html"},
+                new Object[]{"/user/profile.html", "200 /user/profile.html"}
+        );
+    }
+
+    static Stream<Object[]> provideApiPaths() {
+        return Stream.of(
+                new Object[]{"/", "302 /index.html"},
+                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=asdf", "302 /index.html"}
+        );
+    }
+
+    static Stream<Object[]> provideInvalidSignup() {
+        return Stream.of(
+                new Object[]{"/user/create?userId=&password=asdf&name=asdf&email=asdf", "400 /error/badrequest.html"},
+                new Object[]{"/user/create?userId=asdf&password=&name=asdf&email=asdf", "400 /error/badrequest.html"},
+                new Object[]{"/user/create?userId=asdf&password=asdf&name=&email=asdf", "400 /error/badrequest.html"},
+                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=", "400 /error/badrequest.html"}
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideResourcePaths")
+    @DisplayName("리소스 라우팅 테스트")
+    public void routeRequest_Resource(String path, String expected) throws IOException {
         // Given
-        String pathHome = "/";
-        String pathIndex = "/index.html";
-        String pathForm = "/user/form.html";
-        String pathList = "/user/list.html";
-        String pathLogin = "/user/login.html";
-        String pathProfile = "/user/profile.html";
+        Map<String, String> map = new HashMap<>();
 
         // When
-        Map<String, String> map = new HashMap<>();
-        String redirectHome = RequestDataController.routeRequest(new RequestData("GET", pathHome, "HTTP/1.1", map));
-        String redirectIndex = RequestDataController.routeRequest(new RequestData("GET", pathIndex, "HTTP/1.1", map));
-        String redirectForm = RequestDataController.routeRequest(new RequestData("GET", pathForm, "HTTP/1.1", map));
-        String redirectList = RequestDataController.routeRequest(new RequestData("GET", pathList, "HTTP/1.1", map));
-        String redirectLogin = RequestDataController.routeRequest(new RequestData("GET", pathLogin, "HTTP/1.1", map));
-        String redirectProfile = RequestDataController.routeRequest(new RequestData("GET", pathProfile, "HTTP/1.1", map));
+        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
 
         // Then
-        assertThat(redirectHome).isEqualTo("/index.html");
-        assertThat(redirectIndex).isNull();
-        assertThat(redirectForm).isNull();
-        assertThat(redirectList).isNull();
-        assertThat(redirectLogin).isNull();
-        assertThat(redirectProfile).isNull();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideApiPaths")
+    @DisplayName("API 라우팅 테스트")
+    public void routeRequest_Api(String path, String expected) throws IOException {
+        // Given
+        Map<String, String> map = new HashMap<>();
+
+        // When
+        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidSignup")
+    @DisplayName("회원가입 부족한 파라미터 테스트")
+    public void routeRequest_InvalidSignup(String path, String expected) throws IOException {
+        // Given
+        Map<String, String> map = new HashMap<>();
+
+        // When
+        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
     }
 }
