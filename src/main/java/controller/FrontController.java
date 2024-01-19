aedf046cace28477bdfb97f14e477309a640ca7c
LSH <104637774+lsh2613@users.noreply.github.com>
2024-01-19 13:48:23 +0900

    [Feat] Resource Request 처리하여 페이지에 css 적용
    - 기존 페이지 렌더링만 적용되어 css 요청을 처리하는 로직 구현

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import controller.adapter.HandlerAdapter;
+import controller.adapter.StaticResourceHandlerAdapter;
 import controller.adapter.UserControllerHandlerAdapter;
 import controller.user.UserCreateController;
 import controller.user.UserFormController;
+import controller.user.UserLoginController;
 import model.Request;
 
 import java.io.IOException;
@@ -16,7 +18,7 @@ import java.util.Map;
 public class FrontController {
     private final Map<String, Object> handlerMappingMap = new HashMap<>();
     private final List<HandlerAdapter> handlerAdapters = new ArrayList<>();
-    static String DEFAULT_PAGE = "templates/index.html";
+    static String DEFAULT_PAGE = "/templates/index.html";
 
     public FrontController() {
         initHandlerMappingMap();
@@ -24,13 +26,21 @@ public class FrontController {
     }
 
     private void initHandlerMappingMap() {
+        handlerMappingMap.put("css", new StaticResourceController("css"));
+        handlerMappingMap.put("fonts", new StaticResourceController("fonts"));
+        handlerMappingMap.put("images", new StaticResourceController("images"));
+        handlerMappingMap.put("js", new StaticResourceController("js"));
+        handlerMappingMap.put("ico", new StaticResourceController("ico"));
+
         handlerMappingMap.put("/user/form.html", new UserFormController());
         handlerMappingMap.put("/user/create", new UserCreateController());
+        handlerMappingMap.put("/user/login.html", new UserLoginController());
 
         // qna 추가
     }
 
     private void initHandlerAdapters() {
+        handlerAdapters.add(new StaticResourceHandlerAdapter());
         handlerAdapters.add(new UserControllerHandlerAdapter());
 
         //qna 추가
@@ -38,7 +48,6 @@ public class FrontController {
 
     public void service(Request request, OutputStream out) throws IOException {
         Object handler = getHandler(request);
-
         if (handler == null) {
             View view = viewResolver(DEFAULT_PAGE);
             view.render(request, out);
@@ -51,11 +60,16 @@ public class FrontController {
         String viewName = mv.getViewName();
         View view = viewResolver(viewName);
 
+        if (adapter instanceof StaticResourceHandlerAdapter) {
+            StaticResourceController staticResourceController = (StaticResourceController) handler;
+            view.render(request, out, staticResourceController.getType());
+            return;
+        }
         view.render(request, out);
     }
 
     private View viewResolver(String viewName) {
-        return new View("./src/main/resources/" + viewName);
+        return new View("./src/main/resources" + viewName);
     }
 
     private HandlerAdapter getHandlerAdapter(Object handler) {
@@ -69,7 +83,33 @@ public class FrontController {
 
     private Object getHandler(Request request) {
         String requestURI = request.getURI();
+        if (isResourceURI(requestURI)) {
+            return handlerMappingMap.get(extractResourceType(requestURI));
+        }
         return handlerMappingMap.get(requestURI);
+    }
+
+    private String extractResourceType(String requestURI) {
+        if (requestURI.contains("css"))
+            return "css";
+        if (requestURI.contains("fonts"))
+            return "fonts";
+        if (requestURI.contains("images"))
+            return "images";
+        if (requestURI.contains("js"))
+            return "js";
+        if (requestURI.contains("ico"))
+            return "ico";
+
+        return "";
+    }
+
+    private boolean isResourceURI(String uri) {
+        return uri.contains(".css")
+                || uri.contains("fonts")
+                || uri.contains(".images")
+                || uri.contains(".js")
+                || uri.contains(".ico");
     }
 
 }
