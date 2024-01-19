9da83efa7a2edbda5f6218998d2e68ab8bd20fa7
Yang-soeun <soeun8636@naver.com>
2024-01-19 10:01:49 +0900

    addUser 테스트 코드 추가

---
+++ src/test/java/db/DatabaseTest.java
@@ -0,0 +1,28 @@
+package db;
+
+import model.User;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DatabaseTest {
+
+    @DisplayName("userId, user를 맵으로 저장할 수 있다.")
+    @Test
+    void addUser(){
+        //given
+        User user = new User("user1", "1234", "test", "test@naver.com");
+
+        //when
+        Database.addUser(user);
+
+        //then
+        User findUser = Database.findUserById(user.getUserId());
+        assertThat(findUser)
+                .extracting("userId", "password", "name", "email")
+                .contains("user1", "1234", "test", "test@naver.com");
+    }
+
+}
