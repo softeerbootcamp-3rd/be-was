0d19d2ac74de1d0178124bb9c2eb0f9593909812
Yoon-Suji <jidl88@ewhain.net>
2024-01-19 10:56:49 +0900

    refactor: Controller 테스트 코드 수정 - Controller 코드 변경에 따른 테스트 코드 수정 - ParameterizedTest 도입

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -2,64 +2,50 @@ package controller;
 
 import dto.HttpRequestDto;
 import dto.HttpRequestDtoBuilder;
+import dto.HttpResponseDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
 public class UserControllerTest {
     private final UserController userController = new UserController();
 
-    @Test()
+    private final Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
+
+    @ParameterizedTest
+    @MethodSource("handleRequestParams")
     @DisplayName("UserController.handleRequest() Test")
-    public void handleRequestTest() {
+    public void handleRequestTest(String method, String uri, String expectedStatus) {
         // given
-        HttpRequestDto httpRequestDto = new HttpRequestDtoBuilder("GET", "/user/form.html", "HTTP/1.1").build();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(byteArrayOutputStream);
+        HttpRequestDto httpRequestDto = new HttpRequestDtoBuilder(method, uri, "HTTP/1.1").build();
 
         // when
-        userController.handleRequest(httpRequestDto, dos);
+        HttpResponseDto httpResponseDto = userController.handleRequest(httpRequestDto);
 
         // then
-        String response = byteArrayOutputStream.toString();
-        System.out.println(response);
-        Assertions.assertThat(response).contains("HTTP/1.1 200 OK");
+        logger.debug(httpRequestDto.toString());
+        logger.debug(new String(httpResponseDto.getBody()), StandardCharsets.UTF_8);
+        Assertions.assertThat(httpResponseDto.getStatus()).contains(expectedStatus);
     }
 
-    @Test()
-    @DisplayName("UserController.handleRequest() Test - createUser")
-    public void createUserTest() {
-        // given
-        HttpRequestDto httpRequestDto = new HttpRequestDtoBuilder("GET", "/user/create?userId=userId&password=password&name=name&email=softeer@example.com", "HTTP/1.1").build();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(byteArrayOutputStream);
-
-        // when
-        userController.handleRequest(httpRequestDto, dos);
-
-        // then
-        String response = byteArrayOutputStream.toString();
-        System.out.println(response);
-        Assertions.assertThat(response).contains("HTTP/1.1 302 Found");
-    }
-
-    @Test()
-    @DisplayName("UserController.handleRequest() Test - creatUser Fail")
-    public void createUserFailTest() {
-        // given
-        HttpRequestDto httpRequestDto = new HttpRequestDtoBuilder("GET", "/user/create?userId=userId&password=password", "HTTP/1.1").build();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(byteArrayOutputStream);
-
-        // when
-        userController.handleRequest(httpRequestDto, dos);
-
-        // then
-        String response = byteArrayOutputStream.toString();
-        System.out.println(response);
-        Assertions.assertThat(response).contains("HTTP/1.1 400 Bad Request");
+    public static Stream<Arguments> handleRequestParams() {
+        return Stream.of(
+                // 200 OK
+                Arguments.of("GET", "/user/form.html", "200"),
+                // 302 Found
+                Arguments.of("GET", "/user/create?userId=userId&password=password&name=name&email=softeer@example.com", "302"),
+                // 400 Bad Request: /user/create 시 필요한 파라미터를 모두 전달하지 않은 경우
+                Arguments.of("GET", "/user/create?userId=userId", "400")
+        );
     }
 }
