7b041ec80792bfb194a33e3279da6d97c5a45c2b
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-19 13:26:50 +0900

    Test(UserServiceTest) : 중복 회원가입 시, 예외처리 테스트
    회원가입 폼에서 입력한 사용자 아이디 값이 데이터베이스에 이미 있는 값이면 예외를 던져 다른 아이디로 회원가입하게 한다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import com.google.common.collect.Maps;
+import common.exception.DuplicateUserIdException;
 import db.Database;
 import model.User;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ import java.lang.reflect.Field;
 
 import static common.config.WebServerConfig.userService;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserServiceTest {
 
@@ -34,5 +36,19 @@ class UserServiceTest {
 
         //then
         assertThat(Database.findUserById(userId).getEmail()).isEqualTo("javajigi@slipp.net");
+    }
+
+    @Test
+    @DisplayName("중복 회원가입 예외처리 테스트 - 중복된 사용자 아이디가 존재하면 예외를 던진다.")
+    void duplicateUserCreate() {
+
+        //given
+        User user1 = new User("javajigi", "password1", "박재성1", "javajigi@slipp.net1");
+        User user2 = new User("javajigi", "password2", "박재성2", "javajigi@slipp.net2");
+
+        //when, then
+        userService.create(user1);
+        assertThatThrownBy(() -> userService.create(user2))
+                .isInstanceOf(DuplicateUserIdException.class);
     }
 }
