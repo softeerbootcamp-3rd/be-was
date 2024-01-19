9911362f20f3d5be1faa6ba236cacbd0aedceb3c
h-sooah <s2sooas2@ewhain.net>
2024-01-19 16:13:29 +0900

    remove : 사용하지 않는 HttpStatus 삭제
    현재 사용하지 않는 HttpStatus는 삭제. 추후 필요할 때 추가 예정.

--- src/main/java/webserver/HttpStatus.java
+++ src/main/java/webserver/HttpStatus.java
@@ -3,12 +3,8 @@ package webserver;
 public enum HttpStatus {
 
     OK(200, "Ok"),
-    CREATED(201, "Created"),
-    NO_CONTENT(204, "No_Content"),
     FOUND(302, "Redirect"),
     BAD_REQUEST(400, "Bad_Request"),
-    UNAUTHORIZED(401, "Unauthorized"),
-    FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not_Found"),
     INTERNAL_SERVER_ERROR(500, "Internal_Server_Error");
 
