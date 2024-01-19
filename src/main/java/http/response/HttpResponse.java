38b1009fcb2b5aa9eb2485e33093947012bc4e16
OSSU <o.ss.u2056@gmail.com>
2024-01-19 13:46:13 +0900

    remove: GeneralHeader import 제거

--- src/main/java/http/response/HttpResponse.java
+++ src/main/java/http/response/HttpResponse.java
@@ -2,7 +2,6 @@ package http.response;
 
 import http.ContentType;
 import http.HttpStatus;
-import http.request.GeneralHeader;
 import http.request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
