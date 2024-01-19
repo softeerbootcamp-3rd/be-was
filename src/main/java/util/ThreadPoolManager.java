930f264f023ba538de7f57b6f0347a94f01cb72b
LEE, TAEGON <taegon1998@gmail.com>
2024-01-19 20:19:53 +0900

    rename: RequestHandler 네이밍 변경에 따른 executorService 호출 코드 수정
    RequestHandler -> DispatcherServlet 네이밍 변경에 따라, DispatcherServlet 인스턴스 생성 코드 수정

--- src/main/java/util/ThreadPoolManager.java
+++ src/main/java/util/ThreadPoolManager.java
@@ -2,7 +2,7 @@ package util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.RequestHandler;
+import webserver.DispatcherServlet;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -13,7 +13,7 @@ import java.util.concurrent.Executors;
 public final class ThreadPoolManager {
 
     private static final int THREAD_POOL_SIZE = 100;
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
     public static void startServer(int port) { // 스레드 풀을 생성하고 스레드를 실행
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
@@ -23,7 +23,7 @@ public final class ThreadPoolManager {
 
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                executorService.execute(new RequestHandler(connection)); // 스레드 풀에서 스레드를 가져와서 실행
+                executorService.execute(new DispatcherServlet(connection)); // 스레드 풀에서 스레드를 가져와서 실행
             }
         } catch (IOException e) {
             logger.error("Error while starting the server: {}", e.getMessage());
