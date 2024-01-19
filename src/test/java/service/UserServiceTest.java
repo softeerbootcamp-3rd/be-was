326621657a2d05065e2a081032643c2a31e1b412
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-19 13:25:08 +0900

    Fix(UserServiceTest) : 테스트 실행 후 데이터베이스 초기화 기능 구현
    데이터베이스를 초기화하지 않으면 전체 테스트를 실행시켰을 때 값이 누적되어 오류가 발생한다. 개별 테스트 실행 후 데이터베이스를 초기화하는 기능을 추가해 오류를 해결했다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,14 +1,26 @@
 package service;
 
+import com.google.common.collect.Maps;
 import db.Database;
 import model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 import static common.config.WebServerConfig.userService;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserServiceTest {
+
+    @AfterEach
+    void tearDown() throws NoSuchFieldException, IllegalAccessException {
+        Class<Database> databaseClass = Database.class;
+        Field userDatabase = databaseClass.getDeclaredField("users");
+        userDatabase.setAccessible(true);
+        userDatabase.set(databaseClass, Maps.newHashMap());
+    }
 
     @Test
     @DisplayName("회원가입 성공 테스트")
