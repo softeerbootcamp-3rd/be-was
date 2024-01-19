4b495fbe42167333a1e82ba0fb39901b3fff471f
Yang-soeun <soeun8636@naver.com>
2024-01-19 17:43:29 +0900

    Revert .
    This reverts commit ba3d18e9427d292f19e41810a346f3608018554d.

--- src/main/java/webserver/ResponseEnum.java
+++ src/main/java/webserver/ResponseEnum.java
@@ -5,8 +5,7 @@ public enum ResponseEnum {
     CSS("css", "src/main/resources/static", "text/css"),
     JS("js", "src/main/resources/static","application/javascript"),
     FONT("fonts", "src/main/resources/static","font/ttf"),
-    IMAGE("images","src/main/resources/static" , "image/*"),
-    ICON("ico", "src/main/resources/static", "image/x-icon");
+    IMAGE("images","src/main/resources/static" , "image/*");
     private final String extension;
     private final String pathName;
     private final String contentType;
