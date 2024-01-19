aed8d3e23ee3d01006e7f5b1e066bd1d6534b6e3
kelly1422 <kelly1422@naver.com>
2024-01-19 16:45:35 +0900

    [step-3]: 에러 상태코드 숫자 수정

--- src/main/java/constant/HttpStatus.java
+++ src/main/java/constant/HttpStatus.java
@@ -5,7 +5,7 @@ public enum HttpStatus {
     CREATED ("201", "Created"),
     REDIRECT ("302", "Found"),
     BAD_REQUEST ("400", "Bad Request"),
-    NOT_FOUND ("401", "Not Found"),
+    NOT_FOUND ("404", "Not Found"),
     INTERNAL_SERVER_ERROR ("500", "Internal Server Error");
 
     private final String code;
