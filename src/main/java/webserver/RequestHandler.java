dfa82b5ea79642781605b0ce29bb118cd6a6d507
seojeewon <readyjiwon@gmail.com>
2024-01-19 17:05:08 +0900

    Refactor: RequestHandler 리팩토링
    가입 요청과 일반 페이지 요청을 isUserRegistrationRequest 메서드를 통해 분류했습니다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,23 +2,29 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Map;
 
 import dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
-import utils.FileReader;
-import utils.HttpRequestLogger;
-import utils.RequestParser;
-import utils.HTTPResponseSender;
+import utils.FileLoader;
+import utils.HttpRequestParser;
+import utils.HttpResponseSender;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private static final UserService userService = new UserService();
     private Socket connection;
+    private static String USER_CREATE_PATH = "/user/create";
+    private static final String INDEX_HTML_PATH = "/index.html";
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+    }
+
+    private boolean isUserRegistrationRequest(String path) {
+        return path.contains(USER_CREATE_PATH);
     }
 
     public void run() {
@@ -28,22 +34,21 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-
-            HttpRequestLogger.logHttpRequest(in);
+            HttpRequestParser parser = new HttpRequestParser();
+            Map<String, String> map = parser.parseHttpRequest(in);
+            HttpRequest httpRequest = new HttpRequest(map);
+            HttpResponseSender responseSender = new HttpResponseSender();
+            logger.debug(httpRequest.toString());
 
             //TODO 파일을 읽어오는 요청이 아닌 경우(예: 회원가입의 가입 버튼)
-            String path = RequestParser.getPath();
-            //가입 저장 요청
-            if (path.contains("user/create")) {
-                UserDTO userInfo = RequestParser.parseNewUserInfo();
-                String userId = userService.registerUser(userInfo);
-                logger.debug("New User Registered! User Id: {}", userId);
-                HTTPResponseSender.redirectToHomePage(dos);
-            } else {   //html, js, css 등 페이지 요청
-                //파일 읽기
-                byte[] body = FileReader.readFile(RequestParser.getPath());
-                //HTTP Response
-                HTTPResponseSender.sendHttpResponse(dos, body.length, body);
+            String path = httpRequest.getPath();
+            if (isUserRegistrationRequest(path)) {
+                userService.registerUser(httpRequest.toUserDto());
+                logger.debug("New User Registered!");
+                responseSender.redirectToHomePage(dos);
+            } else {
+                byte[] body = FileLoader.loadFileContent(path);
+                responseSender.sendHttpResponse(dos, body.length, body);
             }
 
         } catch (IOException e) {
