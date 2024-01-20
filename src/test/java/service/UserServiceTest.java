985bbddb33376cc57a6a0dddc291a869ff5cc044
Yoon-Suji <jidl88@ewhain.net>
2024-01-20 23:59:05 +0900

    fix: UserService 테스트 코드 수정 - UserDto 클래스 삭제 및 UserService 코드 변경에 따른 UserService 테스트 코드 수정 - createUserFailTest() 메서드 추가: IllegalArgumentException 에러가 발생하는 케이스 - Database 테스트 코드 수정: addUserTest() 확인 방법(then) 수정, findByUserIdTest() 메서드 추가

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,11 +1,13 @@
 package service;
 
 import db.Database;
-import dto.UserDto;
 import model.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserServiceTest {
     public static final UserService userService = new UserService();
@@ -14,18 +16,35 @@ public class UserServiceTest {
     @DisplayName("UserService.createUser() test")
     public void createUserTest() {
         // given
-        UserDto userDto = new UserDto("userId", "password", "name", "softeer@example.com");
+        Map<String, String> createUserParams = new HashMap<>();
+        createUserParams.put("userId", "TestUserId");
+        createUserParams.put("password", "TestPassword");
+        createUserParams.put("name", "TestUser");
+        createUserParams.put("email", "test@example.com");
 
         // when
-        userService.createUser(userDto);
+        userService.createUser(createUserParams);
 
         // then
-        User user = Database.findUserById("userId");
-        Assertions.assertThat(user.getUserId()).isEqualTo(userDto.getUserId());
-        Assertions.assertThat(user.getPassword()).isEqualTo(userDto.getPassword());
-        Assertions.assertThat(user.getEmail()).isEqualTo(userDto.getEmail());
-        Assertions.assertThat(user.getName()).isEqualTo(userDto.getName());
+        User user = Database.findUserById("TestUserId");
+        Assertions.assertThat(user.getUserId()).isEqualTo(createUserParams.get("userId"));
+        Assertions.assertThat(user.getPassword()).isEqualTo(createUserParams.get("password"));
+        Assertions.assertThat(user.getEmail()).isEqualTo(createUserParams.get("email"));
+        Assertions.assertThat(user.getName()).isEqualTo(createUserParams.get("name"));
+    }
 
-        Database.deleteUserById(userDto.getUserId()); // 테스트 종료 후 데이터 정리
+    @Test()
+    @DisplayName("UserService.createUser() Fail Case: IllegalArgumentException")
+    public void createUserFailTest() {
+        // given
+        Map<String, String> createUserParams = new HashMap<>();
+        createUserParams.put("userId", "TestUserId");
+        createUserParams.put("password", "TestPassword");
+        createUserParams.put("name", "TestUser");
+
+        // when & then
+        Assertions.assertThatThrownBy(() -> userService.createUser(createUserParams))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Invalid Parameters");
     }
 }
