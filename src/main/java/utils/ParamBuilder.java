2f1c078d715de1a9cc238085f16fc756671cd006
seonghwan <aiya0422@gmail.com>
2024-01-20 16:08:14 +0900

    ParamBuilder 클래스 주석 추가
    - getParams 함수의 설명을 추가했습니다.

--- src/main/java/utils/ParamBuilder.java
+++ src/main/java/utils/ParamBuilder.java
@@ -5,6 +5,12 @@ import java.util.Map;
 
 public class ParamBuilder {
 
+    /**
+     * 요청 url에서 쿼리의 키와 값을 분리해 반환합니다.
+     *
+     * @param url 요청 url
+     * @return 분리한 쿼리의 키와 값 (Map)
+     */
     public static Map<String, String> getParams(String url) {
         Map<String, String> params = new HashMap<>();
 
