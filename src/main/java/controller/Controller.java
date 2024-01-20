87841244e70d9ca273f348807fcf9cd9b301c329
seonghwan <aiya0422@gmail.com>
2024-01-20 13:06:01 +0900

    Controller 인터페이스 주석 추가
    - route 함수의 주석을 추가했습니다.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,5 +4,10 @@ import model.Response;
 
 public interface Controller {
 
+    /**
+     * url에 따라 적절한 처리를 하는 함수를 호출합니다.
+     * @param url 요청 타겟
+     * @return 요청 작업을 수행한 결과를 담은 객체
+     */
     Response route(String url);
 }
