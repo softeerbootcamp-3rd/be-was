820d2fb49e9dddc497fec6827cf4b390a41fdbef
seonghwan <aiya0422@gmail.com>
2024-01-20 13:25:18 +0900

    HomeController 주석 추가
    - 각 함수를 설명하는 주석을 추가했습니다.
    - route 함수는 인터페이스에 추가되어있기 때문에 별도로 주석을
      작성하지 않았습니다.

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-public class HomeController implements Controller{
+public class HomeController implements Controller {
 
     public Response route(String url) {
         if (url.equals("/")) {
@@ -17,10 +17,23 @@ public class HomeController implements Controller{
         return getStatic(url);
     }
 
+    /**
+     * /index.html로 리다이렉트하는 응답을 반환합니다.
+     *
+     * @return /index.html 리다이렉션 응답
+     */
     private Response redirectIndex() {
         return new Response(302, "/index.html");
     }
 
+    /**
+     * index.html 페이지 파일을 찾아 응답을 반환합니다.
+     *
+     * <p> 요청한 파일을 찾을 수 있는 경우 200 응답을 반환하며, 요청한 파일을 찾을 수 없는 경우 404 응답을 반환합니다.
+     *
+     * @param url 요청 타겟
+     * @return 요청을 수행한 결과를 담은 응답
+     */
     private Response getIndex(String url) {
         String filePath = "src/main/resources/templates/index.html";
 
@@ -33,6 +46,14 @@ public class HomeController implements Controller{
         }
     }
 
+    /**
+     * 페이지 외의 파일을 찾아 응답을 반환합니다.
+     *
+     * <p> 요청한 파일을 찾을 수 있는 경우 200 응답을 반환하며, 요청한 파일을 찾을 수 없는 경우 404 응답을 반환합니다.
+     *
+     * @param url 요청 타겟
+     * @return 요청을 수행한 결과를 담은 응답
+     */
     private Response getStatic(String url) {
         String filePath = "src/main/resources/static" + url;
 
