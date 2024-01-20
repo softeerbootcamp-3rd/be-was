18f28a4481660ba6afa784fce0b1eabb029e6694
seonghwan <aiya0422@gmail.com>
2024-01-20 17:07:47 +0900

    UserService Test 추가
    - 기존의 DisplayName으로는 테스트 내용을 명확하게 설명하지 못한다고
      판단했기 때문에 각 상황에 맞춰서 여러 테스트를 추가했습니다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -13,25 +13,62 @@ class UserServiceTest {
     private final UserService userService = new UserService();
 
     @Test
-    @DisplayName("회원가입")
+    @DisplayName("회원가입 정상처리")
     void saveUser() {
         // given
-        Map<String, String> case1 = Map.of("userId", "user", "password", "pwd", "name", "user",
-                "email", "user@email");
-        Map<String, String> case2 = Map.of("userId", "user", "password", "pwd", "name", "user",
+        Map<String, String> userParams = Map.of("userId", "user", "password", "pwd", "name", "user",
                 "email", "user@email");
 
         // when
-        userService.saveUser(case1);
-        User result1 = Database.findUserById(case1.get("userId"));
-
-        Throwable result2 = catchThrowable(() -> userService.saveUser(case2));
+        userService.saveUser(userParams);
 
         // then
-        assertThat(result1.getUserId()).as("사용자 아이디 일치").isEqualTo(case1.get("userId"));
-        assertThat(result1.getPassword()).as("패스워드 일치").isEqualTo(case1.get("password"));
-        assertThat(result1.getName()).as("이름 일치").isEqualTo(case1.get("name"));
-        assertThat(result1.getEmail()).as("이메일 일치").isEqualTo(case1.get("email"));
-        assertThat(result2).as("같은 아이디로 회원가입하는 경우 ").isInstanceOf(IllegalArgumentException.class);
+        User result = Database.findUserById(userParams.get("userId"));
+        assertThat(result.getUserId()).as("사용자 아이디 일치").isEqualTo(userParams.get("userId"));
+        assertThat(result.getPassword()).as("패스워드 일치").isEqualTo(userParams.get("password"));
+        assertThat(result.getName()).as("이름 일치").isEqualTo(userParams.get("name"));
+        assertThat(result.getEmail()).as("이메일 일치").isEqualTo(userParams.get("email"));
+    }
+
+    @Test
+    @DisplayName("중복 회원가입")
+    void saveUserDuplication() {
+        // given
+        Map<String, String> userParams1 = Map.of("userId", "user2", "password", "pwd", "name", "user",
+                "email", "user@email");
+        Map<String, String> userParams2 = Map.of("userId", "user2", "password", "pwd", "name", "user",
+                "email", "user@email");
+        userService.saveUser(userParams1);
+
+        // when
+        Throwable result = catchThrowable(() -> userService.saveUser(userParams2));
+
+        // then
+        assertThat(result).as("같은 아이디로 회원가입하는 경우 오류 발생")
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("회원가입 파라미터 부족")
+    void saveUserNullParam() {
+        // given
+        Map<String, String> userParams1 = Map.of("password", "pwd", "name", "user", "email",
+                "user@email");
+        Map<String, String> userParams2 = Map.of("userId", "user", "name", "user", "email", "user@email");
+        Map<String, String> userParams3 = Map.of("userId", "user", "password", "pwd", "email",
+                "user@email");
+        Map<String, String> userParams4 = Map.of();
+
+        // when
+        Throwable result1 = catchThrowable(() -> userService.saveUser(userParams1));
+        Throwable result2 = catchThrowable(() -> userService.saveUser(userParams2));
+        Throwable result3 = catchThrowable(() -> userService.saveUser(userParams3));
+        Throwable result4 = catchThrowable(() -> userService.saveUser(userParams4));
+
+        // then
+        assertThat(result1).as("아이디가 비어있는 경우 오류 발생").isInstanceOf(NullPointerException.class);
+        assertThat(result2).as("패스워드가 비어있는 경우 오류 발생").isInstanceOf(NullPointerException.class);
+        assertThat(result3).as("이름이 비어있는 경우 오류 발생").isInstanceOf(NullPointerException.class);
+        assertThat(result4).as("모두 비어있는 경우 오류 발생").isInstanceOf(NullPointerException.class);
     }
 }
