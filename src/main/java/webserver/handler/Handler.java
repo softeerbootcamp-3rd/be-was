8678310ea23c51638bdf73e71e6ff69837777aa0
johan1103 <johan1103@naver.com>
2024-01-22 00:52:25 +0900

    feat: 자원에 맞는 요청을 수행할 Handler 인터페이스 추가 기존의 로직은 모든 요청을 Controller 탐색 후 수행하는 흐름이었지만, static자원 요청의 경우 해당 흐름과 다르게 resource path로부터 자원을 읽어서 반환해야 함. 요청한 자원에 맞는 로직을 처리하기 위해 Handler인터페이스를 생성

--- src/main/java/webserver/handler/Handler.java
+++ src/main/java/webserver/handler/Handler.java
@@ -1,2 +1,9 @@
-package webserver.handler;public interface Handler {
+package webserver.handler;
+
+import webserver.MyHttpServletRequest;
+import webserver.MyHttpServletResponse;
+
+public interface Handler {
+  MyHttpServletResponse handle(MyHttpServletRequest request);
+  boolean canHandle(MyHttpServletRequest request);
 }
