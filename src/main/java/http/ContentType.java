e7594e142fde8ffc419d6444e6ea4d87c4dbb352
OSSU <o.ss.u2056@gmail.com>
2024-01-21 23:09:10 +0900

    feat: 폰트 관련 content type 추가

--- src/main/java/http/ContentType.java
+++ src/main/java/http/ContentType.java
@@ -11,9 +11,14 @@ public enum ContentType {
     JS("application/x-javascript"),
     PNG("image/png"),
     ICO("image/x-icon"),
+    EOT("application/vnd.ms-fontobject"),
+    SVG("image/svg+xml"),
+    TTF("font/ttf"),
+    WOFF("font/woff"),
+    WOFF2("font/woff2"),
     PLAIN_TEXT("text/plain");
 
-    private static final Map<String, ContentType> CONTENT_TYPE = new HashMap<>();
+    private static Map<String, ContentType> CONTENT_TYPE = new HashMap<>();
 
     static {
         CONTENT_TYPE.put("html", HTML);
@@ -21,6 +26,11 @@ public enum ContentType {
         CONTENT_TYPE.put("js", JS);
         CONTENT_TYPE.put("png", PNG);
         CONTENT_TYPE.put("ico", ICO);
+        CONTENT_TYPE.put("eot", EOT);
+        CONTENT_TYPE.put("svg", SVG);
+        CONTENT_TYPE.put("ttf", TTF);
+        CONTENT_TYPE.put("woff", WOFF);
+        CONTENT_TYPE.put("woff2", WOFF2);
         CONTENT_TYPE.put("txt", PLAIN_TEXT);
     }
 
