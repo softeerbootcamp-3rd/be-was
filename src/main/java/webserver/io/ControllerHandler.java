df7a2721d1a0e8ef5ebbd9f41839091bc81e93d0
johan1103 <johan1103@naver.com>
2024-01-22 00:57:19 +0900

    feat: RequestHandler에서 ControlelrHandler 로직 분리 기존의 로직은 RequestHandler에서 ControllerHandler의 흐름을 수행했지만 StaticResourceHandler의 등장으로 상황에 맞는 Handler가 수행되어야 하는 상황. 따라서 Handler의 흐름은 각 Handler에게 맏기고 RequestHandler에서는 handler를 찾고 handler에게 이후 동작을 위임

--- src/main/java/webserver/io/ControllerHandler.java
+++ src/main/java/webserver/io/ControllerHandler.java
@@ -1,19 +1,51 @@
 package webserver.io;
 
+import webserver.MyHttpServletRequest;
 import webserver.MyHttpServletResponse;
+import webserver.handler.Handler;
+import webserver.mapper.ControllerMapper;
+import webserver.mapper.ParameterMapper;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class ControllerHandler {
+public class ControllerHandler implements Handler {
   private Object controllerInstance;
   private Method method;
   private Object[] args;
 
-  public MyHttpServletResponse handleController() throws InvocationTargetException, IllegalAccessException {
+  private static final ControllerMapper controllerMapper = new ControllerMapper();
+  private static final ParameterMapper parameterMapper = new ParameterMapper();
+
+  @Override
+  public MyHttpServletResponse handle(MyHttpServletRequest request){
+    //ControllerMapper로부터 요청을 수행할 수 있는 Method를 가져온다.
+    controllerMapper.findAppropriateControllerMethod(this, request.getUri());
+    //ParameterMapper로부터 Method의 파라미터들을 MyHttpServletRequest가 가지고 있는 쿼리 파라미터에서 추출해 주입한다.
+    parameterMapper.findAppropriateParameter(this, request);
+    //Method를 실행할 수 있는 조건(실행할 Method, Method에 필요한 파라미터값, 실행할 Method를 가지고 있는 객체)이
+    //다 갖춰졌기 때문에 해당 Method를 ControllerHandler가 실행한다.
+    return handleController();
+  }
+
+  @Override
+  public boolean canHandle(MyHttpServletRequest request) {
+    return !request.getUri().contains(".") || request.getUri().contains("html");
+  }
+
+  public MyHttpServletResponse handleController(){
     String controllerReturnValue="";
     //controller method를 실행하고 string값을 받음 (redirect uri 혹은 resource path)
-    controllerReturnValue = (String) method.invoke(controllerInstance, args);
+    try {
+      controllerReturnValue = (String) method.invoke(controllerInstance, args);
+    }catch (InvocationTargetException targetException){
+      if (targetException.getTargetException().getClass().isAssignableFrom(RuntimeException.class))
+        throw (RuntimeException) targetException.getTargetException();
+      else
+        throw new RuntimeException();
+    }catch (IllegalAccessException accessException){
+      throw new RuntimeException();
+    }
     return new MyHttpServletResponse(controllerReturnValue);
   }
 
--- src/main/java/webserver/io/RequestHandler.java
+++ src/main/java/webserver/io/RequestHandler.java
@@ -4,26 +4,27 @@ import java.io.*;
 import java.net.Socket;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.exception.ExceptionResolver;
 import webserver.MyHttpServletRequest;
 import webserver.MyHttpServletResponse;
-import webserver.mapper.ControllerMapper;
-import webserver.mapper.ParameterMapper;
+import webserver.handler.Handler;
+import webserver.handler.StaticResourceHandler;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private HttpResponseBuilder responseBuilder;
-    private final ControllerHandler controllerHandler = new ControllerHandler();
-    private final ControllerMapper controllerMapper = new ControllerMapper();
-    private final ParameterMapper parameterMapper = new ParameterMapper();
     private final ExceptionResolver exceptionResolver = new ExceptionResolver();
+    private final List<Handler> handlers = new ArrayList<>();
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        handlers.add(new StaticResourceHandler());
+        handlers.add(new ControllerHandler());
     }
 
     public void run() {
@@ -36,17 +37,18 @@ public class RequestHandler implements Runnable {
 
             MyHttpServletResponse httpResponse = null;
             try {
-                //ControllerMapper로부터 요청을 수행할 수 있는 Method를 가져온다.
-                controllerMapper.findAppropriateControllerMethod(controllerHandler, httpServletRequest.getUri());
-                //ParameterMapper로부터 Method의 파라미터들을 MyHttpServletRequest가 가지고 있는 쿼리 파라미터에서 추출해 주입한다.
-                parameterMapper.findAppropriateParameter(controllerHandler, httpServletRequest);
-                //Method를 실행할 수 있는 조건(실행할 Method, Method에 필요한 파라미터값, 실행할 Method를 가지고 있는 객체)이
-                //다 갖춰졌기 때문에 해당 Method를 ControllerHandler가 실행한다.
-                httpResponse = controllerHandler.handleController();
+                Handler handler = null;
+                for(Handler h : handlers){
+                    if(h.canHandle(httpServletRequest))
+                        handler=h;
+                }
+                if(handler==null)
+                    throw new RuntimeException();
+                httpResponse = handler.handle(httpServletRequest);
             }catch (Exception e){
                 httpResponse = exceptionResolver.resolve(e);
             }
-            this.responseBuilder= new HttpResponseBuilder(new DataOutputStream(out));
+            HttpResponseBuilder responseBuilder = new HttpResponseBuilder(new DataOutputStream(out));
             //responseBuilder에게 socket에 http응답정보를 작성하도록 요청한다.
             responseBuilder.flushHttpResponse(httpResponse);
 
