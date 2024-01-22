f3791ed69be73f6f46cfa9c5a13263520f4ecd3d
binaryrain97 <binaryrain97@gmail.com>
2024-01-22 21:29:12 +0900

    step4 완료 requestHandler 리팩토링 완료 Util 클래스 활용해서 파싱 정적 메소드 구현 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -17,6 +17,9 @@ public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private static final HashSet<String> printedKey =  // logger.debug로 출력할 헤더값들
             new HashSet<>(Arrays.asList("Accept", "Host", "User-Agent", "Cookie"));
+    private static final HashMap<String, String> statusCodeMessage = new HashMap<>() {{
+        put("200", "OK"); put("302", "Found"); put("404", "Not Found"); }};
+
     private Socket connection;
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -40,53 +43,21 @@ public class RequestHandler implements Runnable {
         }
     }
     private void handleResponse(DataOutputStream dos, Response response) {
+        responseHeader(dos, response);
+        if(response.getBody() != null)
+            responseBody(dos, response.getBody());
+    }
+    private void responseHeader(DataOutputStream dos, Response response) {
         String statusCode = response.getStatusCode();
-        if(statusCode.equals("200")) {
-            byte[] body = response.getBody();
-            String mimeType = response.getMimeType();
-            response200Header(dos, mimeType, body.length);
-            responseBody(dos, body);
-        }
-        else if(statusCode.equals("302")) {
-            response302Header(dos, response.getRedirectUrl());
-        }
-        else if(statusCode.equals("404")){
-            byte[] body = response.getBody();
-            String mimeType = response.getMimeType();
-            response404Header(dos, mimeType, body.length);
-            responseBody(dos, body);
-        }
-    }
-
-    private void response200Header(DataOutputStream dos,
-                                   String mimeType,
-                                   int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + mimeType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String redirectUrl) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: " + redirectUrl);
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-    private void response404Header(DataOutputStream dos,
-                                   String mimeType,
-                                   int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
-            dos.writeBytes("Content-Type: " + mimeType + ";charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("HTTP/1.1 " + statusCode + " " + statusCodeMessage.get(statusCode) + " \r\n");
+            if(response.getBody() != null) {
+                dos.writeBytes("Content-Type: " + response.getMimeType() + ";charset=utf-8\r\n");
+                dos.writeBytes("Content-Length: " + response.getBody().length + "\r\n");
+            }
+            else if(response.getRedirectUrl() != null){
+                dos.writeBytes("Location: " + response.getRedirectUrl());
+            }
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
