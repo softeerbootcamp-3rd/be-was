75419d800c7118167e04a3de395c95786f8f1491
seojeewon <readyjiwon@gmail.com>
2024-01-22 11:49:36 +0900

    Test: HttpRequestParser가 Body도 잘 뽑아내는지 테스트
    given-when-then 구조로 parser가 요청의 바디를 잘 뽑아내는지 테스트.
    또한 GET 요청과 같은 빈 바디의 경우에도 빈 바디로 잘 저장하는지 테스트

--- src/test/java/RequestParserTest.java
+++ src/test/java/RequestParserTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import utils.HttpRequestParser;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -9,37 +11,43 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class RequestParserTest {
-    private static InputStream validRequestStream;
-    @BeforeAll
-    static void init() throws IOException {
+    @Test
+    void messageParsing() throws IOException {
+        //given
+        String requestMessage = "POST /user/create HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Content-Length: 59\r\n" +
+                "Content-Type: application/x-www-form-urlencoded\r\n" +
+                "Accept: */*\r\n" +
+                "\r\n" +
+                "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+        InputStream in = new ByteArrayInputStream(requestMessage.getBytes());
+        String body = "userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net";
+        String length = "59";
 
-        StringBuilder validRequest = new StringBuilder();
-        validRequest.append("GET /index.html HTTP/1.1\n")
-                .append("Host: localhost:8080\n")
-                .append("Connection: keep-alive\n")
-                .append("Cache-Control: max-age=0\n")
-                .append("sec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"\n")
-                .append("sec-ch-ua-mobile: ?0\n")
-                .append("sec-ch-ua-platform: \"macOS\"\n")
-                .append("Upgrade-Insecure-Requests: 1\n")
-                .append("User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36\n")
-                .append("Accept: text/html,application/xhtml+xml,application/xml\n")
-                .append("Sec-Fetch-Site: same-origin\n")
-                .append("Sec-Fetch-Mode: navigate\n")
-                .append("Sec-Fetch-User: ?1\n")
-                .append("Sec-Fetch-Dest: document\n")
-                .append("Referer: http://localhost:8080/user/login.html\n")
-                .append("Accept-Encoding: gzip, deflate, br\n")
-                .append("Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7\n")
-                .append("Cookie: Idea-69015365=c84b7ca8-76bc-4946-8364-2ed31c35b3d4")
-                .append("\r\n\r\n")
-                .append("12345678");
-//        String validRequest = "GET /user/create?userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net HTTP/1.1\n" +
-//                "Host: localhost:8080" +
-//                "Connection: keep-allive" +
-//                "Accept: */*";
-        validRequestStream = new ByteArrayInputStream(validRequest.toString().getBytes());
+        //when
+        HttpRequestParser parser = new HttpRequestParser();
+
+        //then
+        assertThat(parser.parseHttpRequest(in).get("Body")).isEqualTo(body);
 
     }
-    
+
+    @Test
+    void emptyBodyParsing() throws IOException {
+        //given
+        String requestMessage = "GET /index.html HTTP/1.1\r\n"
+                + "Host: localhost:8080\r\n"
+                + "Connection: keep-alive\r\n"
+                + "Accept: */*\r\n\r\n";
+        InputStream in = new ByteArrayInputStream(requestMessage.getBytes());
+
+        //when
+        HttpRequestParser parser = new HttpRequestParser();
+
+        //then
+        assertThat(parser.parseHttpRequest(in).get("Body")).isEmpty();
+    }
+
 }
