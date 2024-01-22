4803dca950a4d7bed0a0f4e6159b9e51985e50ec
99winnmin <ojysep9987@gmail.com>
2024-01-23 00:56:04 +0900

    :sparkles: Feat : FrontController 패턴 추가
    "구현 내용 :
    1. JVM 에 FrontController 가 load 될 때 @RequestMapping 이 붙은 모든 컨트롤러 메서드를 등록함
    2. 'GET:/user/info' 와 같은 형식이 key, 매핑되는 메서드를 HashMap 으로 선언하여 타켓 method 를 빠르게 찾을 수 있도록 함
    3. 메서드를 invoke 할 때 Java Reflection 과 커스텀 Annotation 을 활용하여 동적으로 파라미터를 구성하여 넘기도록 구성"

---
+++ src/main/java/webserver/container/FrontController.java
@@ -0,0 +1,113 @@
+package webserver.container;
+
+import common.annotation.RequestBody;
+import common.annotation.RequestMapping;
+import common.annotation.RequestParam;
+import common.http.request.HttpMethod;
+import common.http.response.HttpStatusCode;
+import domain.user.presentation.UserController;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class FrontController {
+
+    private static final Set<Class<?>> CONTROLLERS = new HashSet<>();
+    private static final HashMap<String, Method> CONTROLLER_METHODS = new HashMap<>();
+
+    private FrontController() {
+    }
+
+    private static class SingletonHelper {
+
+        private static final FrontController SINGLETON = new FrontController();
+    }
+
+    public static FrontController getInstance() {
+        return SingletonHelper.SINGLETON;
+    }
+
+    static {
+        CONTROLLERS.add(UserController.class);
+        initializeControllerMethods();
+    }
+
+    private static void initializeControllerMethods() {
+        for (Class<?> controllerClass : CONTROLLERS) {
+            Method[] methods = controllerClass.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(RequestMapping.class)) {
+                    RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                    String path = requestMapping.path();
+                    HttpMethod httpMethod = requestMapping.method();
+
+                    String key = path + ":" + httpMethod;
+                    CONTROLLER_METHODS.put(key, method);
+                }
+            }
+        }
+    }
+
+    public Method findControllerMethod(HttpMethod method, String path) {
+        String key = path + ":" + method;
+        if (CONTROLLER_METHODS.containsKey(key)) {
+            return CONTROLLER_METHODS.get(key);
+        }
+
+        throw new IllegalArgumentException("Not found controller method");
+    }
+
+    public void invokeFunc(Method method, Map<String, String> params, Map<String, String> body) {
+        try {
+            Class<?> targetClass = method.getDeclaringClass();
+            Object newInstance = targetClass.getDeclaredConstructor().newInstance();
+
+            Parameter[] parameters = method.getParameters();
+            Object[] parameterValues = new Object[parameters.length];
+
+            for (int i = 0; i < parameters.length; i++) {
+                Parameter parameter = parameters[i];
+
+                if (parameter.isAnnotationPresent(RequestBody.class)) {
+                    parameterValues[i] = mapToRequestDto(method, body);
+                } else if (parameter.isAnnotationPresent(RequestParam.class)) {
+                    Parameter param = parameters[i];
+                    String paramName = param.getName();
+                    parameterValues[i] = params.get(paramName);
+                } else {
+                    String paramName = parameter.getName();
+                    parameterValues[i] = params.get(paramName);
+                }
+            }
+
+            method.invoke(newInstance, parameterValues);
+        } catch (Exception e) {
+            ResponseThreadLocal.onFailure(HttpStatusCode.BAD_REQUEST, null, "Bad Request".getBytes());
+        }
+
+    }
+
+    private Object mapToRequestDto(Method method, Map<String, String> body)
+        throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        Parameter[] parameters = method.getParameters();
+        Parameter parameter = parameters[0];
+
+        Class<?> dtoClass = parameter.getType();
+        Constructor<?> dtoClassDeclaredConstructor = dtoClass.getDeclaredConstructors()[0];
+
+        Parameter[] dtoConstructorParameters = dtoClassDeclaredConstructor.getParameters();
+        Object[] parameterValues = new Object[dtoConstructorParameters.length];
+
+        for (int i = 0 ; i<dtoConstructorParameters.length ; i++) {
+            parameterValues[i] = body.get(dtoConstructorParameters[i].getName());
+        }
+
+        return dtoClassDeclaredConstructor.newInstance(parameterValues);
+    }
+
+}
