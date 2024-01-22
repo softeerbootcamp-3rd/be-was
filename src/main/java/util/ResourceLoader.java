8ec505e9ece2560140a55690cd5e1e4eccb9b33f
admin <admin@admin-MacBook-Pro-3.local>
2024-01-22 22:29:24 +0900

    Feat: 파일을 nio가 아닌 io로 읽도록 수정
    기존에는 파일을 nio를 읽어옴
    
    개발자가 직접 버퍼 크기를 설정하고 파일 크기만큼 바이트 배열을 생성하여 파일을 읽도록 수정함

--- src/main/java/util/ResourceLoader.java
+++ src/main/java/util/ResourceLoader.java
@@ -4,9 +4,10 @@ import controller.ResourcePathMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -24,7 +25,24 @@ public class ResourceLoader {
         String extension = getFileExtension(resourcePath);
         String directory = ResourcePathMapping.getDirectory(extension);
 
-        return Files.readAllBytes(Paths.get( url + directory + resourcePath));
+        File file = new File(url + directory + resourcePath);
+
+        try (InputStream inputStream = new FileInputStream(file);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+
+            byte[] buffer = new byte[(int) file.length()];
+            bufferedInputStream.read(buffer);
+            return buffer;
+        }
+//         라인 단위로 읽는 코드
+//        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+//            StringBuilder content = new StringBuilder();
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                content.append(line).append(System.lineSeparator());
+//            }
+//            return content.toString().getBytes(StandardCharsets.UTF_8);
+//        }
     }
 
     public static String getResourceType(String targetUrl) {
