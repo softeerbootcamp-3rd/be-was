d9bd9eef9fdb2334b4fabc44a993e626a0931697
seojeewon <readyjiwon@gmail.com>
2024-01-22 11:47:38 +0900

    Feat: HttpRequestParser에서 요청의 바디도 추출하도록 로직 추가
    빈 공백이 나올 때까지 헤더를 읽고, 다음 바디를 읽음.
    parseHttpRequest는 바디까지 포함된 요청의 해시맵 반환

--- src/main/java/utils/HttpRequestParser.java
+++ src/main/java/utils/HttpRequestParser.java
@@ -10,30 +10,39 @@ import java.util.Map;
 
 
 public class HttpRequestParser {
-    public HttpRequestParser(){
+    public HttpRequestParser() {
     }
 
     public Map<String, String> parseHttpRequest(InputStream in) throws IOException {
-        Map<String, String> header = new HashMap<>();
+        Map<String, String> messageElement = new HashMap<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         //첫 번째 라인에서 메서드, 경로 추출
         String firstLine = reader.readLine();
         String[] firstLineParts = firstLine.split(" ");
-        header.put("Method", firstLineParts[0]);
-        header.put("Path", firstLineParts[1]);
-        header.put("HTTP version", firstLineParts[2]);
+        messageElement.put("Method", firstLineParts[0]);
+        messageElement.put("Path", firstLineParts[1]);
+        messageElement.put("HTTP version", firstLineParts[2]);
         //다른 헤더 추출
         String line;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+        while (!(line = reader.readLine()).isEmpty()) {
             String[] headerParts = line.split(": ");
             if (headerParts.length == 2) {
-                header.put(headerParts[0], headerParts[1]);
+                messageElement.put(headerParts[0], headerParts[1]);
             }
         }
-        //TODO: 닫으면 왜 소켓연결이 끊기는지 공부하기
-        //reader.close();
-        return header;
+
+        // 바디 추출
+        StringBuilder bodyBuilder = new StringBuilder();
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            bodyBuilder.append(line).append("\n");
+        }
+        //마지막 개행 문자 제거
+        if (bodyBuilder.length() > 0) {
+            bodyBuilder.setLength(bodyBuilder.length() - 1);
+        }
+        messageElement.put("Body", bodyBuilder.toString());
+        return messageElement;
     }
 
 }
