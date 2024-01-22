464504db4e22c96d5b296bd8b67144977fd25f68
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-22 18:03:19 +0900

    feat : use annotaion to map method

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,34 +1,33 @@
 package controller;
 
+import annotation.Controller;
+import annotation.GetMapping;
+import annotation.RequestMapping;
 import http.Request;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
 
+import java.lang.reflect.Method;
 import java.net.URLDecoder;
+import java.util.HashMap;
 import java.util.Map;
 
-public class UserController implements Controller{
+@Controller
+@RequestMapping("/user")
+public class UserController implements BasicController {
     private final UserService userService = UserService.getInstance();
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private Map<String, Method> getMap = new HashMap<>();
+    private Map<String, Method> postMap = new HashMap<>();
 
-    @Override
-    public String process(Request req, Map<String, String> model) {
-        logger.debug(new StringBuilder("[MemberFormController.process] req.getUrl() : ").append(req.getUrl()).toString());
-        String subPath = req.getUrl().replace("/user/", "");
-        switch (subPath) {
-            case "create":
-                return createUser(req, model);
-            // Add more cases for other sub-paths as needed
-            default:
-                logger.warn("Unknown subPath: {}", subPath);
-                return null;
-        }
-
+    public UserController(){
     }
 
-    private String createUser(Request req, Map<String, String> model){
+    @GetMapping(url = "/create")
+    public String createUser(Request req, Map<String, String> model){
+        logger.debug("createUser started");
         try{
             User user = new User();
             Map<String, String> paramMap = req.getRequestParam();
