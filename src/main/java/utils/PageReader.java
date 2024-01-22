261bea6eab1f6e20ecd246c2d6a42883a2ebc5b3
seonghwan <aiya0422@gmail.com>
2024-01-22 11:19:00 +0900

    PageReader 클래스의 함수 주석 추가
    함수를 설명하기 위해 주석을 추가했습니다.

--- src/main/java/utils/PageReader.java
+++ src/main/java/utils/PageReader.java
@@ -7,6 +7,15 @@ import model.Response;
 
 public class PageReader {
 
+    /**
+     * 페이지 파일을 불러오는 함수입니다.
+     *
+     * <p> 요청한 파일을 찾을 수 있는 경우 200 응답을 반환하며, 요청한 파일을 찾을 수 없는 경우 404 응답을 반환합니다.
+     *
+     * @param url 요청 타겟
+     * @param filePath 파일 경로
+     * @return 파일을 불러온 결과를 담은 응답
+     */
     public static Response getPage(String url, String filePath) {
         File file = new File(filePath);
 
