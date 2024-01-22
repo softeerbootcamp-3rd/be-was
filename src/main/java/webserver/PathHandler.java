ed5ff6db8cc179a47e0b1a9ae5610545504d8184
win9 <krrr883@gmail.com>
2024-01-22 17:12:39 +0900

    [Feat] 메소드 매핑
    path별 controller의 메소드를 매핑하여 함수 인터페이스를 이용해 if없이 통일성있는 구성을 할 수 있도록 변경

--- src/main/java/webserver/PathHandler.java
+++ src/main/java/webserver/PathHandler.java
@@ -4,18 +4,19 @@ import controller.UserController;
 import dto.ResourceDto;
 import model.QueryParams;
 
-import java.io.IOException;
+import java.util.function.Function;
 
 public class PathHandler {
 
-    public static ResourceDto responseResource(String method, String path, UserController controller) throws IOException {
+    public static ResourceDto responseResource(String method, String path, UserController controller) {
         if (controller != null) {
+            Function<Object, ResourceDto> correctMethod = controller.getCorrectMethod(path);
             if (method.equals("GET") && path.contains("?")) {
                 String[] splits = path.split("\\?");
                 QueryParams queryParams = QueryParams.from(splits[1]);
-                return controller.process(queryParams);
+                return correctMethod.apply(queryParams);
             } else if (method.equals("GET")) {
-                return controller.process(path);
+                return correctMethod.apply(path);
             }
         }
 
