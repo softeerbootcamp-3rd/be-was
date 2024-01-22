13ea9260c58bf03f84b59ff1684dbd7e1cd4f949
johan1103 <johan1103@naver.com>
2024-01-22 17:15:41 +0900

    fix: controllerMapper에게 요청할 때, http method정보도 같이 전달

--- src/main/java/webserver/handler/ControllerHandler.java
+++ src/main/java/webserver/handler/ControllerHandler.java
@@ -20,7 +20,7 @@ public class ControllerHandler implements Handler {
   @Override
   public MyHttpServletResponse handle(MyHttpServletRequest request){
     //ControllerMapper로부터 요청을 수행할 수 있는 Method를 가져온다.
-    controllerMapper.findAppropriateControllerMethod(this, request.getUri());
+    controllerMapper.findAppropriateControllerMethod(this, request.getUri(), request.getMethod());
     //ParameterMapper로부터 Method의 파라미터들을 MyHttpServletRequest가 가지고 있는 쿼리 파라미터에서 추출해 주입한다.
     parameterMapper.findAppropriateParameter(this, request);
     //Method를 실행할 수 있는 조건(실행할 Method, Method에 필요한 파라미터값, 실행할 Method를 가지고 있는 객체)이
