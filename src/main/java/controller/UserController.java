2d4b0a521d68693a734c57d53238b40b7cc09aa1
insiderhj <insiderhj@gmail.com>
2024-01-22 13:57:46 +0900

    feat: 로그인 시 session 생성해서 header에 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -6,6 +6,7 @@ import annotation.RequestMapping;
 import annotation.RequestParam;
 import db.Database;
 import model.User;
+import util.SessionManager;
 import webserver.HttpResponse;
 import constant.HttpStatus;
 
@@ -15,6 +16,7 @@ public class UserController {
     @RequestMapping(method = "POST", path = "/user/create")
     public static HttpResponse createUser(@RequestBody User user) {
         User existUser = Database.findUserById(user.getUserId());
+
         if (existUser != null)
             return HttpResponse.builder()
                     .status(HttpStatus.CONFLICT)
@@ -24,6 +26,25 @@ public class UserController {
         Database.addUser(user);
         return HttpResponse.builder()
                 .status(HttpStatus.FOUND)
+                .addHeader("Location", "/index.html")
+                .build();
+    }
+
+    @RequestMapping(method = "POST", path = "/user/login")
+    public static HttpResponse login(@RequestBody User user) {
+        User existUser = Database.findUserById(user.getUserId());
+        if (existUser == null || !existUser.getPassword().equals(user.getPassword()))
+            return HttpResponse.builder()
+                    .status(HttpStatus.FOUND)
+                    .addHeader("Location", "/user/login_failed.html")
+                    .build();
+
+        String sessionId = SessionManager.generateSessionId();
+        SessionManager.addSession(sessionId, user);
+
+        return HttpResponse.builder()
+                .status(HttpStatus.FOUND)
+                .addHeader("Set-Cookie", "SID=" + sessionId + "; Path=/; HttpOnly")
                 .addHeader("Location", "/index.html")
                 .build();
     }
