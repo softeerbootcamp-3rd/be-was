0c58c1f98fe771decfe1b3cedb5661d18499c8cf
johan1103 <johan1103@naver.com>
2024-01-22 17:14:48 +0900

    feat: HttpBody정보를 Controller에서 받고 싶을 때 사용하는 annotation 작성 파라미터단에서 동작하는 어노테이션, Controller의 메서드 파라미터에 붙이면 해당 파라미터는 Body정보로부터 정보를 대입받음

--- src/main/java/webserver/controller/RequestBody.java
+++ src/main/java/webserver/controller/RequestBody.java
@@ -1,2 +1,11 @@
-package webserver.controller;public @interface RequestBody {
+package webserver.controller;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface RequestBody {
 }
