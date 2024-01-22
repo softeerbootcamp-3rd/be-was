c1a85676d60c54f6d1dbf20a9b26921671a07e8a
seonghwan <aiya0422@gmail.com>
2024-01-22 13:26:02 +0900

    getParamFromBody 함수 구현
    - 쿼리 스트링을 재사용할 수도 있기 때문에 request body로 파라미터 객체를 만드는 새로운 함수를 구현했습니다.
    - 중복되는 코드가 발견되어 유지보수성을 높이기 위해 해당 코드를 별도의 함수로 구현했습니다.

--- src/main/java/utils/ParamBuilder.java
+++ src/main/java/utils/ParamBuilder.java
@@ -11,10 +11,22 @@ public class ParamBuilder {
      * @param url 요청 url
      * @return 분리한 쿼리의 키와 값 (Map)
      */
-    public static Map<String, String> getParams(String url) {
+    public static Map<String, String> getParamFromUrl(String url) {
         Map<String, String> params = new HashMap<>();
 
         String[] splitQuery = url.split("\\?")[1].split("&");
+        return getParamMap(splitQuery, params);
+    }
+
+    public static Map<String, String> getParamFromBody(String body) {
+        Map<String, String> params = new HashMap<>();
+
+        String[] splitQuery = body.split("&");
+        return getParamMap(splitQuery, params);
+    }
+
+    private static Map<String, String> getParamMap(String[] splitQuery,
+            Map<String, String> params) {
         for (String query : splitQuery) {
             String[] split = query.split("=", -1);
             params.put(split[0], split[1]);
