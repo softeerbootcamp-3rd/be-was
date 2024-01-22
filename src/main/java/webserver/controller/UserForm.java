6799c06c89e7b3a20c2fe11eda4265cea71246d9
johan1103 <johan1103@naver.com>
2024-01-22 17:17:26 +0900

    feat: Http Body를 담을 수 있는 UserForm 작성

--- src/main/java/webserver/controller/UserForm.java
+++ src/main/java/webserver/controller/UserForm.java
@@ -1,2 +1,23 @@
-package webserver.controller;public class UserForm {
+package webserver.controller;
+
+public class UserForm {
+  private String userId;
+  private String password;
+  private String email;
+  private String name;
+
+  public UserForm(){
+  }
+  public String getUserId(){
+    return this.userId;
+  }
+  public String getPassword(){
+    return this.password;
+  }
+  public String getEmail(){
+    return this.email;
+  }
+  public String getName(){
+    return this.name;
+  }
 }
