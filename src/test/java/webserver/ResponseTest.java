442fdb9cce3daad574882503d85bc100d1fbee55
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-22 10:42:39 +0900

    Test(ResponseTest) : MimeType 도출 테스트
    메서드 파라미터로 Path를 전달하면 MimeType을 반환한다.

---
+++ src/test/java/webserver/ResponseTest.java
@@ -0,0 +1,24 @@
+package webserver;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class ResponseTest {
+
+    @Test
+    @DisplayName("Path에서 MimeType을 추출한다")
+    void getMimeType() throws IOException {
+
+        //given
+        String path = "/css/bootstrap.min.css";
+
+        //when
+        String mimeType = Response.getMimeType(path);
+
+        //then
+        Assertions.assertThat(mimeType).isEqualTo("text/css");
+    }
+}
