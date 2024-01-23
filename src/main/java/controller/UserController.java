c0cf32d5f2ce375f14caa9ac278083165d8e6d07
Yoon-Suji <jidl88@ewhain.net>
2024-01-23 18:28:52 +0900

    feat: UserController.loginUser() 메소드 추가
    - /user/login POST 요청을 loginUser 메서드로 매핑하는 코드 추가
    - loginUser() 메소드 구현: userService.loginUser() 메서드를 호출하고 성공 여부에 따라 적절한 응답 반환

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,6 +2,7 @@ package controller;
 
 import dto.HttpRequestDto;
 import dto.HttpResponseDto;
+import dto.HttpResponseDtoBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
@@ -25,6 +26,9 @@ public class UserController implements Controller {
         if (request.getMethod().equals("POST") && request.getUri().startsWith("/user/create")) {
             return createUser(request);
         }
+        if (request.getMethod().equals("POST") && request.getUri().startsWith("/user/login")) {
+            return loginUser(request);
+        }
         return getPage(request, logger);
     }
 
@@ -38,6 +42,24 @@ public class UserController implements Controller {
             logger.error(e.getMessage());
 
             return HttpResponseUtil.response400();
+        }
+    }
+
+    public HttpResponseDto loginUser(HttpRequestDto request) {
+        HttpResponseDtoBuilder responseDtoBuilder = new HttpResponseDtoBuilder();
+        try {
+            Map<String, String> parameters = WebUtil.parseRequestBody(request.getBody());
+            String sessionId = userService.loginUser(parameters);
+
+            return responseDtoBuilder.response302Header()
+                    .setHeaders("Location", "/index.html")
+                    .setCookie("sid", sessionId, new String[]{"Path=/"})
+                    .build();
+        } catch (IllegalArgumentException e) {
+            logger.error(e.getMessage());
+
+            return responseDtoBuilder.response302Header()
+                    .setHeaders("Location", "/user/login_failed.html").build();
         }
     }
 }
