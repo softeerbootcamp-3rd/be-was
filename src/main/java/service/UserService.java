032506bcd5cbc5208c122c04a3c26cfee1699550
seojeewon <readyjiwon@gmail.com>
2024-01-23 15:28:39 +0900

    Feat: 유효한 로그인인지 확인 후 세션 생성 & 세션 ID 전달
    userService의 isValidUser를 통해 유효한 로그인인지 확인.
    유효한 로그인인 경우 userService에서 세션 생성 후 만들어진 세션 저장 후 반환.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,8 +1,14 @@
 package service;
 
 import db.Database;
+import dto.Login;
 import dto.UserDTO;
+import model.Session;
 import model.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class UserService {
     public String registerUser(UserDTO userInfo){
@@ -12,5 +18,18 @@ public class UserService {
         User newbie = new User(userInfo.getUserId(), userInfo.getPassword(), userInfo.getName(), userInfo.getEmail());
         Database.addUser(newbie);
         return userInfo.getUserId();
+    }
+
+    public boolean isValidUser(Login loginInfo){
+        User target = Database.findUserById(loginInfo.getUserId());
+        return target != null && target.getPassword().equals(loginInfo.getPassword());
+    }
+
+    public Session login(Login loginInfo){
+        //Session Id 만들기
+        String sessionId = UUID.randomUUID().toString();
+        Session newSession = new Session(sessionId, loginInfo.getUserId(), LocalDateTime.now());
+        Database.addSession(newSession);
+        return newSession;
     }
 }
