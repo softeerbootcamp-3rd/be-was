2b407ed90c82e3fdfd3bd05f0f4bb3599c162f3d
johan1103 <johan1103@naver.com>
2024-01-23 18:46:50 +0900

    feat: Cookie 값 읽고 초기화 및 remove 작업 기능 추가
    ThreadLocal로 존재하기 때문에 remove메서드가 필수임.

--- src/main/java/webserver/io/RequestHandler.java
+++ src/main/java/webserver/io/RequestHandler.java
@@ -15,6 +15,7 @@ import webserver.MyHttpServletResponse;
 import webserver.handler.ControllerHandler;
 import webserver.handler.Handler;
 import webserver.handler.StaticResourceHandler;
+import webserver.session.CustomSession;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,6 +35,7 @@ public class RequestHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             MyHttpServletRequest httpServletRequest = printReceivedRequest(in);
+            CustomSession.initCurrentSession(httpServletRequest);
             logger.debug("http request : {}",httpServletRequest.toString());
 
             MyHttpServletResponse httpResponse = null;
@@ -49,12 +51,14 @@ public class RequestHandler implements Runnable {
             }catch (Exception e){
                 httpResponse = exceptionResolver.resolve(e);
             }
+            httpResponse.addCookieHeader();
             HttpResponseBuilder responseBuilder = new HttpResponseBuilder(new DataOutputStream(out));
             //responseBuilder에게 socket에 http응답정보를 작성하도록 요청한다.
             responseBuilder.flushHttpResponse(httpResponse);
-
         } catch (IOException e) {
             logger.error(e.getMessage());
+        } finally {
+            CustomSession.finishCurrentSession();
         }
     }
 
