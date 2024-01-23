b487d1c140ebe92286846885975b30c5053230ba
Yang-soeun <soeun8636@naver.com>
2024-01-23 10:31:17 +0900

    [TEST]: HttpRequestTest - POST 테스트 추가
    POST 요청이 들어오는 경우 파싱을 적절하게 하는지 테스트

--- src/test/java/model/HttpRequestTest.java
+++ src/test/java/model/HttpRequestTest.java
@@ -1,36 +1,67 @@
 package model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class HttpRequestTest {
 
-    @DisplayName("request를 적절할게 파싱해준다.")
+    @DisplayName("request를 적절할게 파싱해준다. - GET")
     @Test
-    void httpRequestFrom() throws IOException {
+    void httpRequestFromWithGET() throws IOException {
         //given
-        String mockRequest = "GET /index.html HTTP/1.1\n" +
-                "Host: localhost:8080\n" +
-                "Connection: keep-alive\n" +
-                "Accept: */*\n" +
-                "Content-Length: 23\n\n" +
-                "request body";
-
+        String mockRequest = createMockRequestWithGET();
         InputStream inputStream = new ByteArrayInputStream(mockRequest.getBytes());
 
         //when
         HttpRequest httpRequest = HttpRequest.from(inputStream);
 
         //then
-        Assertions.assertThat(httpRequest)
-                .extracting("method", "uri", "host", "connection", "accept")
-                .contains("GET", URI.create("/index.html"), "localhost:8080", "keep-alive", "*/*");
+        String httpRequestToString = httpRequest.toString();
+        assertThat(httpRequestToString).isEqualTo("HttpRequest{\n"+
+                "method=GET,\n" +
+                "uri=/index.html,\n" +
+                "headers={Accept:=*/*, Host:=localhost:8080, Connection:=keep-alive},\n" +
+                "body=null\n}");
     }
 
+    @DisplayName("request를 적절하게 파싱해준다. - POST")
+    @Test
+    void httpRequestFromWithPOST() throws IOException {
+        //given
+        String mockRequest = createMockRequestWithPOST();
+        InputStream inputStream = new ByteArrayInputStream(mockRequest.getBytes());
+
+        //when
+        HttpRequest httpRequest = HttpRequest.from(inputStream);
+
+        //then
+        String httpRequestToString = httpRequest.toString();
+        assertThat(httpRequestToString).isEqualTo("HttpRequest{" + "\n" +
+                "method=POST,\n" +
+                "uri=/user/create,\n" +
+                "headers={Accept:=*/*, Content-Length:=59, Host:=localhost:8080, Connection:=keep-alive},\n" +
+                "body=userId=test1&password=1234&name=test&email=test@naver.com\n}");
+    }
+
+    private static String createMockRequestWithPOST() throws IOException {
+        return "POST /user/create HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Accept: */*\n" +
+                "Content-Length: 59\n\n" +
+                "userId=test1&password=1234&name=test&email=test@naver.com";
+    }
+
+    private static String createMockRequestWithGET() throws IOException {
+        return "GET /index.html HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Accept: */*\n\n";
+    }
 }
