6b8bd8ffc6ec5d9bcc405b8820d87c3dc081319a
LEE, TAEGON <taegon1998@gmail.com>
2024-01-23 15:44:04 +0900

    refactor: HttpResponse 클래스에 있던 getContentType 메소드를 HttpRequest에 작성
    변경전: HttpResponse class에 Content-Type을 반환해주는 getContentType 메소드 작성
    변경후: HttpRequest class에 Content-Type을 반환해주는 getContentType 메소드 작성

--- src/main/java/response/HttpResponse.java
+++ src/main/java/response/HttpResponse.java
@@ -1,31 +1,26 @@
 package response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static util.MimeType.getMimeType;
 
 public class HttpResponse {
-    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
     private final String contentType;
     private final String redirectUri;
     private final byte[] body;
 
-    public HttpResponse(String path, String redirectUrl) {
-        this.contentType = getContentType(path);
-        this.redirectUri = redirectUrl;
+    public HttpResponse(String contentType, String redirectUri) {
+        this.contentType = contentType;
+        this.redirectUri = redirectUri;
         this.body = null;
     }
 
     public HttpResponse(String path) throws IOException {
         this.contentType = getContentType(path);
         this.redirectUri = null;
-        this.body = readFileInBytes(path);
+        this.body = readFileInBytes(Path.of(path));
     }
 
     public Integer getBodyLength() {
@@ -44,24 +39,12 @@ public class HttpResponse {
         return body;
     }
 
-    private static byte[] readFileInBytes(String path) throws IOException { // 파일을 읽어서 byte[]로 반환
-        Path filePath;
-
-        if (path.endsWith(".html")) {
-            filePath = Paths.get("src/main/resources/templates", path);
-        } else {
-            filePath = Paths.get("src/main/resources/static", path);
-        }
-
-        try {
-            return Files.readAllBytes(filePath);
-        } catch (IOException e) {
-            logger.error("Error reading resource: {}", path);
-            throw e;
-        }
+    private static byte[] readFileInBytes(Path filePath) throws IOException { // 파일을 읽어서 byte[]로 반환
+        return Files.readAllBytes(filePath);
     }
 
     private static String getContentType(String file) { // 파일의 확장자에 따라 Content-Type을 결정
+        System.out.println(file);
         String extension = file.substring(file.lastIndexOf("."));
         return getMimeType(extension);
     }
