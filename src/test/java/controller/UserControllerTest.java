a10143604d33cecf680bc51e9820caba1d0fe579
Yoon-Suji <jidl88@ewhain.net>
2024-01-23 18:35:05 +0900

    test: UserController.createUser() 테스트 코드 작성
    - loginUserTest(): 로그인이 성공한 경우 /index.html로 리다이렉트하는 응답을 sessionId와 함께 리턴
    - loginUserFailTest(): 로그인이 실패한 경우 /user/login_failed.html로 리다이렉트하는 응답을 리턴
    - DisplayName의 값이 테스트를 더 잘 설명하도록 수정

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -19,7 +19,7 @@ public class UserControllerTest {
     private final UserController userController = new UserController(userService);
 
     @Test
-    @DisplayName("createUser() Test : Success case - UserService.createUser()가 성공한 경우")
+    @DisplayName("createUser(): 유저 생성이 성공한 경우 /index.html로 리다이렉트하는 응답을 리턴한다")
     public void createUserTest() {
         // given
         HttpRequestDto request = new HttpRequestDtoBuilder("POST", "/user/create", "HTTP/1.1").build();
@@ -40,7 +40,7 @@ public class UserControllerTest {
     }
 
     @Test
-    @DisplayName("createUser() Test : Fail case - UserService가 IllegalArgumentException 던진 경우")
+    @DisplayName("createUser(): 유저 생성이 실패한 경우 400 Bad Request 응답을 리턴한다")
     public void createUserFailTest() {
         // given
         HttpRequestDto request = new HttpRequestDtoBuilder("POST", "/user/create", "HTTP/1.1").build();
@@ -56,5 +56,48 @@ public class UserControllerTest {
 
         // then
         Assertions.assertThat(httpResponseDto.getStatus()).isEqualTo("400");
+    }
+
+    @Test
+    @DisplayName("loginUser(): 로그인이 성공한 경우 /index.html로 리다이렉트하는 응답을 sessionId와 함께 리턴한다")
+    public void loginUserTest() {
+        // given
+        HttpRequestDto request = new HttpRequestDtoBuilder("POST", "/user/login", "HTTP/1.1").build();
+        Mockito.doReturn("sessionId").when(userService).loginUser(Mockito.any());
+
+        // when
+        HttpResponseDto httpResponseDto;
+        try (MockedStatic<WebUtil> webUtil = Mockito.mockStatic(WebUtil.class)) {
+            webUtil.when(() -> WebUtil.parseRequestBody(Mockito.any()))
+                    .thenReturn(Collections.emptyMap());
+            httpResponseDto = userController.handleRequest(request);
+        }
+
+        // then
+        Assertions.assertThat(httpResponseDto.getStatus()).isEqualTo("302");
+        Assertions.assertThat(httpResponseDto.getMessage()).isEqualTo("Found");
+        Assertions.assertThat(httpResponseDto.getHeaders().get("Location")).isEqualTo("/index.html");
+        Assertions.assertThat(httpResponseDto.getHeaders().get("Set-Cookie")).isEqualTo("sid=sessionId; Path=/");
+    }
+
+    @Test
+    @DisplayName("loginUser(): 로그인이 실패한 경우 /user/login_failed.html로 리다이렉트하는 응답을 리턴한다")
+    public void loginUserFailTest() {
+        // given
+        HttpRequestDto request = new HttpRequestDtoBuilder("POST", "/user/login", "HTTP/1.1").build();
+        Mockito.doThrow(new IllegalArgumentException("Invalid Parameters")).when(userService).loginUser(Mockito.any());
+
+        // when
+        HttpResponseDto httpResponseDto;
+        try (MockedStatic<WebUtil> webUtil = Mockito.mockStatic(WebUtil.class)) {
+            webUtil.when(() -> WebUtil.parseRequestBody(Mockito.any()))
+                    .thenReturn(Collections.emptyMap());
+            httpResponseDto = userController.handleRequest(request);
+        }
+
+        // then
+        Assertions.assertThat(httpResponseDto.getStatus()).isEqualTo("302");
+        Assertions.assertThat(httpResponseDto.getMessage()).isEqualTo("Found");
+        Assertions.assertThat(httpResponseDto.getHeaders().get("Location")).isEqualTo("/user/login_failed.html");
     }
 }
