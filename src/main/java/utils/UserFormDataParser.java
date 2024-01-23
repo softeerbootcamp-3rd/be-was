a9f220fe9ea2af9afcf6bbf1fbca3dd49d601a05
admin <admin@admins-MacBook-Pro-6.local>
2024-01-23 14:09:09 +0900

    fix : step4 요구사항중 nio->io
    nio를 사용하는 부분 제거 블로킹, 논블로킹 학습

--- src/main/java/utils/UserFormDataParser.java
+++ src/main/java/utils/UserFormDataParser.java
@@ -1,7 +1,7 @@
 package utils;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,7 +17,13 @@ public class UserFormDataParser {
                 .map(it -> it.split("=", 2))
                 .collect(Collectors.toMap(
                         it -> it[0],
-                        it -> URLDecoder.decode(it.length > 1 ? it[1] : "", StandardCharsets.UTF_8),
+                        it -> {
+                            try {
+                                return URLDecoder.decode(it.length > 1 ? it[1] : "", "UTF-8");
+                            } catch (UnsupportedEncodingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        },
                         (existing, replacement) -> existing
                 ));
     }
--- src/main/java/webserver/http/ResponseHandler.java
+++ src/main/java/webserver/http/ResponseHandler.java
@@ -4,29 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 
 public class ResponseHandler {
+
     private static final Logger logger = LoggerFactory.getLogger(ResponseHandler.class);
     private static final String ROOT_DIRECTORY = System.getProperty("user.dir");
     private static final String TEMPLATE_DIRECTORY ="/src/main/resources/templates";
     private static final String STATIC_DIRECTORY ="/src/main/resources/static";
     public byte[] setResponseBody(Mime responseMimeType, String requestTarget) {
-        byte[] responseBody;
-        try{
-            if(responseMimeType==Mime.TEXT_HTML){
-                responseBody =  Files.readAllBytes(new File(ROOT_DIRECTORY + TEMPLATE_DIRECTORY + requestTarget).toPath());
-            }
-            else if(responseMimeType == Mime.NONE)
+        byte[] responseBody = new byte[0];
+        try {
+            File file = null;
+            if (responseMimeType == Mime.TEXT_HTML) {
+                file = new File(ROOT_DIRECTORY + TEMPLATE_DIRECTORY + requestTarget);
+            } else if (responseMimeType == Mime.NONE) {
                 responseBody = new byte[0];
-            else{
-                responseBody = Files.readAllBytes(new File(ROOT_DIRECTORY + STATIC_DIRECTORY + requestTarget).toPath());
+            } else {
+                file = new File(ROOT_DIRECTORY + STATIC_DIRECTORY + requestTarget);
             }
-        }
-        catch (IOException e) {
+
+            if (file != null && file.exists()) {
+                FileInputStream fis = new FileInputStream(file);
+                responseBody = new byte[(int) file.length()];
+                fis.read(responseBody);
+                fis.close();
+            }
+        } catch (IOException e) {
             logger.error(e.getMessage());
-            responseBody = new byte[0];
         }
         return responseBody;
     }
