7cfd258d2314fb199d3c44054c7df30f3f1e5e2e
LEE, TAEGON <taegon1998@gmail.com>
2024-01-23 16:06:25 +0900

    feat: Reflection을 사용하여 Request body를 동적으로 parsing
    1. getDeclaredConstructor API를 사용하여 private 기본 생성자를 받아옴
    2. 해당하는 class의 인스턴스 생성
    3. getDeclaredFields API를 사용하여 해당 클래스의 필드를 모두 가져옴
    4. @Column Annotation이 있는 필드에 파싱된 Request body 값들을 주입

---
+++ src/main/java/util/Parser.java
@@ -0,0 +1,48 @@
+package util;
+
+import annotation.Column;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+
+    public static <T> T jsonParser(Class<T> clazz, String body) throws Exception {
+        Constructor<T> constructor = clazz.getDeclaredConstructor();
+        constructor.setAccessible(true);
+
+        T object = constructor.newInstance();
+        Field[] fields = clazz.getDeclaredFields();
+
+        Map<String, String> keyValueMap = parseJson(body);
+
+        setField(object, fields, keyValueMap);
+        return object;
+    }
+
+    private static Map<String, String> parseJson(String body) {
+        String[] jsons = body.split("&");
+        Map<String, String> keyValueMap = new HashMap<>();
+
+        for (String s : jsons) {
+            String[] keyValue = s.split("=");
+            keyValueMap.put(keyValue[0], keyValue[1]);
+        }
+        return keyValueMap;
+    }
+
+    private static void setField(Object object, Field[] fields, Map<String, String> keyValueMap) throws Exception {
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Column.class)) { // @Column 어노테이션이 있는 필드만 필드값 설정
+                String value = keyValueMap.get(field.getName());
+
+                if (value == null) throw new Exception("필드명이 잘못된 유효하지 않은 json 문자열입니다.");
+
+                field.setAccessible(true);
+                field.set(object, value);
+            }
+        }
+    }
+}
