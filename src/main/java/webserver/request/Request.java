71aff2324abfbe70436421351db48ffcd0a20ef7
Hymin2 <khm8246@gmail.com>
2024-01-23 10:04:02 +0900

    refactor: GetRequest 제거 후 Request 추가
    GET 요청에서만 사용되는 GetRequest를 삭제하고, 모든 메소드 요청에서 사용할 Request를 새롭게 추가하였습니다.

---
+++ src/main/java/webserver/request/Request.java
@@ -0,0 +1,63 @@
+package webserver.request;
+
+import webserver.header.RequestHeader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+    private final String method;
+    private final String path;
+    private final String protocol;
+    private final Map<String, String> headers;
+    private final Map<String, String> params;
+    private final Map<String, String> body;
+
+    protected Request(String method, String path, String protocol){
+        this.method = method;
+        this.path = path;
+        this.protocol = protocol;
+        this.headers = new HashMap<>();
+        this.params = new HashMap<>();
+        this.body = new HashMap<>();
+    }
+
+    public static Request of(String method, String path, String protocol){
+        return new Request(method, path, protocol);
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+    public String getMethod(){
+        return method;
+    }
+
+    public void setHeader(String key, String value){
+        headers.put(key, value);
+    }
+
+    public void setParam(String key, String value){
+        params.put(key, value);
+    }
+
+    public void setBody(String key, String value){
+        body.put(key, value);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n요청 메소드: %s\n" +
+                "요청한 자원의 경로: %s\n" +
+                "프로토콜: %s\n", method, path, protocol));
+
+        for(String key: headers.keySet()){
+            sb.append(String.format("%s: %s", key, headers.get(key))).append("\n");
+        }
+
+        return sb.toString();
+    }
+}
