50a1fe487cb772642926b74855375fa683ea87d3
admin <lejs1008@naver.com>
2024-01-23 22:41:29 +0900

    step-5 MemberLoginController 수정
    로그인 실패 시 로그인 실패 창으로, 성공 시 cookie에 세션 아이디를 담아서 반환하도록 구현

--- src/main/java/controller/MemberLoginController.java
+++ src/main/java/controller/MemberLoginController.java
@@ -5,6 +5,7 @@ import request.HttpRequest;
 import response.HttpResponse;
 import response.HttpResponseStatus;
 import service.MemberLoginService;
+import session.SessionManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.Map;
 public class MemberLoginController implements Controller{
 
     MemberLoginService memberLoginService = new MemberLoginService();
+    SessionManager sessionManager = new SessionManager();
 
     @Override
     public void process(HttpRequest request, HttpResponse response) {
@@ -21,11 +23,14 @@ public class MemberLoginController implements Controller{
         String password = request.getParams().get("password");
 
         User loginUser = memberLoginService.login(userId, password);
-        if (loginUser == null) { // 로그인 실패 시 다시 로그인 창으로
-            headers.put("Location", "/user/login.html");
+        if (loginUser == null) { // 로그인 실패 시 로그인 실패 창으로
+            headers.put("Location", "/user/login_failed.html");
             response.setResponse(HttpResponseStatus.FOUND, null, headers);
         } else { // 로그인 성공 시 홈으로
+            String sessionId = sessionManager.createSession(loginUser);
+
             headers.put("Location", "/index.html");
+            headers.put("set-cookie", "sid="+sessionId+"; Path=/");
             response.setResponse(HttpResponseStatus.FOUND, null, headers);
         }
     }
