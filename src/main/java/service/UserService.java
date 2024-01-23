aabd44dc41e2b30ee0d5f746d980c23f7a631d4e
Yoon-Suji <jidl88@ewhain.net>
2024-01-23 16:04:58 +0900

    feat: UserService.loginUser() 메서드 추가
    - 유저 데이터베이스 값과 비교해서 userId와 password가 일치하면 sessionId를 생성해 저장하고 반환
    - 필요한 파라미터를 모두 전달하지 않거나, 데이터베이스의 값과 일치하지 않는 경우 IllegalArgumentException throw

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -2,10 +2,13 @@ package service;
 
 import db.Database;
 import model.User;
+import util.SessionUtil;
 
 import java.util.Map;
 
 public class UserService {
+
+    private static final int SESSION_ID_LENGTH = 8;
 
     public void createUser(Map<String, String> createUserParams) {
         String userId = createUserParams.get("userId");
@@ -15,6 +18,24 @@ public class UserService {
 
         if (userId != null && password != null && name != null && email != null) {
             Database.addUser(new User(userId, password, name, email));
+        } else {
+            throw new IllegalArgumentException("Invalid Parameters");
+        }
+    }
+
+    public String loginUser(Map<String, String> loginUserParams) {
+        String userId = loginUserParams.get("userId");
+        String password = loginUserParams.get("password");
+
+        if (userId != null && password != null) {
+            User user = Database.findUserById(userId);
+            if (user != null && user.getPassword().equals(password)) {
+                // 세션 ID 생성
+                String sessionId = SessionUtil.createSessionId(SESSION_ID_LENGTH);
+                Database.updateUserSession(userId, sessionId);
+                return sessionId;
+            }
+            throw new IllegalArgumentException("Invalid userId and password");
         } else {
             throw new IllegalArgumentException("Invalid Parameters");
         }
