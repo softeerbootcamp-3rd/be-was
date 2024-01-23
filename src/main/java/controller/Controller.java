81c5e054d36f4b55d845938424267f35c185a169
ddrongy <ddrongy@gmail.com>
2024-01-23 13:50:32 +0900

    fix: controller 추상클래스로 변경

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,7 +4,7 @@ import http.request.HttpMethod;
 import http.request.Request;
 import http.response.Response;
 
-public class Controller {
+public abstract class Controller {
     public void service(Request request, Response response) {
         if (request.getMethod() == HttpMethod.GET) {
             doGet(request, response);
