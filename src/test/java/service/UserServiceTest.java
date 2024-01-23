fdfcaaa61785190a5d82d6bd173a917b9247ded4
win9 <krrr883@gmail.com>
2024-01-23 11:31:28 +0900

    [Test] BDD 테스트로 재정의
    기존 테스트를 BDD로 재정의 하여 행위에 대한 묘사를 하여 테스트 수행시 의도를 파악하기 쉽도록 함

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -13,9 +13,13 @@ public class UserServiceTest {
     @Test
     @DisplayName("사용자 생성 성공 테스트")
     public void createUserTest() {
+        // given
         QueryParams queryParams = QueryParams.from("userId=javajigi&password=password&name=jaesung&email=javajigi@slipp.net");
+
+        // when
         User user = userService.createUser(queryParams);
 
+        // then
         Assertions.assertEquals("jaesung", user.getName());
         Assertions.assertEquals("javajigi@slipp.net", user.getEmail());
     }
@@ -23,9 +27,13 @@ public class UserServiceTest {
     @Test
     @DisplayName("중복 사용자 저장 예외 테스트")
     public void duplicatedUserExceptionTest() {
+        // given
         QueryParams firstQueryParams = QueryParams.from("userId=win9&password=password&name=seunggu&email=win9@gmail.com");
+
+        // when
         userService.createUser(firstQueryParams);
 
+        // then
         Assertions.assertThrows(SourceException.class, () -> userService.createUser(firstQueryParams));
     }
 }
