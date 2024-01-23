b10d83a9eb2ba678bad2fb299c6f3512515ddf9d
Hymin2 <khm8246@gmail.com>
2024-01-23 14:24:08 +0900

    refactor: TestController의 응답을 Response로 수정
    TestController의 테스트를 위해 기존 String으로 응답하던 것을 Response로 수정하였습니다.

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,10 +1,12 @@
 package controller;
 
 import webserver.annotation.GetMapping;
+import webserver.response.Response;
+import webserver.type.ContentType;
 
 public class TestController {
     @GetMapping(path = "/test")
-    public String test(){
-        return "test";
+    public Response test(){
+        return Response.onSuccess(ContentType.HTML, "test".getBytes());
     }
 }
