5cf6beb5cbaea8871274883fd8443cb3a4514e7e
Yang-soeun <soeun8636@naver.com>
2024-01-23 11:12:38 +0900

    [DOCS]: 주석 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
             UserDto userDto = UserDto.from(httpRequest.getBody());
             UserService.signUp(userDto);
             return HttpResponse.redirect("/index.html");
-        }catch (IllegalArgumentException | NullPointerException e){
+        }catch (IllegalArgumentException | NullPointerException e){//중복 회원인 경우, 회원가입란에 빈란이 있는 경우 예외 발생
             return HttpResponse.errorResponse(BAD_REQUEST, e.getMessage());
         }
     }
