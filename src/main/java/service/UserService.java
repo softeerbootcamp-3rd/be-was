89af485534a83390730ee769d881056c28cfda03
LEE, TAEGON <taegon1998@gmail.com>
2024-01-23 15:46:35 +0900

    elimiate: HttpRequest -> User 객체로 파싱해주는 createUserEntity 메소드 삭제
    join 메소드의 매개변수로 User 객체를 받음으로써 사용하지 않게 된 createUserEntity 메소드 삭제

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -23,8 +23,7 @@ public class UserService {
     }
 
     // 회원가입 요청을 처리하는 메소드
-    public void join(String request) {
-        User user = createUserEntity(request); // 회원정보를 담고 있는 User 객체 생성
+    public void join(User user) {
 
         addUser(user); // 데이터베이스에 회원정보 저장
 
@@ -32,24 +31,5 @@ public class UserService {
         logger.debug("Total User Count : {}\n", getUserCount());
         logger.debug("UserId : {}, Password : {}, Name : {}, Email : {}\n",
                 user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
-    }
-
-    private User createUserEntity(String request) {
-
-        String[] userInfo = request.split(" ")[1].split("&");
-
-        if (userInfo.length != 4) {
-            throw new IllegalArgumentException("회원가입에 실패하였습니다.");
-        }
-        if (!request.startsWith("GET")) {
-            throw new IllegalArgumentException("회원가입에 실패하였습니다.");
-        }
-
-        String userId = userInfo[0].split("=")[1];
-        String password = userInfo[1].split("=")[1];
-        String name = userInfo[2].split("=")[1];
-        String email = userInfo[3].split("=")[1];
-
-        return new User(userId, password, name, email);
     }
 }
