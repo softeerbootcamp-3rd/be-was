a963f511f4c2062be2969472664da746f4478a37
admin <lejs1008@naver.com>
2024-01-23 22:40:22 +0900

    step-5 Login Session Manager 구현
    로그인 성공 시 세션 id 발급, 세션 아이디로 유저 조회, 로그아웃 용 세션 삭제 기능 추가

---
+++ src/main/java/session/SessionManager.java
@@ -0,0 +1,37 @@
+package session;
+
+import model.User;
+import request.HttpRequest;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+    private Map<String, User> sessionRepository = new ConcurrentHashMap<>();
+
+    // 로그인 성공한 유저에게 세션 id 발급
+    public String createSession(User user) {
+        String sessionId = UUID.randomUUID().toString();
+        sessionRepository.put(sessionId, user);
+        return sessionId;
+    }
+
+    // 세션 아이디로 유저 조회
+    public User getUserBySessionId(HttpRequest request) {
+        String cookie = request.getHeaders().get("Cookie");
+        if (cookie != null) {
+            String[] tokens = cookie.split("=");
+            return sessionRepository.get(tokens[1]);
+        }
+        return null;
+    }
+
+    // 로그아웃 처리 용도
+    public void deleteSession(HttpRequest request) {
+        String cookie = request.getHeaders().get("Cookie");
+        String[] tokens = cookie.split("=");
+        sessionRepository.remove(tokens[1]);
+    }
+}
