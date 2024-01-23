0db5017ec4894f98ca6eda261f2b5e549895f5b4
99winnmin <ojysep9987@gmail.com>
2024-01-23 20:29:42 +0900

    :sparkles: Feat : 로그인용 SessionStorage 변수 생성
    "구현 내용 : Database 내에 {key, value} 가 userId, sessionId 로 매핑되는 자료구조 추가
    구현 이유 : 로그인한 userId 당 하나의 sessionId 를 매핑시키기 위함"

--- src/main/java/common/db/Database.java
+++ src/main/java/common/db/Database.java
@@ -7,8 +7,13 @@ import java.util.Map;
 public class Database {
 
     private static final Map<String, User> users = Maps.newConcurrentMap();
+    private static final Map<String, String> sessionStorage = Maps.newConcurrentMap();
 
     public static Map<String, User> Users() {
         return users;
+    }
+
+    public static Map<String, String> SessionStorage() {
+        return sessionStorage;
     }
 }
