5122894c3d4c2d675bf23fc851dc6ca73444cdc4
99winnmin <ojysep9987@gmail.com>
2024-01-23 20:36:04 +0900

    :sparkles: Feat : sessionStorage 관련 조회, 생성, 삭제 메서드 정의 및 구현 / findUserById 리턴형 수정 / 누락된 Override 추가
    "구현 내용 :
    1. saveSession, getSessionIdByUserId, removeSession 메서드 정의 및 추가
    2. findUserById 리턴형을 Optional 로 wrapping
    3. addUser, findUserById, findAll 에 @Override 추가
    구현 이유 :
    1. 로그인 로직에서 session 저장 및 조회가 요구되기 때문, removeSession 은 로그아웃 로직에서 필요
    2. userId 로 User 를 찾을 시 NULL 반환을 대비하기 위함
    3. Repository 인터페이스의 메서드 정의를 구현해야하는 이를 안하고 있었음"

--- src/main/java/domain/user/infrastructure/UserRepositoryImpl.java
+++ src/main/java/domain/user/infrastructure/UserRepositoryImpl.java
@@ -1,21 +1,41 @@
 package domain.user.infrastructure;
 
+import static common.db.Database.SessionStorage;
 import static common.db.Database.Users;
 
 import domain.user.command.domain.User;
 import domain.user.command.domain.UserRepository;
 import java.util.Collection;
+import java.util.Optional;
 
 public class UserRepositoryImpl implements UserRepository {
+    @Override
     public void addUser(User user) {
         Users().put(user.getUserId(), user);
     }
 
-    public User findUserById(String userId) {
-        return Users().get(userId);
+    @Override
+    public Optional<User> findUserById(String userId) {
+        return Optional.ofNullable(Users().get(userId));
     }
 
+    @Override
     public Collection<User> findAll() {
         return Users().values();
+    }
+
+    @Override
+    public void saveSession(String userId, String sessionId) {
+        SessionStorage().put(userId, sessionId);
+    }
+
+    @Override
+    public Optional<String> getSessionIdByUserId(String userId) {
+        return Optional.ofNullable(SessionStorage().get(userId));
+    }
+
+    @Override
+    public void removeSession(String sessionId) {
+        SessionStorage().remove(sessionId);
     }
 }
