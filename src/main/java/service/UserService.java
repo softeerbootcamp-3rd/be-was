f2c02d391493dc7b6731c2523e4b46c12d9b4070
OSSU <o.ss.u2056@gmail.com>
2024-01-24 10:46:58 +0900

    step 5 : 로그인 서비스 로직 구현 해당 아이디로 회원가입 한 정보가 없거나 비밀번호가 올바르지 않을 경우 /user/login_failed.html로 리다이렉트 되도록 구현 로그인에 성공했을 경우 세션 아이디를 UUID 라이브러리를 통해 랜덤 문자열을 생성하여 반환해 줌

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -8,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import http.HttpStatus;
 import webserver.RequestHandler;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * 서비스 로직 구현
@@ -36,15 +38,35 @@ public class UserService {
         // 유저 아이디 중복 검사
         if (!validateDuplicated(userId)) {
             // 고민사항 : BAD_REQUEST를 반환해야 할지 회원가입 폼으로 REDIRECT 시켜야 할지
-            return HttpResponse.of(HttpStatus.REDIRECT, "/user/form.html");
+            Map<String, String> header = Map.of("Location", "/user/form.html");
+            return HttpResponse.of(HttpStatus.REDIRECT, header);
         }
 
         Database.addUser(user);
         logger.info(Database.findAll().toString());
-        return HttpResponse.of(HttpStatus.REDIRECT, "/index.html");
+        Map<String, String> header = Map.of("Location", "/index.html");
+        return HttpResponse.of(HttpStatus.REDIRECT, header);
     }
 
     private boolean validateDuplicated(String userId) {
         return Database.findUserById(userId) == null;
     }
+
+    // TODO: 로그인 기능 구현
+    public HttpResponse join(Map<String, String> params) {
+        String userId = params.get("userId"), password = params.get("password");
+        User user = Database.findUserById(userId);
+
+        // user 정보가 없거나 비밀번호 검증이 실패한 경우 로그인 실패
+        if (user == null || !password.equals(user.getPassword())) {
+            Map<String, String> header = Map.of("Location", "/user/login_failed.html");
+            return HttpResponse.of(HttpStatus.REDIRECT, header);
+        }
+
+        // TODO: 세션 아이디 임의로 설정
+        String sid = UUID.randomUUID().toString();
+        Map<String, String> header = Map.of("Location", "/index.html", "Set-Cookie", "sid=" + sid + "; Path=/");
+        return HttpResponse.of(HttpStatus.REDIRECT, header);
+    }
+
 }
