cb5720cb9fd149947fd0793bc29ce99d5d49f046
admin <admin@admin-MacBook-Pro-3.local>
2024-01-24 12:26:14 +0900

    Test: query 필드별 파싱 테스트
    “key1=value1&key2=value2&key3=value3”가 입력되면 “&”를 기준으로 키값쌍을 맵에 담는지 테스트

--- src/test/java/util/RequestParserUtilTest.java
+++ src/test/java/util/RequestParserUtilTest.java
@@ -1,8 +1,12 @@
 package util;
 
+import controller.HttpStatusCode;
 import data.RequestData;
+import data.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
@@ -10,7 +14,11 @@ import webserver.RequestHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -46,5 +54,21 @@ public class RequestParserUtilTest {
 
         // When, Then
         assertThrows(Exception.class, () -> RequestParserUtil.parseRequest(br)); // 예외타입을 IOException으로 하면 해당 오류에만 test를 검증하게 됨. 전체적으로 보기 위해 Exception으로 검증
+    }
+
+    @Test
+    @DisplayName("query 필드 별 파싱 테스트")
+    void testParseUserRegisterQuery() {
+        // Given
+        String url = "key1=value1&key2=value2&key3=value3";
+
+        // When
+        Map<String, String> actual = RequestParserUtil.parseUserRegisterQuery(url);
+
+        // Then
+        assertThat(actual)
+                .containsEntry("key1", "value1")
+                .containsEntry("key2", "value2")
+                .containsEntry("key3", "value3");
     }
 }
