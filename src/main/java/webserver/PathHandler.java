4375dd510ff9d23f7c8a28c4b125e1d1c2eb0bcb
win9 <krrr883@gmail.com>
2024-01-24 17:48:35 +0900

    [Feat] 회원가입 Post처리 구현
    Post요청에 대해서 데이터를 파싱 및 파싱한 Data를 이용하여 회원가입을 하는 로직 구현

--- src/main/java/webserver/PathHandler.java
+++ src/main/java/webserver/PathHandler.java
@@ -8,10 +8,14 @@ import java.util.function.Function;
 
 public class PathHandler {
 
-    public static ResourceDto responseResource(String method, String path, UserController controller) {
+    public static ResourceDto responseResource(String method, String path, String body, UserController controller) {
         if (controller != null) {
             Function<Object, ResourceDto> correctMethod = controller.getCorrectMethod(path);
-            if (method.equals("GET") && path.contains("?")) {
+            if (method.equals("POST")) {
+                QueryParams queryParam = QueryParams.from(body);
+                return correctMethod.apply(queryParam);
+            }
+            else if (method.equals("GET") && path.contains("?")) {
                 String[] splits = path.split("\\?");
                 QueryParams queryParams = QueryParams.from(splits[1]);
                 return correctMethod.apply(queryParams);
