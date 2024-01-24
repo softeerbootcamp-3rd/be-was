6db1386ffb32f9cc41e9f248b19eca277a23b4c6
kelly1422 <kelly1422@naver.com>
2024-01-24 12:27:24 +0900

    [step-5]: 로그인 실패시 에러코드 추가 dquote> USER_NOT_FOUND - 가입하지 않은 유저, USER_WRONG_PASSWORD - 틀린 비밀번호

--- src/main/java/constant/ErrorCode.java
+++ src/main/java/constant/ErrorCode.java
@@ -3,7 +3,10 @@ package constant;
 public enum ErrorCode {
     PAGE_NOT_FOUND (HttpStatus.NOT_FOUND, "요청한 페이지를 찾을 수 없습니다."),
     USER_ID_DUPLICATED (HttpStatus.BAD_REQUEST, "중복되는 유저 id 입니다."),
-    USER_EMAIL_DUPLICATED (HttpStatus.BAD_REQUEST, "중복되는 유저 이메일 입니다.");
+    USER_EMAIL_DUPLICATED (HttpStatus.BAD_REQUEST, "중복되는 유저 이메일 입니다."),
+    USER_NOT_FOUND (HttpStatus.BAD_REQUEST, "유저가 존재하지 않습니다."),
+    USER_WRONG_PASSWORD (HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
