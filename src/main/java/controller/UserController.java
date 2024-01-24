c5b8f4c4c0186aa59a2cca36be96e64dee24e2b7
OSSU <o.ss.u2056@gmail.com>
2024-01-24 23:42:16 +0900

    refactor: UserController에서 HttpResponse 만들도록 수정 기존 : 서비스에서 전달받은 HttpResponse 그대로 반환 변경 : 서비스 로직을 try catch 문 안에 넣어 중복 아이디 검증에 실패할 경우 CONFLICT를 반환하도록 수정, 성공할 경우 index.html로 리다이렉션 되도록 함

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,11 @@
 package controller;
 
+import http.HttpStatus;
 import http.response.HttpResponse;
 import http.request.HttpRequest;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import service.UserService;
 import utils.Parser;
 
@@ -12,10 +16,24 @@ import java.util.Map;
  * controller - service 연결
  */
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private static final UserService userService = new UserService();
 
-    public static HttpResponse signup(HttpRequest request) {
-        return userService.createUser(request.getBody());
+    public static HttpResponse signup(Map<String, String> body) {
+        // 유효성 검증 실패할 경우 BAD_REQUEST 반환하도록 로직 추가
+
+        User user = new User(body.get("userId"), body.get("password"), body.get("name"), body.get("email"));
+
+        try {
+            userService.createUser(user);
+            // 회원가입에 성공할 경우 index.html로 리다이렉션
+            Map<String, String> header = Map.of("Location", "/index.html");
+            return HttpResponse.of(HttpStatus.REDIRECT, header);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            // 회원가입에 실패할 경우 (중복 아이디) CONFLICT 반환
+            return HttpResponse.of(HttpStatus.BAD_REQUEST); // TODO : CONFLICT로 수정하기
+        }
     }
 
     public static HttpResponse login(HttpRequest request) {
