69937722b357d11e96d2ad23c5414a43e8f05d73
Hymin2 <khm8246@gmail.com>
2024-01-24 18:31:52 +0900

    test: UserService 테스트 추가
    - 이미 존재하는 아이디로 회원가입 할 때 예외가 발생하는지
    
    에 대한 테스트를 추가했습니다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package service;
 
+import dto.UserRequest;
 import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.exception.UserIdAlreadyExistsException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +22,15 @@ class UserServiceTest {
         assertEquals("password", user.getPassword());
         assertEquals("name", user.getName());
         assertEquals("email", user.getEmail());
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 아이디로 회원가입 할 때 예외 발생 테스트")
+    void addUserByExistsUserIdTest(){
+        userService.createUser("userId", "password", "name", "email");
+
+        assertThrows(UserIdAlreadyExistsException.class, () -> {
+            userService.createUser("userId", "password", "name", "email");
+        });
     }
 }
