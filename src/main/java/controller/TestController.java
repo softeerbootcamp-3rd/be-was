72ff6a01552471957ede4223a2b4d3630ce439b3
Hymin2 <khm8246@gmail.com>
2024-01-24 18:25:43 +0900

    feat: TestController에서 POST 테스트 함수 추가
    POST 테스트를 위한 postTest 함수를 추가했습니다.

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import webserver.annotation.GetMapping;
+import webserver.annotation.PostMapping;
 import webserver.response.Response;
 import webserver.type.ContentType;
 
@@ -8,5 +9,10 @@ public class TestController {
     @GetMapping(path = "/test")
     public Response test(){
         return Response.onSuccess("test".getBytes());
+    }
+
+    @PostMapping(path = "/test")
+    public Response postTest(){
+        return Response.onSuccess("post test".getBytes());
     }
 }
