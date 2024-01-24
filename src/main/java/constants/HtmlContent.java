c516f331a1203e293a69440ec0a20fda3a47a782
seonghwan <aiya0422@gmail.com>
2024-01-24 17:57:41 +0900

    HTML 요소 추가
    로그인 상황에 따라 버튼을 다르게 보여주기 위해 해당 상황에 맞는 버튼끼리 묶었습니다.

--- src/main/java/constants/HtmlContent.java
+++ src/main/java/constants/HtmlContent.java
@@ -2,6 +2,10 @@ package constants;
 
 public enum HtmlContent {
 
+    NAV_LOGOUT("<li><a href=\"{{location-login}}\" role=\"button\">로그인</a></li>"
+            + "<li><a href=\"{{location-join}}\" role=\"button\">회원가입</a></li>\n"),
+    NAV_LOGIN("<li><a href=\"#\" role=\"button\">로그아웃</a></li>\n"
+            + "<li><a href=\"#\" role=\"button\">개인정보수정</a></li>"),
     USER_CONTENT("<tr>\n"
             + "<th scope=\"row\">{{count}}</th> "
             + "<td>{{userId}}</td> "
