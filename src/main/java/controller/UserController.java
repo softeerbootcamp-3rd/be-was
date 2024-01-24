39ec648ac923104b39a0fdfa9544115e47520f1f
kelly1422 <kelly1422@naver.com>
2024-01-24 13:24:48 +0900

    [step-5]: POST로 유저 로그인 구현 재로그인 고려, 로그인한 사용자헤게 세션id 생성후 반환

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -4,6 +4,7 @@ import annotation.PostMapping;
 import dto.RequestDto;
 import dto.ResponseDto;
 import service.UserService;
+import session.Session;
 
 import static constant.FilePath.MAIN_PAGE;
 
@@ -14,5 +15,26 @@ public class UserController {
         UserService.signUp(requestDto.getBody());
 
         return new ResponseDto().makeRedirect(MAIN_PAGE.getPath());
+    }
+
+    @PostMapping(path = "/user/login")
+    public static ResponseDto login(RequestDto requestDto) {
+        ResponseDto response = new ResponseDto();
+        response.makeRedirect(MAIN_PAGE.getPath());
+
+        // 이전에 로그인한 유저 (요청 헤더 쿠키에 sessionId 가 있음) && 세션이 만료되지 않은 경우
+        // MAIN_PAGE 로 리다이렉트
+        String sessionId = requestDto.getCookies().get("sessionId");
+        if (sessionId != null && Session.getAttribute(sessionId) != null) {
+            return response;
+        }
+
+        // 처음 로그인한 유저 (오청 헤더 쿠키에 sessionId 없음) OR 세션이 만료된 경우
+        String userId = requestDto.getBody().get("userId");
+        String password = requestDto.getBody().get("password");
+        // UserService.login 하고 생성한 유저의 sessionId 값을 응답 헤더에 추가해 응답 리턴
+        response.addHeader("Set-Cookie", ("sessionId=" + UserService.login(userId, password) + "; Path=/"));
+
+        return response;
     }
 }
