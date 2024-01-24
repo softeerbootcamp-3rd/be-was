95ba5145883dc4b2de9d50788902a5a5e9fcacaf
Hymin2 <khm8246@gmail.com>
2024-01-24 18:01:37 +0900

    feat: Response 객체 수정
    기존 Responsea 필드인 ContentType을 제거하고 모든 헤더가 담겨져 있는 해시 맵 headers를 추가하였습니다. Response 객체를 onSuccess 정적 메소드를 통해 생성할 때 body나 ContentType을 객체 자체에서 넣도록 하여 따로 설정을 하지 않아도 되도록 하였습니다. 또한, redirect 메소드를 생성하여 응답의 결과가 리다이렉트인 경우 해당 메소드를 통해 응답을 만들 수 있도록 했습니다. Response Header 형식에 따라 헤더를 반환해주는 함수도 추가했습니다.

--- src/main/java/webserver/response/Response.java
+++ src/main/java/webserver/response/Response.java
@@ -3,35 +3,65 @@ package webserver.response;
 import webserver.status.HttpStatus;
 import webserver.type.ContentType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Response {
     private final HttpStatus httpStatus;
-    private final ContentType contentType;
+    private final Map<String, String> headers;
     private final byte[] body;
 
-    private Response(HttpStatus httpStatus, ContentType contentType, byte[] body){
+    private Response(HttpStatus httpStatus, byte[] body){
         this.httpStatus = httpStatus;
-        this.contentType = contentType;
+        this.headers = new HashMap<>();
         this.body = body;
     }
 
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
+    public String getHeaders(){
+        StringBuilder sb = new StringBuilder();
 
-    public ContentType getContentType() {
-        return contentType;
+        sb.append("HTTP/1.1 " + httpStatus.getCode() + " " + httpStatus.getName() + "\r\n");
+        headers.forEach((key, value) -> sb.append(key).append(": ").append(value).append("\r\n"));
+        sb.append("\r\n");
+
+        return sb.toString();
     }
 
     public byte[] getBody() {
         return body;
     }
 
-    public static Response onSuccess(ContentType contentType, byte[] body){
-        return new Response(HttpStatus.OK, contentType, body);
+    public boolean existsBody(){
+        return body != null;
     }
 
-    public static Response onFailure(HttpStatus httpStatus, ContentType contentType, byte[] body){
-        return new Response(httpStatus, contentType, body);
+    public static Response redirect(String location){
+        return new Response(HttpStatus.FOUND, null)
+                .setHeader("Location", location);
+    }
+
+    public static Response onSuccess(byte[] body){
+        return new Response(HttpStatus.OK, body)
+                .setHeader("Content-Length", String.valueOf(body.length))
+                .setHeader("Content-Type", ContentType.HTML.getValue());
+    }
+
+    public static Response onSuccess(byte[] body, ContentType contentType){
+        return new Response(HttpStatus.OK, body)
+                .setHeader("Content-Length", String.valueOf(body.length))
+                .setHeader("Content-Type", contentType.getValue());
+    }
+
+    public static Response onFailure(HttpStatus httpStatus, byte[] body){
+        return new Response(httpStatus, body)
+                .setHeader("Content-Length", String.valueOf(body.length))
+                .setHeader("Content-Type", ContentType.HTML.getValue());
+    }
+
+    private Response setHeader(String key, String value){
+        headers.put(key, value);
+
+        return this;
     }
 }
