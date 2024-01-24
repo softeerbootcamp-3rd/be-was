6f41616845457874c9abec0f7a45a44c142e520e
admin <admin@admin-MacBook-Pro-3.local>
2024-01-24 12:01:10 +0900

    Test: Controller, ResourceMapping의 변경으로 인한 테스트 코드 수정
    controller 반환 방식을 변경하고 상태코드를 ENUM으로 관리하게 되어 테스트 코드를 수정함

--- src/test/java/controller/RequestDataControllerTest.java
+++ src/test/java/controller/RequestDataControllerTest.java
@@ -1,6 +1,7 @@
 package controller;
 
 import data.RequestData;
+import data.Response;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,93 +18,86 @@ public class RequestDataControllerTest {
 
     static Stream<Object[]> provideResourcePaths() {
         return Stream.of(
-                new Object[]{"/index.html", "200 /index.html"},
-                new Object[]{"/user/form.html", "200 /user/form.html"},
-                new Object[]{"/user/list.html", "200 /user/list.html"},
-                new Object[]{"/user/login.html", "200 /user/login.html"},
-                new Object[]{"/user/profile.html", "200 /user/profile.html"}
+                new Object[]{new RequestData("GET", "/index.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.OK, "/index.html")},
+                new Object[]{new RequestData("GET", "/user/form.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.OK, "/user/form.html")},
+                new Object[]{new RequestData("GET", "/user/list.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.OK, "/user/list.html")},
+                new Object[]{new RequestData("GET", "/user/login.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.OK, "/user/login.html")},
+                new Object[]{new RequestData("GET", "/user/profile.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.OK, "/user/profile.html")}
         );
     }
 
     static Stream<Object[]> provideInvalidResourcePaths() {
         return Stream.of(
-                new Object[]{"/inddex.html", "404 /error/notfound.html"},
-                new Object[]{"/user/foorm.html", "404 /error/notfound.html"},
-                new Object[]{"/user/lisst.html", "404 /error/notfound.html"},
-                new Object[]{"/user/logiin.html", "404 /error/notfound.html"},
-                new Object[]{"/user/profiile.html", "404 /error/notfound.html"}
+                new Object[]{new RequestData("GET", "/inddex.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html")},
+                new Object[]{new RequestData("GET", "/foorm.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html")},
+                new Object[]{new RequestData("GET", "/lisst.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html")},
+                new Object[]{new RequestData("GET", "/logiin.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html")},
+                new Object[]{new RequestData("GET", "/profiile.html", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html")}
         );
     }
 
     static Stream<Object[]> provideApiPaths() {
         return Stream.of(
-                new Object[]{"/", "302 /index.html"},
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=asdf", "302 /index.html"}
+                new Object[]{new RequestData("GET", "/", "HTTP/1.1", new HashMap<>()), new Response(HttpStatusCode.FOUND, "/index.html")},
+                new Object[]{new RequestData("POST", "/user/create", "HTTP/1.1", new HashMap<>(), "userId=asdf&password=asdf&name=asdf&email=asdf"), new Response(HttpStatusCode.FOUND, "/index.html")}
         );
     }
 
     static Stream<Object[]> provideInvalidSignup() {
         return Stream.of(
-                new Object[]{"/user/create?userId=&password=asdf&name=asdf&email=asdf", "400 /error/badrequest.html"},
-                new Object[]{"/user/create?userId=asdf&password=&name=asdf&email=asdf", "400 /error/badrequest.html"},
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=&email=asdf", "400 /error/badrequest.html"},
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=", "400 /error/badrequest.html"}
+                new Object[]{new RequestData("POST", "/user/create", "HTTP/1.1", new HashMap<>(), "userId=&password=asdf&name=asdf&email=asdf"), new Response(HttpStatusCode.BAD_REQUEST, "/error/badrequest.html")},
+                new Object[]{new RequestData("POST", "/user/create", "HTTP/1.1", new HashMap<>(), "userId=asdf&password=&name=asdf&email=asdf"), new Response(HttpStatusCode.BAD_REQUEST, "/error/badrequest.html")},
+                new Object[]{new RequestData("POST", "/user/create", "HTTP/1.1", new HashMap<>(), "userId=asdf&password=asdf&name=&email=asdf"), new Response(HttpStatusCode.BAD_REQUEST, "/error/badrequest.html")},
+                new Object[]{new RequestData("POST", "/user/create", "HTTP/1.1", new HashMap<>(), "userId=asdf&password=asdf&name=asdf&email="), new Response(HttpStatusCode.BAD_REQUEST, "/error/badrequest.html")}
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideResourcePaths")
     @DisplayName("리소스 라우팅 테스트")
-    public void routeRequest_Resource(String path, String expected) throws IOException {
-        // Given
-        Map<String, String> map = new HashMap<>();
-
+    public void routeRequest_Resource(RequestData requestData, Response expected) throws IOException {
         // When
-        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+        Response actual = RequestDataController.routeRequest(requestData);
 
         // Then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getStatus()).isEqualTo(expected.getStatus());
+        assertThat(actual.getPath()).isEqualTo(expected.getPath());
     }
 
     @ParameterizedTest
     @MethodSource("provideInvalidResourcePaths")
     @DisplayName("유효하지않은 리소스 라우팅 테스트")
-    public void routeRequest_InvalidResource(String path, String expected) throws IOException {
-        // Given
-        Map<String, String> map = new HashMap<>();
-
+    public void routeRequest_InvalidResource(RequestData requestData, Response expected) throws IOException {
         // When
-        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+        Response actual = RequestDataController.routeRequest(requestData);
 
         // Then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getStatus()).isEqualTo(expected.getStatus());
+        assertThat(actual.getPath()).isEqualTo(expected.getPath());
     }
 
     @ParameterizedTest
     @MethodSource("provideApiPaths")
     @DisplayName("API 라우팅 테스트")
-    public void routeRequest_Api(String path, String expected) throws IOException {
-        // Given
-        Map<String, String> map = new HashMap<>();
-
+    public void routeRequest_Api(RequestData requestData, Response expected) throws IOException {
         // When
-        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+        Response actual = RequestDataController.routeRequest(requestData);
 
         // Then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getStatus()).isEqualTo(expected.getStatus());
+        assertThat(actual.getPath()).isEqualTo(expected.getPath());
+        assertThat(actual.getCookie()).isEqualTo(expected.getCookie());
     }
 
     @ParameterizedTest
     @MethodSource("provideInvalidSignup")
     @DisplayName("회원가입 부족한 파라미터 테스트")
-    public void routeRequest_InvalidSignup(String path, String expected) throws IOException {
-        // Given
-        Map<String, String> map = new HashMap<>();
-
+    public void routeRequest_InvalidSignup(RequestData requestData, Response expected) throws IOException {
         // When
-        String actual = RequestDataController.routeRequest(new RequestData("GET", path, "HTTP/1.1", map));
+        Response actual = RequestDataController.routeRequest(requestData);
 
         // Then
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual.getStatus()).isEqualTo(expected.getStatus());
+        assertThat(actual.getPath()).isEqualTo(expected.getPath());
     }
 }
