7fbb13cf8abec65834804dc0268913d03eda63da
h-sooah <s2sooas2@ewhain.net>
2024-01-25 01:52:26 +0900

    remove : Response Builder 테스트 코드 삭제
    ResponseEntity로 인해 Response 클래스가 필요 없어졌다. Response 클래스가 이제 삭제되었기 때문에 테스트 코드도 필요 없어졌으므로 삭제.

--- src/test/java/UserControllerTest.java
+++ src/test/java/UserControllerTest.java
@@ -4,6 +4,7 @@ import model.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import webserver.http.HttpStatus;
+import webserver.http.ResponseEntity;
 
 public class UserControllerTest {
 
@@ -23,32 +24,6 @@ public class UserControllerTest {
         // [ 아래 조건을 만족 ] 해야 한다.
         Assertions.assertThat(user1).isEqualTo(findUser);
 
-    }
-
-    @Test
-    public void responseBuilderTest() {
-
-        HttpStatus httpStatus = HttpStatus.OK;
-        String contentType = "text/html";
-        byte[] body = "Hello World".getBytes();
-
-        // given : 테스트에서 구체화하고자 하는 행동을 하기 전 테스트의 상태를 설명하는 부분
-        // [ 생성자를 통해 ResponseBuilder를 생성 ] 했을 때
-        Response response = new Response(httpStatus, contentType, body);
-
-        // when : 구체화하고자 하는 행동
-        // [ Builder 패턴을 이용해 ResponseBuilder를 생성 ] 했을 때
-        Response builder = new Response.Builder()
-                                        .httpStatus(httpStatus)
-                                        .contentType(contentType)
-                                        .body(body)
-                                        .build();
-
-        // then : 예상되는 변화를 확인, 검증
-        // [ 두 객체가 같은 내용을 갖고 있어야 ] 한다.
-        Assertions.assertThat(builder.getHttpStatus()).isEqualTo(response.getHttpStatus());
-        Assertions.assertThat(builder.getContentType()).isEqualTo(response.getContentType());
-        Assertions.assertThat(builder.getBody()).isEqualTo(response.getBody());
     }
 
 }
