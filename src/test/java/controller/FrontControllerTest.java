b4c292bda4337e118d484f344dde488fc2b25b89
win9 <krrr883@gmail.com>
2024-01-24 19:02:10 +0900

    [Test] 파라미터 변경으로 HttpRequest를 만들어 전달
    frontContorller의 파라미터가 HttpRequest를 받도록 전달하여 기존 header만 받던 테스트를 수정

--- src/test/java/controller/FrontControllerTest.java
+++ src/test/java/controller/FrontControllerTest.java
@@ -4,6 +4,7 @@ import model.CommonResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import request.HttpRequest;
 import util.HttpStatus;
 import webserver.RequestHeader;
 
@@ -16,10 +17,10 @@ class FrontControllerTest {
     @DisplayName("정상 url 응답 테스트")
     public void validUrlServiceTest() throws IOException {
         // given
-        RequestHeader header = createHeader("GET", "/index.html", "HTTP/1.1");
+        HttpRequest request = HttpRequest.of(createHeader("GET", "/index.html", "HTTP/1.1"), "dummyData");
 
         // when
-        CommonResponse response = FrontController.service(header);
+        CommonResponse response = FrontController.service(request);
 
         // then
         Assertions.assertEquals("html", response.getExtension());
@@ -30,10 +31,10 @@ class FrontControllerTest {
     @DisplayName("잘못된 url 요청 응답 테스트")
     public void notValidUrlServiceTest() throws IOException {
         // given
-        RequestHeader header = createHeader("GET", "/bad.html", "HTTP/1.1");
+        HttpRequest request = HttpRequest.of(createHeader("GET", "/index/bad", "HTTP/1.1"), "dummyData");
 
         // when
-        CommonResponse response = FrontController.service(header);
+        CommonResponse response = FrontController.service(request);
 
         // then
         Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getHttpStatus());
@@ -43,10 +44,11 @@ class FrontControllerTest {
     @DisplayName("잘못된 static 자원 요청 응답 테스트")
     public void notValidResourceServiceTest() throws IOException {
         // given
-        RequestHeader header = createHeader("GET", "/css/badcss.css", "HTTP/1.1");
+        HttpRequest request = HttpRequest.of(createHeader("GET", "/css/badcss.css", "HTTP/1.1"), "dummyData");
+
 
         // when
-        CommonResponse response = FrontController.service(header);
+        CommonResponse response = FrontController.service(request);
 
         // then
         Assertions.assertEquals(HttpStatus.NOT_FOUND, response.getHttpStatus());
