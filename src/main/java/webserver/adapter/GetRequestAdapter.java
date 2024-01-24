0dd69f8f36a99ea442610cc8cd7c8b267fb4303b
Hymin2 <khm8246@gmail.com>
2024-01-24 10:29:29 +0900

    refactor: GetRequestAdapter에 기존 구현된 함수를 제거 및 MethodRequestAdapter 상속
    기존 구현되어 있던 메소드 찾기, 파라미터 생성, 메소드 실행에 대한 함수를 제거하고, MethodRequestAdapter를 상속받아 따로 구현하지 않도록 수정했습니다.

--- src/main/java/webserver/adapter/GetRequestAdapter.java
+++ src/main/java/webserver/adapter/GetRequestAdapter.java
@@ -7,6 +7,7 @@ import controller.UserController;
 import webserver.exception.GeneralException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.registry.ControllerRegistry;
 import webserver.request.Request;
 import webserver.response.Response;
 import webserver.status.ErrorCode;
@@ -18,7 +19,7 @@ import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GetRequestAdapter implements Adapter{
+public class GetRequestAdapter extends MethodRequestAdapter{
     private static final Logger logger = LoggerFactory.getLogger(GetRequestAdapter.class);
     private static final GetRequestAdapter getRequestAdapter = new GetRequestAdapter();
 
@@ -30,8 +31,7 @@ public class GetRequestAdapter implements Adapter{
 
     @Override
     public Response run(Request request) throws Throwable {
-        List<Class<?>> classes = getControllerClasses();
-        Method method = findMethod(classes, request.getPath());
+        Method method = findMethod(GetMapping.class, request.getPath());
 
         if(method == null){
             throw new GeneralException(ErrorCode.RESOURCE_NOT_FOUND_ERROR);
@@ -55,63 +55,5 @@ public class GetRequestAdapter implements Adapter{
     @Override
     public boolean canRun(Request request) {
         return request.getMethod().equals("GET");
-    }
-
-    private static Object executeMethod(Method method, Object[] params) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Class<?> c = method.getDeclaringClass();
-        Object instance = c.getDeclaredConstructor().newInstance();
-
-        return method.invoke(instance, params);
-    }
-
-    private static Object[] createParams(Method method, Request request){
-        Class<? extends Annotation> requestParam = RequestParam.class;
-        Parameter[] parameters = method.getParameters();
-
-        Object[] params = new Object[parameters.length];
-        int index = 0;
-
-        for(Parameter parameter: parameters){
-            if(parameter.isAnnotationPresent(requestParam)){
-                RequestParam annotation = (RequestParam) parameter.getAnnotation(requestParam);
-                params[index++]= request.getParam(annotation.name());
-            }
-
-            if(params[index - 1] == null){
-                logger.error(parameter.getName());
-                throw new GeneralException(ErrorCode.ILLEGAL_ARGUMENT_ERROR);
-            }
-        }
-
-        return params;
-    }
-
-    private static Method findMethod(List<Class<?>> classes, String path){
-        Class<? extends Annotation> getMapping = GetMapping.class;
-
-        for(Class<?> c: classes){
-            Method[] methods = c.getDeclaredMethods();
-
-            for(Method method: methods){
-                if(method.isAnnotationPresent(getMapping)){
-                    GetMapping annotation = (GetMapping) method.getAnnotation(getMapping);
-
-                    if(path.equals(annotation.path())){
-                        return method;
-                    }
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private static List<Class<?>> getControllerClasses(){
-        List<Class<?>> classes = new ArrayList<>();
-
-        classes.add(UserController.class);
-        classes.add(TestController.class);
-
-        return classes;
     }
 }
