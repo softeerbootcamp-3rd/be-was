0431efe40193fc6f04f7a08c7e5079e2afb4d6e4
Hymin2 <khm8246@gmail.com>
2024-01-24 10:33:41 +0900

    feat: POST 회원가입 API 추가
    POST 요청으로 회원가입을 요청하는 API를 추가했습니다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import dto.UserRequest;
 import webserver.annotation.GetMapping;
+import webserver.annotation.PostMapping;
+import webserver.annotation.RequestBody;
 import webserver.annotation.RequestParam;
 import webserver.exception.GeneralException;
 import org.slf4j.Logger;
@@ -21,6 +24,13 @@ public class UserController {
         logger.debug("createUser() 실행");
 
         userService.createUser(userId, password, name, email);
+
+        return Response.onSuccess(ContentType.HTML, "회원가입 완료".getBytes());
+    }
+
+    @PostMapping(path = "/user/create")
+    public Response createUserByPost(@RequestBody UserRequest.Register register){
+        userService.createUser(register);
 
         return Response.onSuccess(ContentType.HTML, "회원가입 완료".getBytes());
     }
