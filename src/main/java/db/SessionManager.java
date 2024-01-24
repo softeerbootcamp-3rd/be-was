ed32fc8e8e8f47d9a3f6f5bfe6f2203b922e950f
admin <admin@admins-MacBook-Pro-6.local>
2024-01-24 17:55:23 +0900

    feat : login testcode 추가 및 sessionid 미적용 버그 발견 및 수정
    BDD학습후 displayname을 변경하라는 피드백 적용
    생성한 sessionid가 아닌 userid를 넣고 있어서 버그 수정

--- src/main/java/db/SessionManager.java
+++ src/main/java/db/SessionManager.java
@@ -1,11 +1,11 @@
 package db;
 
-import com.google.common.collect.Maps;
 import model.User;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.HashMap;
+import java.util.Map;
 
 public class SessionManager {
     SessionManager(){
@@ -20,7 +20,19 @@ public class SessionManager {
 
     public static String addSession(User user) {
         String session = generateSessionId();
-        sessions.put(user.getUserId(), user);
+        sessions.put(session, user);
         return session;
+    }
+
+    public static User findUserById(String sessionId) {
+        return sessions.get(sessionId);
+    }
+
+    public static void printAllUsers() {
+        for (Map.Entry<String, User> entry : sessions.entrySet()) {
+            String key = entry.getKey();
+            User user = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + user);
+        }
     }
 }
--- src/test/java/webserver/http/GetRequestTest.java
+++ src/test/java/webserver/http/GetRequestTest.java
@@ -9,8 +9,8 @@ import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RequestTest {
-    String httpRequest = "GET /user/create?userId=123&password=123&name=123&email=1%403 HTTP/1.1\n" +
+class GetRequestTest {
+    private String httpRequest = "GET /user/create?userId=123&password=123&name=123&email=1%403 HTTP/1.1\n" +
             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\n" +
             "Accept-Encoding: gzip, deflate, br\n" +
             "Accept-Language: ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7,ja;q=0.6\n" +
@@ -26,6 +26,7 @@ class RequestTest {
             "sec-ch-ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"\n" +
             "sec-ch-ua-mobile: ?0\n" +
             "sec-ch-ua-platform: \"macOS\"";
+    private Request request;
 
 
     @BeforeEach
@@ -33,21 +34,28 @@ class RequestTest {
         BufferedReader bufferedReader = new BufferedReader(new StringReader(httpRequest));
         request = new Request(bufferedReader);
     }
-    Request request;
 
     @Test
-    @DisplayName("User Create 파싱 테스트")
+    @DisplayName("사용자가 User를 생성했을때 데이터가 정상적으로 파싱이 된다.")
     void getRequestTarget() {
+        //GIVEN
+
+        //WHEN
         String expected = "/user/create?userId=123&password=123&name=123&email=1%403";
         String actual = request.getRequestTarget();
+        //THEN
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("HTTP Request MIME 파싱 결과 테스트")
+    @DisplayName("사용자가 유저를 생성했을때 요청되는 MIME TYPE이 NONE값이다.")
     void getResponseMimeType() {
+        //GIVEN
+
+        //WHEN
         Mime expected = Mime.NONE;
         Mime actual = request.getResponseMimeType();
+        //THEN
         assertEquals(expected, actual);
     }
 }
