d65dc300a765fb4030d2f007705c4938bf271fbe
jbonnie <bonnie8623@gmail.com>
2024-01-24 19:27:35 +0900

    Remove: 세션 만료 판단 코드 삭제
    세션이 만료 기간을 넘어도 http reqeust에서 쿠키가 사라지지 않는 줄 알고 서버 측에서 만료인지 아닌지 판단하는 메소드를 구현했으나, 만료시 요청에서 정상적으로 쿠키가 사라지는 것을 확인하여 해당 메소드들을 삭제함

--- src/main/java/dto/session/Session.java
+++ src/main/java/dto/session/Session.java
@@ -13,7 +13,6 @@ public class Session {
     private LocalDateTime createTime;   // 세션 생성 시간
     private LocalDateTime lastAccessTime;     // 마지막 접속 시간 (로그인 했을 떄 갱신)
     private LocalDateTime expires;      // 세션 만료 시간 : 1시간
-    private static long maxAccessTime = 3600;          // 최대 접속 가능 시간 : 1시간 (3600초)
 
     public Session(String userId) {
         this.id = createSessionID();
@@ -38,29 +37,6 @@ public class Session {
     // 로그인 시 마지막 접속 시간 업데이트
     public void setLastAccessTime(LocalDateTime time) {
         this.lastAccessTime = time;
-    }
-
-    // 해당 세션이 expires와 비교했을 때 유효한지 검증
-    public boolean isValidWithExpires() {
-        LocalDateTime currentTime = LocalDateTime.now();
-        if(currentTime.isBefore(expires))       // 만료 시간보다 이전일 때
-            return true;
-        else if(currentTime.isAfter(expires))       // 만료 시간이 지났을 때
-            return false;
-        else        // 두 시간이 동일할 때
-            return false;
-    }
-
-    // 해당 세션이 maxAccessTime과 비교했을 때 유효한지 검증
-    public boolean isValidWithMaxAccessTime() {
-        LocalDateTime currentTime = LocalDateTime.now();
-
-        Duration duration = Duration.between(lastAccessTime, currentTime);
-        long AccessTimeSeconds = duration.toSeconds();
-
-        if(AccessTimeSeconds < maxAccessTime)       // 유저의 접속 시간이 1시간보다 적을 경우
-            return true;
-        return false;                               // 유저의 접속 시간이 1시간 이상이 되었을 경우
     }
 
     // 세션 ID 생성
