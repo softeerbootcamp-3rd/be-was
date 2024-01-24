81dbc8e348ac1f4becc9afe7e981660d8286e665
Hymin2 <khm8246@gmail.com>
2024-01-24 10:38:13 +0900

    feat: POST 요청 시 회원가입 기능 구현
    POST 요청 시 회원가입을 처리해주는 기능을 UserService에서 구현했습니다.

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,6 +1,7 @@
 package service;
 
 import db.Database;
+import dto.UserRequest;
 import webserver.exception.GeneralException;
 import model.User;
 import org.slf4j.Logger;
@@ -21,6 +22,24 @@ public class UserService {
                 .password(password)
                 .name(name)
                 .email(email)
+                .build();
+
+        Database.addUser(user);
+        logger.debug("User 생성 완료");
+        logger.debug(user.toString());
+    }
+
+    public void createUser(UserRequest.Register register){
+        if(existsUserId(register.getUserId())){
+            logger.error("userId가 이미 존재하는 예외 발생");
+            throw new GeneralException(ErrorCode.USER_ID_ALREADY_EXISTS_ERROR);
+        }
+
+        User user = User.builder()
+                .userId(register.getUserId())
+                .password(register.getPassword())
+                .name(register.getName())
+                .email(register.getEmail())
                 .build();
 
         Database.addUser(user);
