a2607fe7acfba60620507a03b88f279560fe5091
99winnmin <ojysep9987@gmail.com>
2024-01-24 23:31:03 +0900

    :ambulance: Feat : response.Header 객체 getter 구현

--- src/main/java/common/http/response/Header.java
+++ src/main/java/common/http/response/Header.java
@@ -10,6 +10,10 @@ public class Header {
         this.headers = headers;
     }
 
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
     public void setHeaders(Map<String, String> headers) {
         this.headers = headers;
     }
