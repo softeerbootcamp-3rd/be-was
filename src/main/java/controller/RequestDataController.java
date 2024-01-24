b42d0df82fd2412f137327d53d2069f61522cdda
admin <admin@admin-MacBook-Pro-3.local>
2024-01-24 23:21:58 +0900

    Refactor: 컨트롤러 메서드 세분화
    기존에는 컨트롤러 메서드를 `하나` 정의함
    
    가독성에 좋지않고 유지보수에 장애가 있다고 판단함
    
    컨트롤러 메서드의 내부 동작을 하위 메서드로 분리하여 정의하고 각 부분에서 호출하도록 변경함

--- src/main/java/controller/RequestDataController.java
+++ src/main/java/controller/RequestDataController.java
@@ -2,16 +2,13 @@ package controller;
 
 import data.CookieData;
 import data.RequestData;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
 import util.ResourceLoader;
-import webserver.RequestHandler;
 import data.Response;
 
 import java.io.File;
-import java.util.Locale;
 
 import static util.RequestParserUtil.getFileExtension;
 import static util.ResourceLoader.getResourceType;
@@ -29,42 +26,9 @@ public class RequestDataController {
             String fileOrApi = getResourceType(url); // URL이 FILE을 나타내는지 API를 나타내는지 문자열로 반환
 
             if (fileOrApi.equals("FILE")) {
-                String directory = ResourceMapping.valueOf(extension.toUpperCase()).getDirectory();
-                File file = new File(ResourceLoader.url + directory + url);
-                if (file.exists() && !file.isDirectory()) {
-                    if (url.equals("/user/login.html")) {
-                        if (requestData.getHeaderValue("Cookie") != null) {
-                            if (UserService.isLoggedIn(requestData)) {
-                                return new Response(HttpStatusCode.FOUND, "/index.html");
-                            }
-                        }
-                    }
-                    return new Response(HttpStatusCode.OK, url);
-                } else {
-                    logger.debug("유효하지 않은 파일 경로입니다.");
-                    return new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html");
-                }
+                return handleFileRequest(url, extension, requestData);
             } else if (fileOrApi.equals("API")) {
-                if (url.equals("/")) {
-                    return new Response(HttpStatusCode.FOUND, "/index.html");
-                } else if (url.startsWith("/user/create")) {
-                    UserService.registerUser(requestData);
-                    return new Response(HttpStatusCode.FOUND, "/index.html");
-                } else if (url.equals("/user/login")) {
-                    logger.debug("[API] /user/login");
-                    CookieData cookieData = UserService.login(requestData);
-                    if (cookieData != null) {
-                        return new Response(HttpStatusCode.FOUND, "/index.html", cookieData);
-                    }
-                    return new Response(HttpStatusCode.FOUND, "/index.html");
-                } else if (url.equals("/user/logout")) {
-                    logger.debug("[API] /user/logout");
-                    CookieData cookieData = UserService.logout(requestData);
-                    return new Response(HttpStatusCode.FOUND, "/index.html", cookieData);
-                } else {
-                    logger.debug("유효하지 않은 API입니다.");
-                    return new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html");
-                }
+                return handleApiRequest(url, requestData);
             } else {
                 return new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html");
             }
@@ -72,5 +36,54 @@ public class RequestDataController {
             logger.debug("IllegalArgumentException caught: {}", e.getMessage());
             return new Response(HttpStatusCode.BAD_REQUEST, "/error/badrequest.html");
         }
+    }
+
+    private static Response handleFileRequest(String url, String extension, RequestData requestData) {
+        String directory = ResourceMapping.valueOf(extension.toUpperCase()).getDirectory();
+        File file = new File(ResourceLoader.url + directory + url);
+        if (file.exists() && !file.isDirectory()) {
+            if (url.equals("/user/login.html")) {
+                if (requestData.getHeaderValue("Cookie") != null) {
+                    if (UserService.isLoggedIn(requestData)) {
+                        return new Response(HttpStatusCode.FOUND, "/index.html");
+                    }
+                }
+            }
+            return new Response(HttpStatusCode.OK, url);
+        } else {
+            logger.debug("유효하지 않은 파일 경로입니다.");
+            return new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html");
+        }
+    }
+
+    private static Response handleApiRequest(String url, RequestData requestData) {
+        if (url.equals("/")) {
+            return new Response(HttpStatusCode.FOUND, "/index.html");
+        } else if (url.startsWith("/user/create")) {
+            UserService.registerUser(requestData);
+            return new Response(HttpStatusCode.FOUND, "/index.html");
+        } else if (url.equals("/user/login")) {
+            logger.debug("[API] /user/login");
+            return handleLogin(requestData);
+        } else if (url.equals("/user/logout")) {
+            logger.debug("[API] /user/logout");
+            return handleLogout(requestData);
+        } else {
+            logger.debug("유효하지 않은 API입니다.");
+            return new Response(HttpStatusCode.NOT_FOUND, "/error/notfound.html");
+        }
+    }
+
+    private static Response handleLogin(RequestData requestData) {
+        CookieData cookieData = UserService.login(requestData);
+        if (cookieData != null) {
+            return new Response(HttpStatusCode.FOUND, "/index.html", cookieData);
+        }
+        return new Response(HttpStatusCode.FOUND, "/index.html");
+    }
+
+    private static Response handleLogout(RequestData requestData) {
+        CookieData cookieData = UserService.logout(requestData);
+        return new Response(HttpStatusCode.FOUND, "/index.html", cookieData);
     }
 }
