89c23c83a0ab58631f63d0a7a6200d1a8e351e80
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-25 10:43:43 +0900

    feat : create Cookie object

---
+++ src/main/java/http/Cookie.java
@@ -0,0 +1,28 @@
+package http;
+
+public class Cookie {
+    private String key;
+    private String value;
+
+    public Cookie(){}
+    public Cookie(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
