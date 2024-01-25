18337d2f573544226d036df5c129af3949813e8c
admin <admin@admin-MacBook-Pro-3.local>
2024-01-25 17:12:28 +0900

    Chore: `modifyHtml` 메서드 해시맵 구성 변경
    기존에는 `변경 문서` : `기존 문서` 이었으나 `기존 문서` : `변경 문서`로 매핑함
    
    `/reources`의 모든 `html` 문서에 적용 가능하도록 해시맵 내용 추가함

--- src/main/java/util/DynamicHtml.java
+++ src/main/java/util/DynamicHtml.java
@@ -9,12 +9,13 @@ public class DynamicHtml {
 
         // 예시: 로그인 상태에 따라 동적으로 변경할 부분
         Map<String, String> dynamicContent = new HashMap<>();
-        dynamicContent.put("<li>사용자입니다.</li>", "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>");
+        dynamicContent.put("<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>", "<li>사용자입니다.</li>"); // `/index.html`의 로그인 버튼
+        dynamicContent.put("<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>", "<li>사용자입니다.</li>"); // `/user/*.html`의 로그인 버튼
 
         // 동적으로 HTML 수정
         for (Map.Entry<String, String> entry : dynamicContent.entrySet()) {
-            String loggedInText = entry.getKey();
-            String loggedOutText = entry.getValue();
+            String loggedOutText = entry.getKey();
+            String loggedInText = entry.getValue();
 
             int index = modifiedHtml.indexOf(loggedOutText);
             if (index != -1) {
