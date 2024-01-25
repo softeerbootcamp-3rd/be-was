3ea8bdbc45b8b87fb4b3a5b21103191c07b735e5
LEE, TAEGON <taegon1998@gmail.com>
2024-01-25 11:24:49 +0900

    feat: 로그인 요청을 처리 및 결과에 따른 HttpResponse entity를 생성
    login 요청시에 jsonParser 메서드를 통해 LoginRequest entity 생성
    login 성공: sessionId를 생성 및 저장하고, /index.html로 302 Status code와 함께 redirect
    login 실패: sessionId를 생성하지 않고, /user/login_failed.html로 302 Status code와 함께 redirect

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,14 +1,15 @@
 package controller;
 
 import model.User;
-import request.HttpRequest;
-import response.HttpResponse;
+import request.http.HttpRequest;
+import request.user.LoginRequest;
+import response.http.HttpResponse;
 import service.UserService;
 import util.Parser;
 
 import java.io.File;
-import java.io.IOException;
 
+import static db.Session.*;
 import static util.Uri.*;
 import static util.StatusCode.*;
 
@@ -29,7 +30,7 @@ public class UserController implements Controller {
     private final UserService userService = UserService.getInstance();
 
     @Override
-    public HttpResponse handleUserRequest(HttpRequest httpRequest) throws IOException {
+    public HttpResponse handleUserRequest(HttpRequest httpRequest) throws Exception {
 
         String uri = httpRequest.getUri();
         String filePath = httpRequest.getFilePath(uri);
@@ -42,23 +43,24 @@ public class UserController implements Controller {
         }
 
         if (method.equals("POST")) {
-            if (uri.equals(USER_CREATE.getUri())) {
-                try {
-                    User user = Parser.jsonParser(User.class, httpRequest.getRequestBody());
-                    signUp(user);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                return new HttpResponse(FOUND, "text/html", "/index.html");
+            if (uri.equals(USER_CREATE.getUri())) { // 회원가입
+                User user = Parser.jsonParser(User.class, httpRequest.getRequestBody());
+                userService.join(user);
 
+                return new HttpResponse(FOUND, "text/html", HOME_INDEX.getUri(), null);
+            }
+
+            if (uri.equals("/user/login")) { // 로그인
+
+                LoginRequest loginRequest = Parser.jsonParser(LoginRequest.class, httpRequest.getRequestBody());
+
+                if (!userService.login(loginRequest)) return new HttpResponse(FOUND, "text/html", USER_LOGIN_FAILED.getUri(), null);
+
+                String sessionId = getSessionId(loginRequest.getUserId());
+
+                return new HttpResponse(FOUND, "text/html", HOME_INDEX.getUri(), sessionId);
             }
         }
-
         return new HttpResponse(NOT_FOUND);
-    }
-
-    // 회원가입 요청 처리
-    public void signUp(User user) {
-        userService.join(user);
     }
 }
