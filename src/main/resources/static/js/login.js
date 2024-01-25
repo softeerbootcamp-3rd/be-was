0e0de8ed7f05ce74e2e211f5814ad28efd0fe5a1
h-sooah <s2sooas2@ewhain.net>
2024-01-25 10:54:19 +0900

    feat : 로그인 성공 시, 상단에 텍스트 표시
    유저가 로그인을 하면, 화면 우측 상단에 '사용지 이름'이라는 텍스트가 나타난다.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,16 +1,29 @@
 // js/login.js
 
 document.addEventListener("DOMContentLoaded", function () {
+    let isLoggedIn = false;
     const loginButton = document.getElementById("loginButton");
+    const userNameElement = document.getElementById("userName");
 
-    // 쿠키에서 "sid" 값을 가져옵니다.
+    // 쿠키에서 SID 값 가져오기
     const sid = getCookie("SID");
 
-    // "sid"가 유효하면 로그인 상태로 간주하고 버튼을 숨깁니다.
+    // SID 값이 있으면 로그인 한 상태
     if (sid) {
+        isLoggedIn = true;
+    } else {
+        isLoggedIn = false;
+    }
+
+    // 로그인 O : 로그인 버튼 숨김, 사용자 이름 표시
+    // 로그인 X : 로그인 버튼 표시, 사용자 이름 X
+    if (isLoggedIn) {
         loginButton.style.display = "none";
+        userNameElement.textContent = "사용자 이름";
+        userNameElement.style.display = "inline-block";
     } else {
         loginButton.style.display = "block";
+        userNameElement.style.display = "none";
     }
 
     // 쿠키에서 특정 이름의 쿠키 값을 가져오는 함수
