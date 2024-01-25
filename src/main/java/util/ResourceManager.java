2ca2cf57486886e90bcb2ec7e608ef4a670ec3ef
h-sooah <s2sooas2@ewhain.net>
2024-01-26 00:37:30 +0900

    refactor : ResoureManager 클래스 코드 수정
    - 가독성을 위해 코드 재구성
    - 정적 컨텐츠를 처리할 때, byte[]가 아닌 File 객체를 response에 담음(ResponseEntity의 body가 T 클래스이기 때문)

--- src/main/java/util/ResourceManager.java
+++ src/main/java/util/ResourceManager.java
@@ -1,13 +1,10 @@
 package util;
 
-import db.Database;
-import model.User;
 import webserver.http.HttpRequest;
 import webserver.http.HttpStatus;
 import webserver.http.HttpHeader;
 import webserver.http.ResponseEntity;
 
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.util.*;
 
@@ -15,6 +12,10 @@ public class ResourceManager {
 
     private static final Map<String, String> CONTENT_TYPE_MAP = new HashMap<>();
     private static final String DEFAULT_PATH = "src/main/resources";
+
+    private static final String USER_NAME_CLASS = "userNameClass";
+    private static final String LOGIN_BUTTON = "loginButton";
+    private static final String LOGOUT_BUTTON = "logoutButton";
 
     static {
         CONTENT_TYPE_MAP.put("html", "text/html");
@@ -25,6 +26,8 @@ public class ResourceManager {
         CONTENT_TYPE_MAP.put("jpeg", "image/jpeg");
         CONTENT_TYPE_MAP.put("gif", "image/gif");
         CONTENT_TYPE_MAP.put("font", "image/svg+xml");
+        CONTENT_TYPE_MAP.put("woff", "font/woff");
+        CONTENT_TYPE_MAP.put("ttf", "font/ttf");
     }
 
     public static String getContentType(String path) {
@@ -45,96 +48,33 @@ public class ResourceManager {
         String path = request.getPath().equals("/") ? "/index.html" : request.getPath();
         String contentType = getContentType(path);
 
-        byte[] body = null;
+        StringBuilder pathBuilder = new StringBuilder(DEFAULT_PATH);
         if (path.endsWith(".html")) {
-            String filePath = DEFAULT_PATH + "/templates" + path;
-            // html일 땐 한 줄씩 String으로 변환해서 처리
-            String SID = null;
-            if (request.getCookie() != null) {
-                SID = StringParser.getCookieValue(request.getCookie(), "SID");
-                if (!SessionManager.isValidateSession(SID))
-                    SID = null;
-            }
-            body = readFileLineByLine(filePath, SID);
-
+            pathBuilder.append("/templates");
         } else {
-            String filePath = DEFAULT_PATH + "/static" + path;
-            // static이면 그냥 byte로 처리
-            File file = new File(filePath);
-            body = readAllBytes(file);
+            pathBuilder.append("/static");
         }
+        pathBuilder.append(path);
 
-        if (body == null)
+        File file = new File(pathBuilder.toString());
+        if (!file.exists() || !file.isFile())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
         Map<String, List<String>> header = new HashMap<>();
         header.put(HttpHeader.CONTENT_TYPE, Collections.singletonList(contentType));
-        return new ResponseEntity<>(HttpStatus.OK, header, body);
+        header.put(HttpHeader.CONTENT_LENGTH, Collections.singletonList(String.valueOf(file.length())));
+        return new ResponseEntity<>(HttpStatus.OK, header, file);
     }
 
-    private static byte[] readFileLineByLine(String filePath, String SID) {
-        StringBuilder sb = new StringBuilder();
+    public static byte[] readAllBytes(File file) throws IOException {
+        byte[] byteArray = null;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.contains("userNameClass") && SID != null) {
-                    sb.append(line + "\n");
-                    sb.append("<li>\n"
-                            + "<a id=\"userName\">"
-                            + Database.findUserNameById((String) SessionManager.getAttribute(SID, "user"))
-                            + "님"
-                            + "</a>\n"
-                            + "</li>");
-                    continue;
-                }
-
-                if (line.contains("loginButton") && SID != null) {
-                    continue;
-                }
-
-                if (line.contains("logoutButton") && SID == null) {
-                    continue;
-                }
-
-                sb.append(line + "\n");
-
-                if (filePath.contains("/user/list") && line.contains("<tbody>") && SID != null) {
-                    int id = 1;
-                    Collection<User> users = Database.findAll();
-                    for (User u : users) {
-                        sb.append("<tr>\n");
-                        sb.append("<th scope=\"row\">" + id + "</th> <td>"
-                                + u.getUserId() + "</td> <td>" + u.getName() + "</td> <td>"
-                                + u.getEmail() + "</td> <td>");
-                        sb.append("<a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n");
-                        sb.append("</tr>\n");
-                        id++;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byteArray = new byte[(int) file.length()];
+            fis.read(byteArray);
         }
 
-        return sb.toString().getBytes();
-    }
-
-    private static byte[] readAllBytes(File file) throws IOException {
-        if (!file.exists())
-            return null;
-
-        try (FileInputStream fis = new FileInputStream(file); ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-            int size = (int) file.length();
-            byte[] buffer = new byte[size];
-            int bytesRead;
-
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                bos.write(buffer, 0, bytesRead);
-            }
-
-            return bos.toByteArray();
-        }
+        return byteArray;
     }
 
 }
