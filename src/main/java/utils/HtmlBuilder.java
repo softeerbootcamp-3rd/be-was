f87cd4af155fa5b7770ef0f26dce352accfacfb6
isexample <yeji0331@naver.com>
2024-01-25 19:03:26 +0900

    [Feat] 동적 HTML 생성을 위한 HtmlBuilder 클래스 구현
    정적 HTML 템플릿에 사용자 데이터를 동적으로 삽입하여 HTML을 생성하는 HtmlBuilder 클래스를 추가했습니다.

---
+++ src/main/java/utils/HtmlBuilder.java
@@ -0,0 +1,22 @@
+package utils;
+
+import model.User;
+
+import java.util.List;
+
+public class HtmlBuilder {
+    public static String userListHtml(List<User> users) {
+        StringBuilder userListHtml = new StringBuilder();
+        int index = 1;
+        for (User user : users) {
+            userListHtml.append("<tr>")
+                    .append("<th scope=\"row\">").append(index++).append("</th>")
+                    .append("<td>").append(user.getUserId()).append("</td>")
+                    .append("<td>").append(user.getName()).append("</td>")
+                    .append("<td>").append(user.getEmail()).append("</td>")
+                    .append("<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>")
+                    .append("</tr>");
+        }
+        return userListHtml.toString();
+    }
+}
