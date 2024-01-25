caf10dc6b04c7b2b6d17f99228c71a46a42094ff
admin <admin@admin-MacBook-Pro-3.local>
2024-01-25 18:49:10 +0900

    Feat: 로그인 상태인 경우 `/user/list.html`에 사용자 목록을 출력
    기존에는 `/user/list.html`에 하드코딩된 정보가 들어있음
    
    `Database`에 저장된 모든 사용자의 정보를 리스트로 출력함

--- src/main/java/util/DynamicHtml.java
+++ src/main/java/util/DynamicHtml.java
@@ -3,7 +3,9 @@ package util;
 import data.RequestData;
 import db.Database;
 import db.Session;
+import model.User;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +21,26 @@ public class DynamicHtml {
         dynamicContent.put("<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>", ""); // `/user/*.html`의 로그인 버튼
         dynamicContent.put("<li><a href=\"./user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>", "<li class=\"navbar-brand\">" + name + "님 안녕하세요</li>\r\n<li><a href=\"./user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>"); // `/index.html`의 로그인 버튼
         dynamicContent.put("<li><a href=\"../user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>", "<li class=\"navbar-brand\">" + name + "님 안녕하세요</li>\r\n<li><a href=\"../user/list.html\"><i class=\"glyphicon glyphicon-user\"></i></a></li>"); // `/user/*.html`의 로그인 버튼
+
+        // 사용자 리스트 동적 변경
+        Collection<User> allUsers = Database.findAll();
+
+        StringBuilder userListHtml = new StringBuilder();
+        for (User user : allUsers) {
+            userListHtml.append("<tr>\n")
+                    .append("    <th scope=\"row\">").append(user.getUserId()).append("</th>")
+                    .append("    <td>").append(user.getName()).append("</td>")
+                    .append("    <td>").append(user.getEmail()).append("</td>")
+                    .append("    <td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n")
+                    .append("</tr>\n");
+        }
+
+        dynamicContent.put("<tr>\n" +
+                "                    <th scope=\"row\">1</th> <td>javajigi</td> <td>자바지기</td> <td>javajigi@sample.net</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
+                "                </tr>\n" +
+                "                <tr>\n" +
+                "                    <th scope=\"row\">2</th> <td>slipp</td> <td>슬립</td> <td>slipp@sample.net</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
+                "                </tr>", userListHtml.toString());
 
         // 동적으로 HTML 수정
         for (Map.Entry<String, String> entry : dynamicContent.entrySet()) {
