d0be4aea68cc98fb92d8c36f0a52bc8f55980f71
win9 <krrr883@gmail.com>
2024-01-25 17:37:59 +0900

    [Feat] 로그인에 대한 에러 추가
    로그인시 발생할 수 있는 아이디 불일치, 패스워드 불일치 두가지 사항에 대한 에러를 구분

--- src/main/java/util/ErrorCode.java
+++ src/main/java/util/ErrorCode.java
@@ -3,7 +3,9 @@ package util;
 public enum ErrorCode {
 
     DUPLICATED_USER(HttpStatus.BAD_REQUEST, "존재하는 사용자 입니다."),
-    NOT_VALID_PATH(HttpStatus.NOT_FOUND, "잘못된 요청입니다.");
+    NOT_VALID_PATH(HttpStatus.NOT_FOUND, "잘못된 요청입니다."),
+    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자 입니다."),
+    NOT_VALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
