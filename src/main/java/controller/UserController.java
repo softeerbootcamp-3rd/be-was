b0b6b425a7fc1bbddb165a126f60b1d7b05bfc94
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-26 17:52:23 +0900

    feat : change get /user/login to render template & create user/list

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -11,9 +11,11 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
+import webserver.Model;
 
+import java.util.ArrayList;
 import java.util.Optional;
-
+import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController implements RequestController {
@@ -25,43 +27,50 @@ public class UserController implements RequestController {
     }
 
     @GetMapping(url = "/create")
-    public String createUser(@RequestParam(name = "email") String email, @RequestParam(name = "userId") String userId,
-                             @RequestParam(name = "name") String name, @RequestParam(name = "password") String password){
-        try{
-            User user = new User();
-            user.setEmail(email);
-            user.setUserId(userId);
-            user.setName(name);
-            user.setPassword(password);
-            userService.signUp(user);
-        }
-        catch (Exception e) {
-            throw new InternalException();
-        }
-        return "redirect:/user/login.html";
+    public String createUser(){
+        return "/user/login";
     }
 
-    @PostMapping(url = "/create")
+       @PostMapping(url = "/create")
     public String createUserPost(@RequestBody UserDto userDto){
         User user = new User(userDto);
         userService.signUp(user);
-        return "redirect:/index.html";
+        return "redirect:/";
     }
 
+    @GetMapping(url = "/login")
+    public String loginForm(){
+        return "user/login";
+    }
     @PostMapping(url = "/login")
     public String login(@RequestBody LoginDto form, Response response) {
         Optional<User> loginMember = userService.login(form.getUserId(), form.getPassword());
         if (loginMember.isEmpty()) {
-            return "user/login_failed.html";
+            return "user/login_failed";
         }
 
-        sessionManager.createSession(loginMember, response);
-        return "redirect:/index.html";
+        sessionManager.createSession(loginMember.get(), response);
+        return "redirect:/";
     }
 
     @GetMapping(url = "/logout")
     public String logout(Request req){
         sessionManager.expire(req);
-        return "redirect:/index.html";
+        return "redirect:/";
+    }
+
+    @GetMapping(url = "/list")
+    public String userList(Request req,Model model){
+        if(SessionManager.getSession(req,"SID") == null){
+            return "redirect:/user/login";
+        }
+        List<User> users = SessionManager.getLoginedUsers();
+        List<String> userjson = new ArrayList<>();
+        for(User user : users){
+            UserDto userDto = new UserDto(user);
+            userjson.add(userDto.toJsonString());
+        }
+        model.addAttribute("users",userjson);
+        return "user/list";
     }
 }
