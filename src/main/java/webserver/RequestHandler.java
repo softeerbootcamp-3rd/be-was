4a9b34b45f49a9432f17e949eaed45974d9a1ebb
OSSU <o.ss.u2056@gmail.com>
2024-01-26 10:20:32 +0900

    step 6 : 동적 HTML 처리를 위한 ResourceHandler 도입 RequestHandler에서 정적 자원 처리할 때 ResourceHandler의 process 메서드로 이동하여 파일을 읽어들이고 내부적으로 세션에 따라 동작하도록 수정함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,13 +3,12 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 
+import http.request.RequestLine;
 import http.response.HttpResponse;
 import http.request.HttpRequest;
 import http.HttpStatus;
-import model.User;
 import utils.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,47 +43,28 @@ public class RequestHandler implements Runnable {
             }
 
             // 정적 자원부터 탐색 후 동적 자원 처리
-            // 정적 자원 처리 - basePath 찾은 경우 or 세션 아이디가 없는 경우
-
-            // 세션 아이디 추출
-            String cookie = httpRequest.getEtcHeaders().getOrDefault("Cookie", "");
-            String sid = "";
-            if (!cookie.isEmpty()) {
-                sid = Parser.extractSid(cookie);
-            }
-            logger.info("sid : {}", sid);
-
+            // 정적 자원 처리 - basePath 찾은 경우
             String basePath = FileReader.getBasePath(requestUrl);
-            if (!basePath.isEmpty() && !Objects.requireNonNull(sid).isEmpty()) {
+            if (!basePath.isEmpty()) {
                 String filePath = basePath + requestUrl;
-                File file = new File(filePath);
-                // 파일 존재할 경우 해당 파일 반환
-                if (file.exists() && file.isFile()) {
-                    byte[] body = FileReader.readFile(file);
-
-                    // Request 헤더의 Accept 필드를 참고하여 반환할 타입 지정
-                    String contentType = httpRequest.getEtcHeaders().get("Accept").split(",")[0];
-                    response = HttpResponse.of(HttpStatus.OK, contentType, body);
-                } else {
-                    // 파일 존재하지 않을 경우 404 not found 반환
-                    logger.info("404 not found");
-                    response = HttpResponse.of(HttpStatus.NOT_FOUND);
-                }
+                response = ResourceHandler.process(filePath, httpRequest);
             } else {
                 // 동적 자원 처리
-                // method + path 갖고오기
-                String requestURL = httpRequest.getRequestLine().getMethodAndPath();
-                logger.debug("requestURL: {}", requestURL);
-                // 해당 값으로 Validator, Controller 탐색
-                ValidatorController vc = ValidatorController.getValidatorController(requestURL);
-                Function<Map<String, String>, Boolean> validator = vc.getValidator();
-                // 유효성 검증 통과한 경우 컨트롤러에 값 전달
-                if (validator.apply(httpRequest.getBody())) {
-                    Function<Map<String, String>, HttpResponse> controller = vc.getController();
-                    response = controller.apply(httpRequest.getBody());
-                } else {
+                RequestLine requestLine = httpRequest.getRequestLine();
+                ValidatorControllerMapper vc = ValidatorControllerMapper.getValidatorAndControllerByPath(requestLine.getPath());
+
+                if (vc != null) {
+                    Function<Map<String, String>, Boolean> validator = vc.getValidator();
                     // 유효성 검증 실패할 경우 BAD_REQUEST 반환
-                    response = HttpResponse.of(HttpStatus.BAD_REQUEST);
+                    if (validator != null && !validator.apply(httpRequest.getBody())) {
+                        response = HttpResponse.of(HttpStatus.BAD_REQUEST);
+                    } else {
+                        // validator가 없거나 유효성 검증에 통과할 경우 컨트롤러에게 HttpRequest
+                        Function<HttpRequest, HttpResponse> controller = vc.getController();
+                        response = controller.apply(httpRequest);
+                    }
+                } else {
+                    response = HttpResponse.of(HttpStatus.NOT_FOUND);
                 }
             }
 
@@ -96,5 +76,4 @@ public class RequestHandler implements Runnable {
             logger.error(e.getMessage());
         }
     }
-
 }
