1842886c3b0b71594357ae8694ddfbbecc6df913
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-26 21:52:45 +0900

    Refactor : UserController를 멤버변수로 변경
    기존에 메서드내에서 AppConfig.userController()를 통해 불러오던 컨트롤러를 멤버변수로 선언

--- src/main/java/handler/DynamicResponseHandlerImpl.java
+++ src/main/java/handler/DynamicResponseHandlerImpl.java
@@ -16,16 +16,21 @@ import static config.AppConfig.*;
 
 public class DynamicResponseHandlerImpl implements DynamicResponseHandler {
     private static class DynamicResponseHandlerHolder {
-        private static final DynamicResponseHandler INSTANCE = new DynamicResponseHandlerImpl();
+        private static final DynamicResponseHandler INSTANCE = new DynamicResponseHandlerImpl(userController());
     }
     private static final Logger logger = LoggerFactory.getLogger(DynamicResponseHandler.class);
     public static DynamicResponseHandler getInstance(){
         return DynamicResponseHandlerHolder.INSTANCE;
     }
+    private final UserController userController;
+
+    public DynamicResponseHandlerImpl(UserController userController) {
+        this.userController = userController;
+    }
+
     @Override
     public void handle(HttpRequest httpRequest, HttpResponseDto httpResponseDto) {
         try {
-            UserController userController = userController();
             userController.doGet(httpRequest, httpResponseDto);
         }catch (InvalidLogin e){
             handleInvalidLoginException(e, httpResponseDto);
