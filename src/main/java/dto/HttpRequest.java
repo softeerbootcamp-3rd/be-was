6e9e8ba8660d6d6c89bc32c4671c62b3635784c3
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-26 18:49:47 +0900

    Refactor : Request 요청의 body를 읽는 클래스 변경
    기존엔 RequestHandler 클래스에서 읽었다. 그러나 요청을 읽어 body를 추출하는 건 HttpRequest 클래스가 적절하다고 판단하여 HttpRequest 객체를 만들 때, body를 추출하도록 수정했다

--- src/main/java/dto/HttpRequest.java
+++ src/main/java/dto/HttpRequest.java
@@ -13,6 +13,7 @@ public class HttpRequest {
     private final String path;
     private final String queryString;
     private final Map<String, String> headers;
+    private final String body;
 
     public HttpRequest(BufferedReader br) throws IllegalAccessException, IOException {
         String line = br.readLine();
@@ -21,6 +22,12 @@ public class HttpRequest {
         this.path = tokens[0];
         this.queryString = tokens[1];
         this.headers = parseHeaders(br, line);
+        if (headers.containsKey("Content-Length")) {
+            int contentLength = Integer.parseInt(headers.get("Content-Length"));
+            this.body = getRequestBody(br, contentLength);
+        } else {
+            this.body = null;
+        }
         printRequest(this);
     }
 
@@ -38,5 +45,15 @@ public class HttpRequest {
 
     public Map<String, String> getHeaders() {
         return headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    private String getRequestBody(BufferedReader br, int contentLength) throws IOException {
+        char[] buffer = new char[contentLength];
+        br.read(buffer, 0, contentLength);
+        return new String(buffer);
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,16 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Map;
 
 import common.util.FileManager;
 import dto.HttpRequest;
 import dto.HttpResponse;
-import http.ContentType;
 import http.ExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static dto.HttpResponse.*;
 import static http.Status.*;
 import static common.WebServerConfig.*;
 
@@ -35,15 +34,13 @@ public class RequestHandler implements Runnable {
             HttpResponse response = new HttpResponse();
 
             String path = request.getPath();
-            String contentType = getContentType(path);
-
             try {
-                if (request.getMethod().equals("GET") && (FileManager.getFile(path, contentType)) != null) {
+                if (request.getMethod().equals("GET")
+                        && (FileManager.getFile(path, getContentType(request.getPath()))) != null) {
                     response.makeBody(OK, path);
                 }
                 if (request.getMethod().equals("POST")) {
-                    int contentLength = getContentLength(request);
-                    String body = getRequestBody(br, contentLength);
+                    String body = request.getBody();
                     if (path.equals("/user/create")) {
                         response = userController.create(body);
                     }
@@ -59,28 +56,5 @@ public class RequestHandler implements Runnable {
         } catch (Exception e) {
             logger.debug(e.getMessage());
         }
-    }
-
-    private int getContentLength(HttpRequest request) {
-        Map<String, String> headers = request.getHeaders();
-        String contentLength = headers.get("Content-Length");
-        return Integer.parseInt(contentLength);
-    }
-
-    private String getRequestBody(BufferedReader br, int contentLength) throws IOException {
-        char[] buffer = new char[contentLength];
-        br.read(buffer, 0, contentLength);
-        return new String(buffer);
-    }
-
-    private static String getContentType(String path) {
-        String extension = getFileExtension(path);
-        return ContentType.getMimeType(extension);
-    }
-
-    private static String getFileExtension(String path) {
-        File file = new File(path);
-        String name = file.getName();
-        return name.substring(name.lastIndexOf(".") + 1);
     }
 }
