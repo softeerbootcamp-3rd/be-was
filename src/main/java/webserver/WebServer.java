b415837b4aac9c424f8faf316c17e9e9451a1e62
kelly1422 <kelly1422@naver.com>
2024-01-26 10:43:49 +0900

    [step-6]: executor.shutdown() 프로그램 종료시 스레드 풀을 종료 시켜, 혹시모를 메모리 누수 등의 문제 방지

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -3,6 +3,7 @@ package webserver;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
@@ -20,16 +21,21 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
-            Executor executor = Executors.newFixedThreadPool(10);
             logger.info("Web Application Server started {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                executor.execute(new RequestHandler(connection));
+                executor.submit(new RequestHandler(connection));
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
         }
     }
 }
