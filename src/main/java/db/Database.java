98076318d4ef415219a39131cfe99ae956d0c6cb
win9 <krrr883@gmail.com>
2024-01-27 21:31:23 +0900

    [Feat] 세션 추가
    세션을 설정하고 이를 저장할 수 있는 데이터베이스 추가

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -3,18 +3,17 @@ package db;
 import com.google.common.collect.Maps;
 
 import model.User;
+import util.Session;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 
 public class Database {
     private static Map<String, User> users = Maps.newHashMap();
-    private static Map<String, String> sessoinDB = Maps.newConcurrentMap();
+    private static Map<String, Session> sessoinDB = Maps.newConcurrentMap();
 
-    public static void addSession(String UUID, String userId) {
-        sessoinDB.put(UUID, userId);
-    }
-
+    // User
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
     }
@@ -25,5 +24,14 @@ public class Database {
 
     public static Collection<User> findAll() {
         return users.values();
+    }
+
+    // Session
+    public static void addSession(Session session) {
+        sessoinDB.put(session.getSessionId(), session);
+    }
+
+    public static Session getSession(String sessionId) {
+        return sessoinDB.get(sessionId);
     }
 }
