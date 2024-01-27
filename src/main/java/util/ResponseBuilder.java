9cf4dc533f6103b2177995c90839dac5a1d44323
win9 <krrr883@gmail.com>
2024-01-27 21:32:47 +0900

    [Feat] 리다리렉션 추가
    다양한 리다이렉션 URL을 설정하고 이에대해 유연하게 리다이렉션을 설정하라고 반환할 수 있도록 설정

--- src/main/java/util/ResponseBuilder.java
+++ src/main/java/util/ResponseBuilder.java
@@ -1,18 +1,23 @@
 package util;
 
+import db.Database;
 import model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class ResponseBuilder {
     private static final Logger logger = LoggerFactory.getLogger(ResponseBuilder.class);
 
-    public static void sendResponse(DataOutputStream dos, byte[] body, HttpStatus httpStatus, String extension) throws IOException {
+    public static void sendResponse(DataOutputStream dos, byte[] body, String path, HttpStatus httpStatus, String extension) throws IOException {
         try {
-            sendHeader(dos, httpStatus, body.length, extension);
+            sendHeader(dos, httpStatus, path, body.length, extension);
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
@@ -20,22 +25,29 @@ public class ResponseBuilder {
         }
     }
 
-    public static void sendHeader(DataOutputStream dos, HttpStatus httpStatus, int bodyLength, String extension) throws IOException {
+    public static void sendHeader(DataOutputStream dos, HttpStatus httpStatus, String path, int bodyLength, String extension) throws IOException {
         dos.writeBytes("HTTP/1.1  " + httpStatus.getCode() + " " + httpStatus.getMessage() + "\r\n");
         if (httpStatus == HttpStatus.REDIRECT) {
-            dos.writeBytes("Location: http://localhost:8080/index.html\r\n");
+            dos.writeBytes("Location: http://localhost:8080" + path + "\r\n");
         }
         dos.writeBytes("Content-Type: text/" + extension + ";charset=utf-8\r\n");
         dos.writeBytes("Content-Length: " + bodyLength + "\r\n");
         Model.getAttribute("sessionId").ifPresent(
                 value -> {
+                    String stringValue = String.valueOf(value);
                     try {
-                        dos.writeBytes("Set-Cookie: sid=" + value + "; Path=/\r\n");
+                        dos.writeBytes("Set-Cookie: sid=" + stringValue + "; Expires="
+                                + formattingDate(Database.getSession(stringValue).getExpires()) + "; Path=/");
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
         );
         dos.writeBytes("\r\n");
+    }
+
+    private static String formattingDate(LocalDateTime date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zz", Locale.ENGLISH);
+        return formatter.format(date);
     }
 }
