2baea8dc9724b8016a95881d93c34b4a664fc77c
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-27 17:16:37 +0900

    Test(UtilTest) : 이진 데이터 디코딩 테스트 작성

--- src/test/java/common/util/UtilTest.java
+++ src/test/java/common/util/UtilTest.java
@@ -3,11 +3,27 @@ package common.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.UnsupportedEncodingException;
+
 import static common.util.Util.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UtilTest {
+    
+    @Test
+    @DisplayName("이진데이터의 디코딩 테스트")
+    void decodeTest() throws UnsupportedEncodingException {
+        
+        //given
+        String name = "%EB%B0%95%EC%9E%AC%EC%84%B1";
 
+        //when
+        String decodeName = decode(name);
+
+        //then
+        assertThat(decodeName).isEqualTo("박재성");
+    }
+    
     @Test
     @DisplayName("10자리 랜덤 문자열 생성")
     void getSessionIdTest() {
