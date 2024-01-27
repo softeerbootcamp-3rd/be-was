dc705d460a79038c881b5dad2e97fb511e1c8f4b
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-01-27 17:15:57 +0900

    Refactor : Http Response body 값 할당의 중복 코드 제거
    HttpResponse 클래스의 getBody와 FileManager 클래스의 getFile은 같은 기능을 하는 중복코드이므로 제거했다

--- src/main/java/dto/HttpResponse.java
+++ src/main/java/dto/HttpResponse.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static common.util.FileManager.getFile;
+
 public class HttpResponse {
 
     private Status status;
@@ -26,28 +28,13 @@ public class HttpResponse {
     public void makeBody(Status status, String path) throws IOException {
         String contentType = getContentType(path);
         this.status = status;
-        this.body = getBody(path, contentType);
+        this.body = getFile(path, contentType);
         addHeader("Content-Type", contentType);
         addHeader("Content-Length", String.valueOf(body.length));
     }
 
     public void addHeader(String key, String value) {
         headers.put(key, value);
-    }
-
-    private byte[] getBody(String url, String contentType) throws IOException {
-        // 파일 경로 판단
-        String path = RESOURCES_PATH;
-        if (contentType.equals("text/html")) {
-            path += "templates";
-        } else {
-            path += "static";
-        }
-
-        // 파일을 바이트 포맷으로 변환
-        try (FileInputStream fileInputStream = new FileInputStream(path + url)) {
-            return fileInputStream.readAllBytes();
-        }
     }
 
     public Status getStatus() {
