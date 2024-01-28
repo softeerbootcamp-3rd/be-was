8912fc07fe5bceb6f2883cd7573a73b2b73a5a58
Hymin2 <khm8246@gmail.com>
2024-01-29 00:29:44 +0900

    feat: 로그인 실패 시 예외 핸들링 추가
    로그인 실패 예외가 발생했을 때 login_failed.html로 리다이렉트 될 수 있도록 핸들링을 추가했습니다.

--- src/main/java/webserver/handler/ExceptionHandler.java
+++ src/main/java/webserver/handler/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package webserver.handler;
 
 import webserver.exception.GeneralException;
+import webserver.exception.LoginFailedException;
 import webserver.exception.UserIdAlreadyExistsException;
 import webserver.response.Response;
 import webserver.status.ErrorCode;
@@ -9,12 +10,17 @@ public class ExceptionHandler {
     public static Response handle(Throwable e){
         if(e instanceof UserIdAlreadyExistsException){
             return handleUserIdAlreadyExistsException();
-        }
-        else if(e instanceof GeneralException){
+        } else if(e instanceof LoginFailedException){
+            return handleLoginFailedException();
+        } else if(e instanceof GeneralException){
             return handleGeneralException((GeneralException) e);
         } else{
             return handleException();
         }
+    }
+
+    private static Response handleLoginFailedException(){
+        return Response.redirect("/user/login_failed.html");
     }
 
     private static Response handleUserIdAlreadyExistsException(){
