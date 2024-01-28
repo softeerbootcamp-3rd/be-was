6a1f87a6a37d6d88c62dd5668ec91fd110de0f82
kelly1422 <kelly1422@naver.com>
2024-01-29 03:12:49 +0900

    [step-6]: 어노테이션 멤버 value로 변경 멤버명을 value로 하여 어노테이션을 사용할 때, 따로 변수명을 입력하지 않아도 되도록 하였습니다.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,24 +1,24 @@
 package controller;
 
 import annotation.PostMapping;
-import webserver.HttpRequest;
-import webserver.HttpResponse;
+import webserver.request.HttpRequest;
+import webserver.response.HttpResponse;
 import service.UserService;
 
-import static constant.StaticFile.LOGIN_FAILED_PATH;
+import static constant.StaticFile.LOGIN_FAILED_PAGE_PATH;
 import static constant.StaticFile.MAIN_PAGE_PATH;
 
 
 public class UserController {
 
-    @PostMapping(path = "/user/create")
+    @PostMapping("/user/create")
     public static HttpResponse create(HttpRequest httpRequest) {
         UserService.signUp(httpRequest.getBody());
 
         return new HttpResponse().makeRedirect(MAIN_PAGE_PATH);
     }
 
-    @PostMapping(path = "/user/login")
+    @PostMapping("/user/login")
     public static HttpResponse login(HttpRequest httpRequest) {
         HttpResponse httpResponse = new HttpResponse();
 
@@ -26,13 +26,13 @@ public class UserController {
         String userId = httpRequest.getBody().get("userId");
         String password = httpRequest.getBody().get("password");
 
+        // todo max-age 설정
         if ((sessionId = UserService.login(userId, password)) != null ) {
             httpResponse.addHeader("Set-Cookie", ("sessionId=" + sessionId + "; Path=/"));
             httpResponse.makeRedirect(MAIN_PAGE_PATH);
         } else {
-            httpResponse.makeRedirect(LOGIN_FAILED_PATH);
+            httpResponse.makeRedirect(LOGIN_FAILED_PAGE_PATH);
         }
-        // UserService.login 하고 생성한 유저의 sessionId 값을 응답 헤더에 추가해 응답 리턴
 
         return httpResponse;
     }
