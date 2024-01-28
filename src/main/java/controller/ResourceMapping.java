0f2252eb9f2c9b5bb5addb09ec911aa0ba25c6e6
admin <admin@admin-MacBook-Pro-3.local>
2024-01-28 21:56:54 +0900

    Refactor: 리소스 매핑 열거형에서 중복된 문자열 리터럴을 상수로 대체
    중복된 문자열 리터럴을 방지하기 위해 ResourceMapping 열거형에서 ResourceConstants 클래스를 도입하여 상수로 문자열 리터럴을 정의함

--- src/main/java/controller/ResourceMapping.java
+++ src/main/java/controller/ResourceMapping.java
@@ -1,14 +1,14 @@
 package controller;
 
 public enum ResourceMapping {
-    HTML("/templates", "text/html"),
-    CSS("/static", "text/css"),
-    JS("/static", "application/javascript"),
-    ICO("/static", "image/x-icon"),
-    PNG("/static", "image/png"),
-    JPG("/static", "image/jpg"),
-    WOFF("/static", "font/woff"),
-    TTF("/static", "font/ttf");
+    HTML(ResourceConstants.TEMPLATE_DIRECTORY, "text/html"),
+    CSS(ResourceConstants.STATIC_DIRECTORY, "text/css"),
+    JS(ResourceConstants.STATIC_DIRECTORY, "application/javascript"),
+    ICO(ResourceConstants.STATIC_DIRECTORY, "image/x-icon"),
+    PNG(ResourceConstants.STATIC_DIRECTORY, "image/png"),
+    JPG(ResourceConstants.STATIC_DIRECTORY, "image/jpg"),
+    WOFF(ResourceConstants.STATIC_DIRECTORY, "font/woff"),
+    TTF(ResourceConstants.STATIC_DIRECTORY, "font/ttf");
 
     private final String directory;
     private final String contentType;
@@ -23,5 +23,10 @@ public enum ResourceMapping {
     }
     public String getContentType() {
         return contentType;
+    }
+
+    public static class ResourceConstants {
+        public static final String TEMPLATE_DIRECTORY = "/templates";
+        public static final String STATIC_DIRECTORY = "/static";
     }
 }
