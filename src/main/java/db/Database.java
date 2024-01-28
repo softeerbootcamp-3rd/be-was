49db4f21790282d6b097236a06e8ac9ffb565ab2
Hymin2 <khm8246@gmail.com>
2024-01-29 00:51:18 +0900

    fix: User 저장 Map을 ConcurrentMap으로 변경
    기존 멀티 쓰레드 환경에서 동시성 문제를 일어날 수 있는 HashMap을 ConcurrentHashMap으로 변경했습니다.

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -5,9 +5,10 @@ import model.User;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Database {
-    private static Map<String, User> users = Maps.newHashMap();
+    private static final Map<String, User> users = new ConcurrentHashMap<>();
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
