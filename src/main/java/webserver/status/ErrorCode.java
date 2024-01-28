651791e4490f48208d9f19c18bc4477b8509fdca
Hymin2 <khm8246@gmail.com>
2024-01-29 00:22:03 +0900

    feat: ErrorCode에 로그인 실패의 ErrorCode를 추가
    ErrorCode에 로그인 실패 했을 때의 ErrorCode를 추가했습니다.

--- src/main/java/webserver/status/ErrorCode.java
+++ src/main/java/webserver/status/ErrorCode.java
@@ -4,6 +4,7 @@ public enum ErrorCode {
     RESOURCE_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "요청하신 데이터를 찾을 수 없습니다."),
     ILLEGAL_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
     USER_ID_ALREADY_EXISTS_ERROR(HttpStatus.CONFLICT, "현재 사용중인 아이디입니다."),
+    LOGIN_FAILED_ERROR(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 틀립니다. 다시 로그인 해주세요."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
