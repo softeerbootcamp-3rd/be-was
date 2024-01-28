35c7d651af5f5594d73afe9bd4c4d2fd1dfc8433
dlwnsgus07 <dlwnsgus07@naver.com>
2024-01-28 18:56:05 +0900

    Feat : Controller mapper 및 handler 기능 추가
    어노테이션을 활용한 path 매핑을 해주기 위해 객체를 생성하는 시점에서 자바 리플랙션을 활용하여 모든 주소를 조합해 map으로 만들어두는 register와 handleRequest를 추가

--- src/main/java/handler/DynamicResponseHandlerImpl.java
+++ src/main/java/handler/DynamicResponseHandlerImpl.java
@@ -1,19 +1,26 @@
 package handler;
 
-import controller.UserController;
+import annotation.GetMapping;
+import annotation.PostMapping;
+import annotation.RequestMapping;
 import dto.HttpResponseDto;
 import exception.BadRequestException;
 import model.http.ContentType;
+import model.http.HttpMethod;
 import model.http.Status;
 import model.http.request.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static config.AppConfig.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+import static config.AppConfig.userController;
 
 public class DynamicResponseHandlerImpl implements DynamicResponseHandler {
     private static class DynamicResponseHandlerHolder {
-        private static final DynamicResponseHandler INSTANCE = new DynamicResponseHandlerImpl(userController());
+        private static final DynamicResponseHandler INSTANCE = new DynamicResponseHandlerImpl();
     }
 
     private static final Logger logger = LoggerFactory.getLogger(DynamicResponseHandler.class);
@@ -22,25 +29,66 @@ public class DynamicResponseHandlerImpl implements DynamicResponseHandler {
         return DynamicResponseHandlerHolder.INSTANCE;
     }
 
-    private final UserController userController;
+    private final HashMap<String, Method> urlMapper = new HashMap<>();
+    private final HashMap<String, Object> controllerMapper = new HashMap<>();
 
-    public DynamicResponseHandlerImpl(UserController userController) {
-        this.userController = userController;
+    public DynamicResponseHandlerImpl() {
+        register(userController());
     }
-
-    @Override
-    public void handle(HttpRequest httpRequest, HttpResponseDto httpResponseDto) {
-        try {
-            userController.doGet(httpRequest, httpResponseDto);
-        } catch (BadRequestException e) {
-            handleBadRequestException(e, httpResponseDto);
+    public void register(Object controller) {
+        logger.debug("Controller Mapper에 컨트롤러 등록");
+        Class<?> clazz = controller.getClass();
+        controllerMapper.put(clazz.getName(), controller);
+        if (clazz.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping classAnnotation = clazz.getAnnotation(RequestMapping.class);
+            String baseUrl = classAnnotation.path();
+            Method[] methods = clazz.getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(GetMapping.class)) {
+                    GetMapping getAnnotation = method.getAnnotation(GetMapping.class);
+                    String url = baseUrl + getAnnotation.value();
+                    urlMapper.put(url, method);
+                } else if (method.isAnnotationPresent(PostMapping.class)) {
+                    PostMapping postAnnotation = method.getAnnotation(PostMapping.class);
+                    String url = baseUrl + postAnnotation.value();
+                    urlMapper.put(url, method);
+                }
+            }
         }
     }
 
-    private void handleBadRequestException(BadRequestException e, HttpResponseDto httpResponseDto) {
+    public void handleRequest(HttpRequest httpRequest, HttpResponseDto httpResponseDto) throws InvocationTargetException, IllegalAccessException, InstantiationException {
+        String url = httpRequest.getStartLine().getPathUrl();
+        Method method = urlMapper.get(url);
+        if (method != null) {
+            Object controller = controllerMapper.get(method.getDeclaringClass().getName());
+            logger.debug(method.getName() + "호출");
+            if(method.isAnnotationPresent(GetMapping.class) && httpRequest.getStartLine().getMethod() == HttpMethod.GET){
+                method.invoke(controller, httpRequest, httpResponseDto);
+            } else if (method.isAnnotationPresent(PostMapping.class) && httpRequest.getStartLine().getMethod() == HttpMethod.POST) {
+                method.invoke(controller, httpRequest, httpResponseDto);
+            }
+            else{
+                logger.error("처리할 수 없는 요청이 들어옴");
+                throw new BadRequestException("해당하는 요청을 처리할 수 없습니다.");
+            }
+        }
+    }
+    @Override
+    public void handle(HttpRequest httpRequest, HttpResponseDto httpResponseDto) {
+        try {
+            handleRequest(httpRequest, httpResponseDto);
+        } catch (InvocationTargetException e) {
+            handleBadRequestException(e, httpResponseDto);
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.getStackTrace();
+        }
+    }
+
+    private void handleBadRequestException(InvocationTargetException e, HttpResponseDto httpResponseDto) {
         httpResponseDto.setStatus(Status.BAD_REQUEST);
         httpResponseDto.setContentType(ContentType.PLAIN);
-        httpResponseDto.setContent(e.getMessage().getBytes());
+        httpResponseDto.setContent(e.getCause().getMessage().getBytes());
         logger.error("Bad_Request 발생", e.getMessage());
         e.getStackTrace();
     }
