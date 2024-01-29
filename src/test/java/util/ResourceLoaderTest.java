7c5eb1d140e2b9746a499de5564188b4fd09120a
admin <admin@admin-MacBook-Pro-3.local>
2024-01-29 10:37:33 +0900

    test: ResourceLoader 테스트 코드 변경
    - getResourceType 관련 테스트 제거
    - 경로에 해당하는 파일을 byte[]로 잘 불러오는지 검증

--- src/test/java/util/ResourceLoaderTest.java
+++ src/test/java/util/ResourceLoaderTest.java
@@ -1,60 +1,63 @@
 package util;
 
 
+import controller.HttpMethod;
+import controller.ResourceMapping;
+import data.RequestData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static util.RequestParserUtil.getFileExtension;
 
 public class ResourceLoaderTest {
 
     static Stream<Object[]> provideUrl() {
         return Stream.of(
-                new Object[]{"/", "API"},
                 new Object[]{"/index.html", "FILE"},
                 new Object[]{"/user/form.html", "FILE"},
                 new Object[]{"/user/list.html", "FILE"},
                 new Object[]{"/user/login.html", "FILE"},
-                new Object[]{"/user/profile.html", "FILE"},
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=asdf", "API"}
+                new Object[]{"/user/profile.html", "FILE"}
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideUrl")
-    @DisplayName("유효한 입력")
-    public void getResourceType_ValidUrl(String path, String expect) {
+    @DisplayName("리소스 로딩 테스트")
+    public void loadResource_ValidResourcePath(String path, String expect) throws IOException {
         // Given
+        String extension = getFileExtension(path);
+        String directory = ResourceMapping.valueOf(extension.toUpperCase()).getDirectory();
+        File file = new File(ResourceLoader.RESOURCE_URL + directory + path);
+        byte[] expectedResult;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            // 파일 내용을 바이트 배열로 읽어오기
+            expectedResult = new byte[(int) file.length()];
+            int bytesRead = inputStream.read(expectedResult);
+
+            // bytesRead가 -1이 아니면 계속 읽기
+            while (bytesRead != -1) {
+                bytesRead = inputStream.read(expectedResult);
+            }
+        }
 
         // When
-        String actual = ResourceLoader.getResourceType(path);
+        byte[] actualResult = ResourceLoader.loadResource(path, createDummyRequestData());
 
         // Then
-        assertThat(actual).isEqualTo(expect);
-
+        assertThat(actualResult).isEqualTo(expectedResult);
     }
 
-    static Stream<Object[]> provideUrlWithEmail() {
-        return Stream.of(
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=asdf@gmail.com", "API"},
-                new Object[]{"/user/create?userId=asdf&password=asdf&name=asdf&email=asdf@aa.bb.cc", "API"}
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("provideUrlWithEmail")
-    @DisplayName("이메일('~~.com') 입력")
-    public void getResourceType_ValidUrl_Email(String path, String expect) {
-        // Given
-
-        // When
-        String actual = ResourceLoader.getResourceType(path);
-
-        // Then
-        assertThat(actual).isEqualTo(expect);
-
+    private RequestData createDummyRequestData() {
+        // 테스트를 위한 더미 RequestData 객체 생성
+        return new RequestData(HttpMethod.GET, "/", "HTTP/1.1", null, false);
     }
 }
