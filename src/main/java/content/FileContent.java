88734e340f7e63781dba5d0c6c44913b9de19545
win9 <krrr883@gmail.com>
2024-01-29 17:14:41 +0900

    [Feat] content Enum 생성
    로그인 여부에 따른 동적 HTML파일을 넣을 수 있도록 추가

---
+++ src/main/java/content/FileContent.java
@@ -0,0 +1,24 @@
+package content;
+
+import model.Model;
+
+public enum FileContent {
+    NON_LOGIN("<li><a href=\"user/login.html\" role=\"button\">회원가입</a></li>\n" +
+            "<li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>"),
+    LOGIN("<li><a href=\"#\" role=\"button\">로그아웃</a></li>\n" +
+            "<li><a href=\"user/form.html\" role=\"button\">%s</a></li>\n" +
+            "<li><a href=\"#\" role=\"button\">개인정보 수정</a></li>");
+
+    private String text;
+    FileContent (String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getText(String username) {
+        return String.format(text, username);
+    }
+}
