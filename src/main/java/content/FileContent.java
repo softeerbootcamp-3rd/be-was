be39a1a2d90965e8412bc6f58b9bdd8a1f85d5a7
win9 <krrr883@gmail.com>
2024-01-29 23:29:02 +0900

    [Feat] 사용자리스트 content추가
    파라미터를 반영하여 전체 사용자의 리스트를 불러올 수 있는 enum생성

--- src/main/java/content/FileContent.java
+++ src/main/java/content/FileContent.java
@@ -7,8 +7,11 @@ public enum FileContent {
             "<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>"),
     LOGIN("<li><a href=\"#\" role=\"button\">로그아웃</a></li>\n" +
             "<li><a href=\"#\" role=\"button\">%s 님</a></li>\n" +
-            "<li><a href=\"#\" role=\"button\">개인정보 수정</a></li>\n");
-
+            "<li><a href=\"#\" role=\"button\">개인정보 수정</a></li>\n"),
+    USER_LIST("<tr>\n" +
+            "<th scope=\"row\">%s</th> " +
+            "<td>%s</td> <td>%s</td> <td>%s</td><td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>\n" +
+            "</tr>");
     private String text;
     FileContent (String text) {
         this.text = text;
@@ -20,5 +23,10 @@ public enum FileContent {
 
     public String getText(String username) {
         return String.format(text, username);
+    }
+
+    public String getText(int index, String id, String name, String email) {
+        System.out.println("here");
+        return String.format(text, index, id, name, email);
     }
 }
