47382115c8778ae1f57585a8954277cdd4584e45
jjyk999 <jjyk999@gmail.com>
2024-01-29 11:46:18 +0900

    step3 완성
    예전에 해둔 내용들 업로드..

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -33,28 +34,143 @@ public class RequestHandler implements Runnable {
 
             String[] splitHeader = line.split(" ");
             String reqPath = "src/main/resources/templates";
+            int bodyLength = 0;
+            String userInfoString = null;
+            DataOutputStream dos = new DataOutputStream(out);
 
-            if(splitHeader[1].contains("html")) {
-                logger.debug("splitHeader: " + splitHeader[1]);
-                reqPath += splitHeader[1];
+            // POST request 읽어오기
+            char[] bodyContent = null;
+            int c;
+            if(splitHeader[0].equals("POST")) {
+                // Content-Length 확인
+                while((line = br.readLine()) != null) {
+                    logger.debug(line);
+                    if(line.contains("Content-Length")){
+                        bodyLength = Integer.parseInt(line.split(" ")[1]);
+
+                    }
+                    if(line.isEmpty()) {
+                        logger.debug("<<<<Http body part>>>>");
+                        char[] httpBody = new char[bodyLength];
+                        br.read(httpBody);
+                        userInfoString = new String(httpBody);
+                        logger.debug("userInfoString : " + userInfoString);
+                        break;
+                    }
+                }
             }
 
-            // 회원가입 정보 추출
-            if(splitHeader[1].contains("create")) {
+            // extract path
+            int idx = splitHeader[1].lastIndexOf(".");
+            String ext = splitHeader[1].substring(idx+1);
+
+            if(ext.equals("html")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath += splitHeader[1];
+                reqType = "text/html";
+            }
+            if(ext.equals("css")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "text/css";
+            }
+            if(ext.equals("woff")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "font/woff";
+            }
+            if(ext.equals("ttf")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "font/ttf";
+            }
+            if(ext.equals("js")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "application/javascript";
+            }
+            if(ext.equals("ico")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "image/x-icon";
+            }
+
+            if(ext.equals("png")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "image/png";
+            }
+            if(ext.equals("jpg")) {
+                logger.debug("splitHeader: " + splitHeader[1]);
+                reqPath = "src/main/resources/static" + splitHeader[1];
+                reqType = "image/jpg";
+            }
+
+            // 로그인 시도
+            if(splitHeader[1].contains("login") && splitHeader[0].equals("POST")) {
                 String userId = null, userPwd = null, userName = null, userEmail = null;
 
-                String[] userInfo = splitHeader[1].split("&");
-                String[] idSplit = userInfo[0].split("=");
-                userId = idSplit[1];
+                if(splitHeader[0].equals("POST")) { // 삭제 예정
+                    String[] userInfo = userInfoSplit(userInfoString,false);
+                    userId = userInfo[0];
+                    userPwd = userInfo[1];
+                    logger.debug(">>>>>>>userId : " + userId + " | " + "userPwd : " + userPwd);
+                }
 
-                String[] pwdSplit = userInfo[1].split("=");
-                userPwd = pwdSplit[1];
+                User userData;
 
-                String[] nameSplit = userInfo[2].split("=");
-                userName = nameSplit[1];
+                if( (userData = Database.findUserById(userId)) != null) {
+                    // 로그인 성공
+                    if(userData.getPassword().equals(userPwd)) {
+                        logger.debug(">>>>>>>>login success");
+                        response302Header(dos, "index.html");
+                    }
+                    // 로그인 실패(비밀번호 불일치)
+                    else {
+                        logger.debug(">>>>>>>>login failed - wrong password");
+                        response302Header(dos, "user/login_failed.html");
+                    }
+                }
+                //로그인 실패(미등록 유저)
+                else {
+                    logger.debug(">>>>>>>>login failed - unregistered user");
+                    response302Header(dos, "user/login_failed.html");
+                }
 
-                String[] emailSplit = userInfo[3].split("=");
-                userEmail = emailSplit[1];
+            }
+            // 회원가입 정보 추출 & 리다이렉트
+            else if(splitHeader[1].contains("create")) {
+                String userId = null, userPwd = null, userName = null, userEmail = null;
+                String[] userInfo = null;
+                if(splitHeader[0].equals("GET")) {
+                    userInfo = splitHeader[1].split("&");
+                    String[] idSplit = userInfo[0].split("=");
+                    userId = idSplit[1];
+
+                    String[] pwdSplit = userInfo[1].split("=");
+                    userPwd = pwdSplit[1];
+
+                    String[] nameSplit = userInfo[2].split("=");
+                    userName = nameSplit[1];
+
+                    String[] emailSplit = userInfo[3].split("=");
+                    userEmail = emailSplit[1];
+                }
+                else if(splitHeader[0].equals("POST")) {
+                    userInfo = userInfoString.split("&");
+                    String[] idSplit = userInfo[0].split("=");
+                    userId = idSplit[1];
+
+                    String[] pwdSplit = userInfo[1].split("=");
+                    userPwd = pwdSplit[1];
+
+                    String[] nameSplit = userInfo[2].split("=");
+                    userName = nameSplit[1];
+
+                    String[] emailSplit = userInfo[3].split("=");
+                    userEmail = emailSplit[1];
+                }
+
 
                 // 유저 저장
                 User user = new User(userId, userPwd, userName, userEmail);
@@ -65,27 +181,42 @@ public class RequestHandler implements Runnable {
                 logger.debug("UserPwd : " + user.getPassword());
                 logger.debug("UserName : " + user.getName());
                 logger.debug("UserEmail : " + user.getEmail());
+
+                response302Header(dos, "index.html");
+
             }
 
-            //logger.debug(reqPath);
-            DataOutputStream dos = new DataOutputStream(out);
-            String readResult = readFile(reqPath);
+            byte[] body = readFile(reqPath);
 
-            byte[] body = readResult.getBytes();
-
-            response200Header(dos, body.length);
+            response200Header(dos, body.length, reqType);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            if(contentType.equals("html"))
+                dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            else
+                dos.writeBytes("Content-Type: " + reqType + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String location) {
+        logger.debug(">>>>>>Response 302");
+        try{
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            // 리다이렉트 위치
+            String redirectPath = "Location : http://localhost:8080/" + location;
+            dos.writeBytes(redirectPath);
+            System.out.println(redirectPath);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -100,24 +231,33 @@ public class RequestHandler implements Runnable {
         }
     }
 
-    public String readFile(String filePath) {
-        BufferedReader br = null;
-        StringBuffer sb = new StringBuffer();
-        try {
-            br = new BufferedReader(new FileReader(filePath));
-            String ln = null;
+    private String[] userInfoSplit(String userInfoString, boolean extraInfo) {
+        String userId, userPwd, userName = null, userEmail = null;
 
-            while((ln = br.readLine()) != null) {
-                sb.append(ln);
-            }
-        } catch (IOException e) {
-            logger.debug(e.getMessage());
-        } finally {
-            try {
-                if(br != null) br.close();
-            } catch (Exception e) {}
+        String[] userInfo = userInfoString.split("&");
+
+        userId = userInfo[0].split("=")[1];
+        userPwd = userInfo[1].split("=")[1];
+        if(extraInfo) {
+            userName = userInfo[2].split("=")[1];
+            userEmail = userInfo[3].split("=")[1];
         }
-        return sb.toString();
+
+        String[] userInfoSplitted = {userId, userPwd, userName, userEmail};
+        return userInfoSplitted;
+    }
+
+    public byte[] readFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] content = new byte[(int) file.length()];
+            fileInputStream.read(content);
+            return content;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
 
