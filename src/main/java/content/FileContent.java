0f74197a86a87fefcb1abbb998a8c81d712c5030
win9 <krrr883@gmail.com>
2024-01-29 17:45:07 +0900

    [Feat] href링크 수정
    현재의 url에서 링크가 접속될 수 있도록 변경

--- src/main/java/content/FileContent.java
+++ src/main/java/content/FileContent.java
@@ -3,11 +3,11 @@ package content;
 import model.Model;
 
 public enum FileContent {
-    NON_LOGIN("<li><a href=\"user/login.html\" role=\"button\">회원가입</a></li>\n" +
-            "<li><a href=\"user/form.html\" role=\"button\">회원가입</a></li>"),
+    NON_LOGIN("<li><a href=\"../user/form.html\" role=\"button\">회원가입</a></li>\n" +
+            "<li><a href=\"../user/login.html\" role=\"button\">로그인</a></li>"),
     LOGIN("<li><a href=\"#\" role=\"button\">로그아웃</a></li>\n" +
-            "<li><a href=\"user/form.html\" role=\"button\">%s</a></li>\n" +
-            "<li><a href=\"#\" role=\"button\">개인정보 수정</a></li>");
+            "<li><a href=\"#\" role=\"button\">%s 님</a></li>\n" +
+            "<li><a href=\"#\" role=\"button\">개인정보 수정</a></li>\n");
 
     private String text;
     FileContent (String text) {
