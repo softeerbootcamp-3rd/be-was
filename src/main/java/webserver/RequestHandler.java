9849ee6742a2305494510272464818aa4b5b22d9
OSSU <o.ss.u2056@gmail.com>
2024-01-29 23:42:03 +0900

    refactor: run() 메서드 내에서 수행되는 여러 로직 메서드 분리 HttpRequest 처리 로직, 정적 자원 처리 로직, 동적 자원 처리 로직을 각각 메서드로 분리함

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -18,6 +18,8 @@ import utils.FileReader;
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final Socket connection;
+    private static final String INDEX_PAGE = "/index.html";
+    private static final String REDIRECT_PAGE = "/404.html";
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -28,11 +30,8 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-            // httpRequest 생성 및 출력
-            HttpRequest httpRequest = new HttpRequest(br);
-            logger.debug(httpRequest.toString());
+            // HttpRequest 처리 로직
+            HttpRequest httpRequest = processRequest(in);
 
             // 동적 & 정적 자원 분할 처리
             String requestUrl = httpRequest.getRequestLine().getUri();
@@ -40,34 +39,17 @@ public class RequestHandler implements Runnable {
 
             // "/" -> "/index.html"로 반환
             if (requestUrl.equals("/")) {
-                requestUrl = "/index.html";
+                requestUrl = INDEX_PAGE;
             }
 
             // 정적 자원부터 탐색 후 동적 자원 처리
             // 정적 자원 처리 - basePath 찾은 경우
             String basePath = FileReader.getBasePath(requestUrl);
             if (!basePath.isEmpty()) {
-                String filePath = basePath + Parser.extractPath(requestUrl);
-                response = ResourceHandler.process(filePath, httpRequest);
+                response = handleStaticResource(basePath, requestUrl, httpRequest);
             } else {
                 // 동적 자원 처리
-                RequestLine requestLine = httpRequest.getRequestLine();
-                ValidatorControllerMapper vc = ValidatorControllerMapper.getValidatorAndControllerByPath(requestLine.getMethodAndPath());
-
-                if (vc != null) {
-                    Function<String, Boolean> validator = vc.getValidator();
-                    // 유효성 검증 실패할 경우 BAD_REQUEST 반환
-                    if (validator != null && !validator.apply(httpRequest.getBody())) {
-                        response = HttpResponse.of(HttpStatus.BAD_REQUEST);
-                    } else {
-                        // validator 없거나 유효성 검증에 통과할 경우 컨트롤러에게 HttpRequest 전달
-                        Function<String, HttpResponse> controller = vc.getController();
-                        response = controller.apply(httpRequest.getBody());
-                    }
-                } else {
-                    Map<String, String> header = Map.of("Location", "/404.html");
-                    response = HttpResponse.of(HttpStatus.REDIRECT, header);
-                }
+                response = handleDynamicResource(httpRequest.getRequestLine(), httpRequest);
             }
 
             // 응답
@@ -76,6 +58,40 @@ public class RequestHandler implements Runnable {
 
         } catch (IOException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private HttpRequest processRequest(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        // httpRequest 생성 및 출력
+        HttpRequest httpRequest = new HttpRequest(br);
+        logger.debug(httpRequest.toString());
+
+        return httpRequest;
+    }
+
+    private HttpResponse handleStaticResource(String basePath, String requestUrl, HttpRequest httpRequest) {
+        String filePath = basePath + Parser.extractPath(requestUrl);
+        return ResourceHandler.process(filePath, httpRequest);
+    }
+
+    private HttpResponse handleDynamicResource(RequestLine requestLine, HttpRequest httpRequest) {
+        ValidatorControllerMapper vc = ValidatorControllerMapper.getValidatorAndControllerByPath(requestLine.getMethodAndPath());
+
+        if (vc != null) {
+            Function<String, Boolean> validator = vc.getValidator();
+            // 유효성 검증 실패할 경우 BAD_REQUEST 반환
+            if (validator != null && !validator.apply(httpRequest.getBody())) {
+                return HttpResponse.of(HttpStatus.BAD_REQUEST);
+            } else {
+                // validator 없거나 유효성 검증에 통과할 경우 컨트롤러에게 HttpRequest 전달
+                Function<String, HttpResponse> controller = vc.getController();
+                return controller.apply(httpRequest.getBody());
+            }
+        } else {
+            Map<String, String> header = Map.of("Location", REDIRECT_PAGE);
+            return HttpResponse.of(HttpStatus.REDIRECT, header);
         }
     }
 }
