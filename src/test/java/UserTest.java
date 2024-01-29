cd4a5e046864480b13a84494fc66ddfff697fd84
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-01-29 10:38:32 +0900

    feat : delete old tests

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,29 +1,30 @@
-import controller.BasicController;
 import controller.UserController;
 import db.Database;
 import http.HttpStatus;
 import http.Request;
 import http.Response;
+import http.SessionManager;
 import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.ModelAndView;
-import webserver.view.InternalResourceView;
+import webserver.adaptor.RequestHandlerAdapter;
 import webserver.ViewResolver;
-import webserver.adaptor.MyHandlerAdapter;
+import webserver.view.View;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserTest.class);
 
-    MyHandlerAdapter adapter = new MyHandlerAdapter();
+    RequestHandlerAdapter adapter = new RequestHandlerAdapter();
     ViewResolver viewResolver = new ViewResolver();
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     DataOutputStream dos = new DataOutputStream(baos);
@@ -37,7 +38,7 @@ public class UserTest {
     public void UserCreate() throws IOException {
         //given
         Request req = new Request("GET","/user/create?userId=jomulagy&password=1234%21&name=%EA%B9%80%EC%A7%80%ED%9B%88&email=jomulagy688%40gmail.com");
-        Response res = new Response();
+        Response res = new Response(dos);
         BasicController handler = new UserController();
         ModelAndView mv = adapter.handle(req, res, handler);
         //when
@@ -56,15 +57,36 @@ public class UserTest {
     public void UserCreateRedirect() throws IOException {
         //given
         Request req = new Request("GET","/user/create?userId=jomulagy&password=1234%21&name=%EA%B9%80%EC%A7%80%ED%9B%88&email=jomulagy688%40gmail.com");
-        Response res = new Response();
+        Response res = new Response(dos);
         BasicController handler = new UserController();
         ModelAndView mv = adapter.handle(req, res, handler);
         //when
 
-        InternalResourceView view = viewResolver.resolve(mv.getViewName());
-        view.render(dos, req, res);
+        View view = viewResolver.resolve(mv.getViewName());
+        view.render(req, res,mv.getModel());
         //then
         assertEquals(HttpStatus.MOVED_PERMANENTLY,res.getStatus());
         assertEquals("/user/login.html",res.getLocation());
+    }
+
+    @Test
+    @DisplayName("/user/profile 페이지")
+    public void UserProfileRendering() throws IOException {
+        //given
+
+        Request req = new Request("GET","/user/profile");
+        Response res = new Response(dos);
+
+        BasicController handler = new UserController();
+        User user = new User();
+        user.setUserId("jomulagy");
+        SessionManager.createSession(user,res,"SID");
+        req.setCookies("SID = ");
+        //when
+        ModelAndView mv = adapter.handle(req, res, handler);
+        logger.debug(mv.getViewName());
+        User returnedUser = (User)mv.getModel().getAttribute("user");
+        //then
+        assertEquals(user.getUserId(),returnedUser.getUserId());
     }
 }
