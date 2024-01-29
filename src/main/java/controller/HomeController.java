7000a7ce2b4887cdff64a28cb007e3d33d5fbf79
Yang-soeun <soeun8636@naver.com>
2024-01-29 11:51:02 +0900

    [FEAT]: replace 하는 내용 수정
    기존 로직은 버튼의 이름만 변경이 되어 회원가입이 되어도 사용자 이름은 뜨지만 사용자 이름을 눌렀을 경우 로그인 창으로 이동하게 된다. 이 부분을 수정

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -16,9 +16,10 @@ public class HomeController {
 
             if(httpRequest.getSessionId() != null){//로그인 된 경우
                 User user = SessionManager.findUserBySessionId(httpRequest.getSessionId());
-                httpResponse.setBody("로그인", user.getName() + "님");
+                httpResponse.setBody("{{login}}", "<li><a>" + user.getName() + "님</a></li>");
             }
 
+            httpResponse.setBody("{{login}}", "<li><a href=\"user/login.html\" role=\"button\">로그인</a></li>");
             return httpResponse;
         } catch (IOException e){
             return HttpResponse.errorResponse(INTERNAL_SERVER_ERROR, e.getMessage());
