6f8c8eea32d5b75fa024980d5ac95b6b1383bd1b
isexample <yeji0331@naver.com>
2024-01-29 13:48:43 +0900

    [Refactor] HtmlBuilder로 HTML 생성 로직 이동 및 네비게이션 바 최적화
    Controller에서 HTML 생성 관련 로직을 HtmlBuilder로 이동하여 책임을 명확히 분리했습니다.
    
    - HTML 생성 과정의 재사용성과 유지보수성을 향상
    - 로그인 상태와 비로그인 상태를 구분하는 주석 태그를 간소화하여 클라이언트에 전송되는 HTML에서 불필요한 데이터의 양을 최소화

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -4,7 +4,6 @@ import db.Database;
 import model.User;
 import service.UserService;
 import utils.HtmlBuilder;
-import utils.ResourceReader;
 import utils.SessionManager;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
@@ -50,36 +49,27 @@ public class UserController {
         }
 
         // 로그인 사용자 처리
+        User user = Database.findUserById(SessionManager.getUserId(sessionId));
         List<User> loggedInUsers = userService.getLoginUsers();
-        String userTableHtml = HtmlBuilder.userListHtml(loggedInUsers);
+        String htmlTemplate = HtmlBuilder.loggedInNavBarHtml("/user/list.html", user.getName());
+        byte[] body = HtmlBuilder.userListHtml(htmlTemplate, loggedInUsers).getBytes();
 
-        String htmlTemplate = ResourceReader.getInstance().getFileTemplate("/user/list.html");
-        String finalHtml = htmlTemplate.replace("<!-- UserList -->", userTableHtml);
-
-        byte[] body = finalHtml.getBytes();
         return HttpResponseBuilder.getInstance().createSuccessResponse(HttpStatus.OK, body);
     }
 
     public HttpResponse home(HttpRequest request) throws IOException {
         String sessionId = request.getSessionId();
-        String htmlTemplate = ResourceReader.getInstance().getFileTemplate("/index.html");
 
         // 로그인하지 않은 사용자 처리
         if(!SessionManager.isLoggedInUser(sessionId)){
-            htmlTemplate = htmlTemplate.replace("<!-- GuestUser_start -->", "<!--");
-            htmlTemplate = htmlTemplate.replace("<!-- GuestUser_end -->", "-->");
-            return HttpResponseBuilder.getInstance().createSuccessResponse(HttpStatus.OK, htmlTemplate.getBytes());
+            byte[] body = HtmlBuilder.guestNavBarHtml("/index.html").getBytes();
+            return HttpResponseBuilder.getInstance().createSuccessResponse(HttpStatus.OK, body);
         }
 
         // 로그인 사용자 처리
         User user = Database.findUserById(SessionManager.getUserId(sessionId));
-        String homeHtml = HtmlBuilder.loggedInHomeHtml(user.getName());
+        byte[] body = HtmlBuilder.loggedInNavBarHtml("/index.html", user.getName()).getBytes();
 
-        htmlTemplate = htmlTemplate.replace("<!-- LoggedInUser_start -->", "<!--");
-        htmlTemplate = htmlTemplate.replace("<!-- LoggedInUser_end -->", "-->");
-        String finalHtml = htmlTemplate.replace("<!-- UserName -->", homeHtml);
-
-        byte[] body = finalHtml.getBytes();
         return HttpResponseBuilder.getInstance().createSuccessResponse(HttpStatus.OK, body);
     }
 
