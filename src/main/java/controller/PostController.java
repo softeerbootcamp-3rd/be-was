c511dae2142670621b9d8c8301f9feb542d95da4
admin <lejs1008@naver.com>
2024-01-29 18:04:55 +0900

    step-7 [Refactor] url 이름 변경
    mapping url을 동사에서 명사를 사용하도록 변경, write -> post

--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -9,7 +9,7 @@ import session.SessionManager;
 public class PostController {
     SessionManager sessionManager = new SessionManager();
 
-    @GetMapping(url = "/write")
+    @GetMapping(url = "/post")
     public String getWriteForm(HttpRequest request) {
         if (sessionManager.getUserBySessionId(request) == null) {
             return "/user/login.html";
@@ -17,4 +17,5 @@ public class PostController {
             return "write_form.html";
         }
     }
+
 }
