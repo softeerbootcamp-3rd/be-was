c10c63d514c3166bcced1e856b793716c0a9fdb7
LSH <104637774+lsh2613@users.noreply.github.com>
2024-01-29 17:25:23 +0900

    [Refactor] 애매한 메소드명 수정 findAll -> findAllUsers
    - Database에서 qna도 관리하기 위한 애매한 메소드명 수정

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -2,6 +2,7 @@ package db;
 
 import com.google.common.collect.Maps;
 
+import model.Qna;
 import model.User;
 
 import java.util.Collection;
@@ -9,6 +10,11 @@ import java.util.Map;
 
 public class Database {
     private static Map<String, User> users = Maps.newHashMap();
+    private static Map<String, Qna> qnas = Maps.newHashMap();
+
+    static {
+        addUser(new User("test", "test", "test", "test@test"));
+    }
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
@@ -18,11 +24,24 @@ public class Database {
         return users.get(userId);
     }
 
-    public static Collection<User> findAll() {
+    public static Collection<User> findAllUsers() {
         return users.values();
     }
 
     public static void clearUsers() {
         users.clear();
     }
+
+    public static void addQna(Qna qna) {
+        qnas.put(qna.getWriter(), qna);
+    }
+
+    public static Qna findQnaByUsername(String username) {
+        return qnas.get(username);
+    }
+
+    public static Collection<Qna> findAllQnas() {
+        return qnas.values();
+    }
+
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -47,6 +47,6 @@ public class UserService {
     }
 
     public static Collection<User> findAll() {
-        return Database.findAll();
+        return Database.findAllUsers();
     }
 }
