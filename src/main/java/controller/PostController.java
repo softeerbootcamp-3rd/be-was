62db649b1512b96eb956128ca3c337fc48ac5f3a
OSSU <o.ss.u2056@gmail.com>
2024-01-30 17:42:11 +0900

    step 7 : Post 컨트롤러에 파일 첨부 로직 추가 Post 객체 생성 시 첨부된 파일 정보 추가

--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -1,10 +1,11 @@
 package controller;
 
 import http.HttpStatus;
+import http.request.HttpRequest;
+import http.request.MultiPartForm;
 import http.response.HttpResponse;
 import model.Post;
 import service.PostService;
-import utils.Parser;
 
 import java.util.Date;
 import java.util.Map;
@@ -13,10 +14,11 @@ import java.util.UUID;
 public class PostController {
     private static final PostService postService = new PostService();
 
-    public static HttpResponse upload(String body) {
-        Map<String, String> bodyMap = Parser.extractParams(body);
+    public static HttpResponse upload(HttpRequest request) {
+        MultiPartForm multiPartForm = request.getMultiPartForm();
+        Map<String, String> fields = multiPartForm.getFields();
         String postId = UUID.randomUUID().toString();
-        Post post = new Post(postId, bodyMap.get("writer"), bodyMap.get("title"), bodyMap.get("contents"), new Date());
+        Post post = new Post(postId, fields.get("writer"), fields.get("title"), fields.get("contents"), new Date(), fields.get("file"));
 
         postService.createPost(post);
         Map<String, String> header = Map.of("Location", "/index.html");
