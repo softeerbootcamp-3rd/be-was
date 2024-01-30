970038b30cbb8fffafe6c5b5018ca81006242a54
admin <rlawnghks99@gmail.com>
2024-01-30 16:39:39 +0900

    chore: ResouceMapping 클래스 확장자 추가
    확장자 TXT, PNG, JPG, JPEG에 대한 upload 경로와 컨텐츠 타입을 매핑

--- src/main/java/controller/ResourceMapping.java
+++ src/main/java/controller/ResourceMapping.java
@@ -2,13 +2,16 @@ package controller;
 
 public enum ResourceMapping {
     HTML(ResourceConstants.TEMPLATE_DIRECTORY, "text/html"),
+    TXT(ResourceConstants.UPLOADS_URL, "text/plain"),
     CSS(ResourceConstants.STATIC_DIRECTORY, "text/css"),
     JS(ResourceConstants.STATIC_DIRECTORY, "application/javascript"),
     ICO(ResourceConstants.STATIC_DIRECTORY, "image/x-icon"),
-    PNG(ResourceConstants.STATIC_DIRECTORY, "image/png"),
-    JPG(ResourceConstants.STATIC_DIRECTORY, "image/jpg"),
     WOFF(ResourceConstants.STATIC_DIRECTORY, "font/woff"),
-    TTF(ResourceConstants.STATIC_DIRECTORY, "font/ttf");
+    TTF(ResourceConstants.STATIC_DIRECTORY, "font/ttf"),
+    PNG(ResourceConstants.UPLOADS_URL, "image/png"),
+    JPG(ResourceConstants.UPLOADS_URL, "image/jpg"),
+    JPEG(ResourceConstants.UPLOADS_URL, "image/jpeg");
+
 
     private final String directory;
     private final String contentType;
@@ -28,5 +31,6 @@ public enum ResourceMapping {
     public static class ResourceConstants {
         public static final String TEMPLATE_DIRECTORY = "/templates";
         public static final String STATIC_DIRECTORY = "/static";
+        public static final String UPLOADS_URL = "/uploads";
     }
 }
